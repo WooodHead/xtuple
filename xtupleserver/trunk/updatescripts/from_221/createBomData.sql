117897127f98d94acc2b64aee092d757ce5a1220
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2007-10-22 18:09:33 +0000

    4771:  replace bom reporting data type with something more universal

---
+++ xtupleserver/trunk/updatescripts/from_221/createBomData.sql
@@ -0,0 +1,24 @@
+BEGIN;
+
+--DROP TYPE bomdata;
+
+CREATE TYPE bomdata AS
+   (bomdata_bomwork_id INTEGER,
+    bomdata_bomwork_parent_id INTEGER,
+    bomdata_bomwork_level INTEGER,
+    bomdata_bomwork_seqnumber INTEGER,
+    bomdata_item_number TEXT,
+    bomdata_uom_name TEXT,
+    bomdata_item_descrip1 TEXT,
+    bomdata_item_descrip2 TEXT,
+    bomdata_itemdescription TEXT,
+    bomdata_qtyper TEXT,
+    bomdata_scrap TEXT,
+    bomdata_createchild TEXT,
+    bomdata_issuemethod TEXT,
+    bomdata_effective TEXT,
+    bomdata_expires TEXT,
+    bomdata_expired BOOLEAN,
+    bomdata_future  BOOLEAN);
+
+COMMIT;
