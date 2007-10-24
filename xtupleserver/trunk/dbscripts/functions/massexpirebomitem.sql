cc2e42b1c96c6cce1baa2e458ca2be396a1838d8
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2007-10-24 14:12:13 +0000

    4771: mass expire support for rev control

--- xtupleserver/trunk/dbscripts/functions/massexpirebomitem.sql
+++ xtupleserver/trunk/dbscripts/functions/massexpirebomitem.sql
@@ -9,7 +9,8 @@ BEGIN
   UPDATE bomitem
   SET bomitem_expires=pExpireDate
   WHERE ( (bomitem_expires >= CURRENT_DATE)
-   AND (bomitem_item_id=pItemid) );
+   AND (bomitem_item_id=pItemid)
+   AND (bomitem_rev_id=getActiveRevId(''BOM'',bomitem_parent_item_id)) );
 
   RETURN TRUE;
 END;
