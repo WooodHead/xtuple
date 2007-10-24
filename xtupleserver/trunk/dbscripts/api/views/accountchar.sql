09f67adadbe93c52e9a313146ee89c295d1be75c
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2007-10-24 23:51:03 +0000

    contact and account views

---
+++ xtupleserver/trunk/dbscripts/api/views/accountchar.sql
@@ -0,0 +1,59 @@
+BEGIN;
+
+-- Account Characteristic
+
+DROP VIEW api.accountchar;
+CREATE VIEW api.accountchar
+AS 
+   SELECT 
+     crmacct_number::varchar(100) AS account_number,
+     char_name::varchar(100) AS characteristic,
+     charass_value AS value
+   FROM crmacct, char, charass
+   WHERE (('CRMACCT'=charass_target_type)
+   AND (crmacct_id=charass_target_id)
+   AND (charass_char_id=char_id));
+
+GRANT ALL ON TABLE api.accountchar TO openmfg;
+COMMENT ON VIEW api.accountchar IS '
+This view can be used as an interface to import Account Characteristic data directly  
+into the system.  Required fields will be checked and default values will be 
+populated';
+
+--Rules
+
+CREATE OR REPLACE RULE "_INSERT" AS
+    ON INSERT TO api.accountchar DO INSTEAD
+
+  INSERT INTO charass (
+    charass_target_type,
+    charass_target_id,
+    charass_char_id,
+    charass_value,
+    charass_default
+    )
+  VALUES (
+    'CRMACCT',
+    getCustId(NEW.account_number),
+    getCharId(NEW.characteristic,'CRMACCT'),
+    NEW.value,
+    false);
+
+CREATE OR REPLACE RULE "_UPDATE" AS 
+    ON UPDATE TO api.accountchar DO INSTEAD
+
+  UPDATE charass SET
+    charass_value=NEW.value
+  WHERE ((charass_target_type='CRMACCT')
+  AND (charass_target_id=getCustId(OLD.account_number))
+  AND (charass_char_id=getCharId(OLD.characteristic,'CRMACCT')));
+           
+CREATE OR REPLACE RULE "_DELETE" AS 
+    ON DELETE TO api.accountchar DO INSTEAD
+
+  DELETE FROM charass
+  WHERE ((charass_target_type='CRMACCT')
+  AND (charass_target_id=getCustId(OLD.account_number))
+  AND (charass_char_id=getCharId(OLD.characteristic,'CRMACCT')));
+
+COMMIT;
---
+++ xtupleserver/trunk/dbscripts/api/views/contactchar.sql
@@ -0,0 +1,59 @@
+BEGIN;
+
+-- Contact Characteristic
+
+DROP VIEW api.contactchar;
+CREATE VIEW api.contactchar
+AS 
+   SELECT 
+     cntct_id::varchar(100) AS contact_number,
+     char_name::varchar(100) AS characteristic,
+     charass_value AS value
+   FROM cntct, char, charass
+   WHERE (('CNTCT'=charass_target_type)
+   AND (cntct_id=charass_target_id)
+   AND (charass_char_id=char_id));
+
+GRANT ALL ON TABLE api.contactchar TO openmfg;
+COMMENT ON VIEW api.contactchar IS '
+This view can be used as an interface to import Contact Characteristic data directly  
+into the system.  Required fields will be checked and default values will be 
+populated';
+
+--Rules
+
+CREATE OR REPLACE RULE "_INSERT" AS
+    ON INSERT TO api.contactchar DO INSTEAD
+
+  INSERT INTO charass (
+    charass_target_type,
+    charass_target_id,
+    charass_char_id,
+    charass_value,
+    charass_default
+    )
+  VALUES (
+    'CNTCT',
+    getCustId(NEW.contact_number),
+    getCharId(NEW.characteristic,'CNTCT'),
+    NEW.value,
+    false);
+
+CREATE OR REPLACE RULE "_UPDATE" AS 
+    ON UPDATE TO api.contactchar DO INSTEAD
+
+  UPDATE charass SET
+    charass_value=NEW.value
+  WHERE ((charass_target_type='CNTCT')
+  AND (charass_target_id=getCustId(OLD.contact_number))
+  AND (charass_char_id=getCharId(OLD.characteristic,'CNTCT')));
+           
+CREATE OR REPLACE RULE "_DELETE" AS 
+    ON DELETE TO api.contactchar DO INSTEAD
+
+  DELETE FROM charass
+  WHERE ((charass_target_type='CNTCT')
+  AND (charass_target_id=OLD.contact_number)
+  AND (charass_char_id=getCharId(OLD.characteristic,'CNTCT')));
+
+COMMIT;
