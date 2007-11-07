7c4559a6f926486998dbd25787dadcc18fb79c69
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2007-11-07 19:21:01 +0000

    bug 5904 - couldn't create checks if we owed any one vendor 0 or less

--- xtupleserver/trunk/dbscripts/functions/createchecks.sql
+++ xtupleserver/trunk/dbscripts/functions/createchecks.sql
@@ -23,46 +23,54 @@ BEGIN
              AND (apopen_vend_id=vend_id)
              AND (apselect_bankaccnt_id=pBankaccntid) ) LOOP
 
-    -- 0.01 is a temporary amount; we''ll update the check amount later
-    _checkid := createCheck(pBankaccntid,	''V'',	_v.vend_id,
-			    pCheckDate,		0.01,	_check_curr_id,
-			    NULL,		NULL, '''',	'''',	FALSE);
+    -- if we owe this vendor anything (we might not) then create a check
+    IF ((SELECT SUM(currToCurr(apselect_curr_id, _check_curr_id,
+			       apselect_amount, pCheckDate))
+	 FROM apselect, apopen
+	 WHERE ((apselect_apopen_id=apopen_id)
+	   AND  (apopen_vend_id=_v.vend_id)
+	   AND  (apselect_bankaccnt_id=pBankaccntid)) ) > 0) THEN
+      -- 0.01 is a temporary amount; we''ll update the check amount later
+      _checkid := createCheck(pBankaccntid,	''V'',	_v.vend_id,
+			      pCheckDate,		0.01,	_check_curr_id,
+			      NULL,		NULL, '''',	'''',	FALSE);
 
-    FOR _r IN SELECT apopen_id, apselect_id,
-                     apopen_docnumber, apopen_invcnumber, apopen_ponumber,
-                     apopen_docdate, apselect_curr_id,
-                     apselect_amount, apselect_discount
-              FROM apselect, apopen
-              WHERE ( (apselect_apopen_id=apopen_id)
-               AND (apopen_vend_id=_v.vend_id)
-               AND (apselect_bankaccnt_id=pBankaccntid) ) LOOP
+      FOR _r IN SELECT apopen_id, apselect_id,
+		       apopen_docnumber, apopen_invcnumber, apopen_ponumber,
+		       apopen_docdate, apselect_curr_id,
+		       apselect_amount, apselect_discount
+		FROM apselect, apopen
+		WHERE ( (apselect_apopen_id=apopen_id)
+		 AND (apopen_vend_id=_v.vend_id)
+		 AND (apselect_bankaccnt_id=pBankaccntid) ) LOOP
 
-      INSERT INTO checkitem
-      ( checkitem_checkhead_id, checkitem_apopen_id,
-        checkitem_vouchernumber, checkitem_invcnumber, checkitem_ponumber,
-        checkitem_amount, checkitem_discount, checkitem_docdate,
-        checkitem_curr_id )
-      VALUES
-      ( _checkid, _r.apopen_id,
-        _r.apopen_docnumber, _r.apopen_invcnumber, _r.apopen_ponumber,
-        _r.apselect_amount, _r.apselect_discount, _r.apopen_docdate,
-        _r.apselect_curr_id );
+	INSERT INTO checkitem
+	( checkitem_checkhead_id, checkitem_apopen_id,
+	  checkitem_vouchernumber, checkitem_invcnumber, checkitem_ponumber,
+	  checkitem_amount, checkitem_discount, checkitem_docdate,
+	  checkitem_curr_id )
+	VALUES
+	( _checkid, _r.apopen_id,
+	  _r.apopen_docnumber, _r.apopen_invcnumber, _r.apopen_ponumber,
+	  _r.apselect_amount, _r.apselect_discount, _r.apopen_docdate,
+	  _r.apselect_curr_id );
 
-      DELETE FROM apselect
-      WHERE (apselect_id=_r.apselect_id);
+	DELETE FROM apselect
+	WHERE (apselect_id=_r.apselect_id);
 
-    END LOOP;
+      END LOOP;
 
-    -- one check can pay for purchases on multiple dates in multiple currencies
-    UPDATE checkhead
-    SET checkhead_amount = (SELECT SUM(currToCurr(checkitem_curr_id,
-						  _check_curr_id,
-						  checkitem_amount, pCheckDate))
-			    FROM checkitem
-			    WHERE (checkitem_checkhead_id=checkhead_id))
-    WHERE (checkhead_id=_checkid);
+      -- one check can pay for purchases on multiple dates in multiple currencies
+      UPDATE checkhead
+      SET checkhead_amount = (SELECT SUM(currToCurr(checkitem_curr_id,
+						    _check_curr_id,
+						    checkitem_amount, pCheckDate))
+			      FROM checkitem
+			      WHERE (checkitem_checkhead_id=checkhead_id))
+      WHERE (checkhead_id=_checkid);
 
-    _counter := (_counter + 1);
+      _counter := (_counter + 1);
+    END IF;
 
   END LOOP;
 
