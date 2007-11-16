763b90515535fefdeaa1990c60e785fe9a453a88
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2007-11-16 18:24:03 +0000

    4832:  add ability to rollback credited quantities and amounts on ra when associated credit memo is deleted.

--- xtupleserver/trunk/dbscripts/triggers/cmhead.sql
+++ xtupleserver/trunk/dbscripts/triggers/cmhead.sql
@@ -1,7 +1,13 @@
 CREATE OR REPLACE FUNCTION _cmheadTrigger() RETURNS "trigger" AS '
 BEGIN
   IF (TG_OP = ''DELETE'') THEN
-    -- Something can go here
+    -- If this was created by a return, then reset the return
+    IF (OLD.cmhead_rahead_id IS NOT NULL) THEN
+      UPDATE rahead SET
+        rahead_freight_credited=rahead_freight_credited-OLD.cmhead_freight,
+        rahead_misc_credited=rahead_misc_credited-OLD.cmhead_misc
+      WHERE (rahead_id=cmhead_rahead_id);
+    END IF;
     RETURN OLD;
   END IF;
   IF (TG_OP = ''INSERT'') THEN
--- xtupleserver/trunk/dbscripts/triggers/cmitem.sql
+++ xtupleserver/trunk/dbscripts/triggers/cmitem.sql
@@ -1,10 +1,22 @@
 CREATE OR REPLACE FUNCTION _cmitemTrigger() RETURNS "trigger" AS '
+DECLARE
+  _ext NUMERIC;
+
 BEGIN
   IF (TG_OP = ''DELETE'') THEN
     IF((COALESCE(OLD.cmitem_tax_ratea,0.0)
       + COALESCE(OLD.cmitem_tax_rateb,0.0)
       + COALESCE(OLD.cmitem_tax_ratec,0.0)) <> 0.0) THEN
       PERFORM recalculateCmheadTaxTotal(OLD.cmitem_cmhead_id);
+    END IF;
+
+--  If this was created by a return, reset return values
+    IF (OLD.cmitem_raitem_id) IS NOT NULL THEN
+      _ext := ROUND((OLD.cmitem_qtycredit * OLD.cmitem_qty_invuomratio) *  (OLD.cmitem_unitprice / OLD.cmitem_price_invuomratio),2);
+      UPDATE raitem SET
+        raitem_qtycredited = raitem_qtycredited-OLD.cmitem_qtycredit,
+        raitem_amtcredited = raitem_amtcredited-_ext
+      WHERE (raitem_id=OLD.cmitem_raitem_id);
     END IF;
     RETURN OLD;
   END IF;
