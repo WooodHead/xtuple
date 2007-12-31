ab61620caf891bbedb43c62a11f763685920c1f3
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2007-12-31 22:05:18 +0000

    5877: Precheck for item api view

---
+++ xtupleserver/trunk/updatescripts/from_230/Precheck.sql
@@ -0,0 +1,16 @@
+--  All items must have an inventory UOM
+SELECT COUNT(*)=0 FROM item WHERE (item_inv_uom_id IS NULL);
+--  All items must have an Pricing UOM
+SELECT COUNT(*)=0 FROM item WHERE (item_price_uom_id IS NULL);
+--  All items must have a class code id
+SELECT COUNT(*)=0 FROM item WHERE (item_classcode_id IS NULL);
+--  All item class codes must be valid
+SELECT COUNT(*)=0 FROM item WHERE (item_classcode_id NOT IN (
+SELECT classcode_id FROM classcode WHERE (classcode_id=item_classcode_id)
+));
+--  All items must have a valid item type
+SELECT COUNT(*)=0 FROM item WHERE (item_type NOT IN ('P','M','F','J','O','R','S','T','B','L','Y','C'));
+--  All items must have a planning type
+SELECT COUNT(*)=0 FROM item WHERE (item_planning_type NOT IN ('N','M','S'));
+
+
