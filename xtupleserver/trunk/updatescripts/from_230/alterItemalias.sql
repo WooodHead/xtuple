57ac3ac2b28021e88b23da15b628cecaf2ed826c
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-01-14 17:05:22 +0000

    5881:   Revert alteritemalias file back to previous state.

--- xtupleserver/trunk/updatescripts/from_230/alterItemalias.sql
+++ xtupleserver/trunk/updatescripts/from_230/alterItemalias.sql
@@ -2,5 +2,8 @@ BEGIN;
 
   ALTER TABLE itemalias ADD UNIQUE (itemalias_item_id, itemalias_number);
   ALTER TABLE itemalias ALTER COLUMN itemalias_usedescrip SET NOT NULL;
+  ALTER TABLE itemalias ALTER COLUMN itemalias_item_id SET NOT NULL;
+  ALTER TABLE itemalias ALTER COLUMN itemalias_number SET NOT NULL;
+  ALTER TABLE itemalias ALTER COLUMN itemalias_usedescrip SET NOT NULL;
 
 COMMIT;
