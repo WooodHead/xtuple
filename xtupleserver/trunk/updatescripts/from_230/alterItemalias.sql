bd6cd86d8d03a1ae42cfbc64e2b8653a13105088
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-01-14 17:11:49 +0000

    Restored contents to rev 1128.

--- xtupleserver/trunk/updatescripts/from_230/alterItemalias.sql
+++ xtupleserver/trunk/updatescripts/from_230/alterItemalias.sql
@@ -1,7 +1,9 @@
 BEGIN;
 
+  UPDATE itemalias SET itemalias_usedescrip=FALSE WHERE (itemalias_usedescrip IS NULL);
+
+  ALTER TABLE itemalias ADD FOREIGN KEY (itemalias_item_id) REFERENCES item (item_id);
   ALTER TABLE itemalias ADD UNIQUE (itemalias_item_id, itemalias_number);
-  ALTER TABLE itemalias ALTER COLUMN itemalias_usedescrip SET NOT NULL;
   ALTER TABLE itemalias ALTER COLUMN itemalias_item_id SET NOT NULL;
   ALTER TABLE itemalias ALTER COLUMN itemalias_number SET NOT NULL;
   ALTER TABLE itemalias ALTER COLUMN itemalias_usedescrip SET NOT NULL;
