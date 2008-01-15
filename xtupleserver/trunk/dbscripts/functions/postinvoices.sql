7bfea6dd66e7bae77d1ae62889533ba4e2118f8d
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-01-15 15:10:56 +0000

    Updated function to properly handle invoices that don't have line items. Issue # 4943.

--- xtupleserver/trunk/dbscripts/functions/postinvoices.sql
+++ xtupleserver/trunk/dbscripts/functions/postinvoices.sql
@@ -10,7 +10,7 @@ END;
 CREATE OR REPLACE FUNCTION postInvoices(BOOLEAN, BOOLEAN) RETURNS INTEGER AS '
 DECLARE
   pPostUnprinted ALIAS FOR $1;
-  pInclZeros	 ALIAS FOR $2;
+  pInclZeros     ALIAS FOR $2;
   _journalNumber INTEGER;
   _counter INTEGER;
   _r RECORD;
@@ -27,14 +27,14 @@ BEGIN
 
   ELSE
     PERFORM postInvoice(invchead_id, _journalNumber)
-    FROM invchead, invcitem LEFT OUTER JOIN item ON (invcitem_item_id=item_id)  
-    WHERE ((invchead_id=invcitem_invchead_id)
-      AND  (NOT invchead_posted)
-      AND  (pPostUnprinted OR invchead_printed))
-    GROUP BY invchead_id 
-    HAVING SUM(round((invcitem_billed * invcitem_qty_invuomratio) * (invcitem_price /  
-	      CASE WHEN (item_id IS NULL) THEN 1 
-	      ELSE invcitem_price_invuomratio END), 2)) > 0;
+       FROM invchead LEFT OUTER JOIN invcitem ON (invchead_id=invcitem_invchead_id) LEFT OUTER JOIN item ON (invcitem_item_id=item_id)  
+      WHERE((NOT invchead_posted)
+        AND (pPostUnprinted OR invchead_printed))
+      GROUP BY invchead_id, invchead_freight, invchead_tax, invchead_misc_amount
+     HAVING (COALESCE(SUM(round((invcitem_billed * invcitem_qty_invuomratio) * (invcitem_price /  
+              CASE WHEN (item_id IS NULL) THEN 1 
+              ELSE invcitem_price_invuomratio END), 2)),0)
+             + invchead_freight + invchead_tax + invchead_misc_amount) > 0;
   END IF;
 
   RETURN _journalNumber;
