eec2079567b29f16d6174b470a9f51c0bb5ed736
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-01-31 20:27:50 +0000

    fixes bug #6207 and fixes bug #6209 - can no longer delete a sales order with credit card history. now you can cancel a sales order with preauths but not with one with charges. you also can no longer charge a preauth originally created for a sales order that's since been canceled.

--- xtupleserver/trunk/updatescripts/from_230/alterccpay.sql
+++ xtupleserver/trunk/updatescripts/from_230/alterccpay.sql
@@ -9,6 +9,7 @@ ALTER TABLE ccpay RENAME ccpay_yp_r_error	TO ccpay_r_error;
 ALTER TABLE ccpay RENAME ccpay_yp_r_approved	TO ccpay_r_approved;
 ALTER TABLE ccpay RENAME ccpay_yp_r_code	TO ccpay_r_code;
 ALTER TABLE ccpay RENAME ccpay_yp_r_ref		TO ccpay_r_ref;
+ALTER TABLE ccpay RENAME ccpay_yp_r_tax		TO ccpay_r_tax;
 ALTER TABLE ccpay RENAME ccpay_yp_r_message	TO ccpay_r_message;
 ALTER TABLE ccpay DROP ccpay_pfpro_pnref;
 ALTER TABLE ccpay DROP ccpay_pfpro_result;
