894771befca6fd0e11235519cacc9023482c30aa
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-02-06 14:16:17 +0000

    Added some xsl wrappers for generating the userguide and modified the script we have so far to use them simplifying things.

--- xtupledocs/trunk/userguide/generateUserguide.sh
+++ xtupledocs/trunk/userguide/generateUserguide.sh
@@ -2,13 +2,12 @@
 # lets setup some variables that will make it easier to change the script
 USERGUIDE=/home/cryan/src/openmfg/xtupledocs/userguide
 OUTDIR=/home/cryan/tmp2/output
-DOCBOOK_XSL=/usr/share/sgml/docbook/xsl-stylesheets-1.65.1-2
-XSLTPROC=/home/cryan/src/libxslt-1.1.22/xsltproc/xsltproc
+XSLTPROC=xsltproc
 
 # You need to have the appropriate Docbook DTD's setup in your xml catalog.
 # At this time 4.5 is the version being used. Depending on your system
 # this location for the catalog will vary and how to update it.
-
+#
 # This variable sets an alternate location for a catalog.
 #XML_CATALOG_FILES=/home/cryan/xmlcatalog
 
@@ -16,16 +15,8 @@ XSLTPROC=/home/cryan/src/libxslt-1.1.22/xsltproc/xsltproc
 # using the specified docbook xsl (second to last argument) and specifies
 # several different options to customize the output
 $XSLTPROC --xinclude \
-          --stringparam generate.section.toc.level 3 \
-          --stringparam toc.section.depth 3 \
-          --stringparam section.autolabel 1 \
-          --stringparam section.label.includes.component.label 1 \
-          --stringparam show.comments 0 \
-          --stringparam chunk.section.depth 2 \
-          --stringparam chunk.first.sections 1 \
-          --stringparam chunk.separate.lots 1 \
           --stringparam base.dir $OUTDIR/ \
-          $DOCBOOK_XSL/html/chunk.xsl \
+          $USERGUIDE/xsl/html/chunk.xsl \
           $USERGUIDE/userguide.xml
 
 # now that the output has been generated we need to copy over the images
---
+++ xtupledocs/trunk/userguide/xsl/html/chunk.xsl
@@ -0,0 +1,8 @@
+<?xml version='1.0'?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+ <xsl:import href="http://docbook.sourceforge.net/release/xsl/current/html/chunk.xsl" />
+ <xsl:include href="../common-options.xsl" />
+ <xsl:param name="chunk.section.depth" select="2" />
+ <xsl:param name="chunk.first.sections" select="1" />
+ <xsl:param name="chunk.separate.lots" select="1" />
+</xsl:stylesheet>
