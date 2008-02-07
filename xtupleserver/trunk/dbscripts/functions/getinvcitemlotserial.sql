e030eaa5dc86a6912874186354edb13a684a3f2c
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-02-07 15:59:07 +0000

    issue #6367:  get correct lot/serial info on invoice.

--- xtupleserver/trunk/dbscripts/functions/getinvcitemlotserial.sql
+++ xtupleserver/trunk/dbscripts/functions/getinvcitemlotserial.sql
@@ -1,10 +1,11 @@
 
 CREATE OR REPLACE FUNCTION getInvcitemLotSerial(INTEGER) RETURNS TEXT AS '
 DECLARE
-  _invcitemid ALIAS FOR $1;
+  pInvcitemid ALIAS FOR $1;
   _lotserial text;
   _r RECORD;
   _first BOOLEAN;
+  _newMethod BOOLEAN;
 BEGIN
  
   --Test to see if Lot/Serial Enabled
@@ -17,22 +18,54 @@ BEGIN
     _lotserial := '''';
     _first := true;
 
-    FOR _r IN SELECT DISTINCT invdetail_lotserial
-              FROM invdetail JOIN invhist ON (invdetail_invhist_id=invhist_id)
-             WHERE ((invhist_transtype=''SH'')
-               AND  (invdetail_invcitem_id=_invcitemid)) LOOP
-      IF ((_r.invdetail_lotserial IS NOT NULL) AND (_r.invdetail_lotserial != '''')) THEN
-        IF (_first = false) THEN
-          _lotserial := _lotserial || '', '';
-        END IF;
-        _lotserial := _lotserial || _r.invdetail_lotserial;
-        _first := false;
-      END IF;
-    END LOOP;
+--  Two ways of doing this: old method and new method
+--  First, find out if new method employed.
+--  (new method is more accurate, but unfortunately no
+--  way to migrate or correct old data.  Have to keep
+--  old method in case someone reprints an old invoice.)
 
-    RETURN _lotserial;
+    SELECT (COUNT(*) > 0) INTO _newMethod 
+    FROM shipitem 
+    WHERE ((shipitem_invcitem_id=pInvcitemid)
+    AND (shipitem_invhist_id IS NOT NULL));
+
+    IF (_newMethod) THEN
+      FOR _r IN SELECT DISTINCT invdetail_lotserial
+                FROM invdetail, invhist, shipitem
+               WHERE ((shipitem_invcitem_id=pInvcitemid)
+                 AND  (shipitem_invhist_id=invhist_id)
+                 AND  (invhist_id=invdetail_invhist_id)
+                 AND  (invdetail_invcitem_id=pInvcitemid)) LOOP
+        IF ((_r.invdetail_lotserial IS NOT NULL) AND (_r.invdetail_lotserial != '''')) THEN
+          IF (_first = false) THEN
+            _lotserial := _lotserial || '', '';
+          END IF;
+          _lotserial := _lotserial || _r.invdetail_lotserial;
+          _first := false;
+        END IF;
+      END LOOP;
+
+      RETURN _lotserial;
+    ELSE
+      -- Handle it old way
+      FOR _r IN SELECT DISTINCT invdetail_lotserial
+                FROM invdetail JOIN invhist ON (invdetail_invhist_id=invhist_id)
+               WHERE ((invhist_transtype=''SH'')
+                 AND  (invdetail_invcitem_id=pInvcitemid)) LOOP
+        IF ((_r.invdetail_lotserial IS NOT NULL) AND (_r.invdetail_lotserial != '''')) THEN
+          IF (_first = false) THEN
+            _lotserial := _lotserial || '', '';
+          END IF;
+          _lotserial := _lotserial || _r.invdetail_lotserial;
+          _first := false;
+        END IF;
+      END LOOP;
+
+      RETURN _lotserial;
+    END IF;
   ELSE
     RETURN '''';
   END IF;
+  
 END
 ' LANGUAGE 'plpgsql';
