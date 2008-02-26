38b7ecd5c3d72b6bf663429848babc8ae1736557
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-02-26 14:21:45 +0000

    issue #6461: Change character assignments to be drop down list on bomitems.

--- xtupleserver/trunk/updatescripts/from_231/alterBomitem.sql
+++ xtupleserver/trunk/updatescripts/from_231/alterBomitem.sql
@@ -1,6 +1,5 @@
 BEGIN;
 
-ALTER TABLE bomitem ADD COLUMN bomitem_char_id INTEGER REFERENCES char (char_id);
-ALTER TABLE bomitem ADD COLUMN bomitem_char_value TEXT;
+ALTER TABLE bomitem ADD COLUMN bomitem_charass_id INTEGER REFERENCES charass (charass_id);
 
 COMMIT;
