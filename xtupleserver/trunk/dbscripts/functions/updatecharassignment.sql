35f5796adb038c4833c7c9bc477cf8588d4b64d1
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-03-03 20:55:37 +0000

    issue #6461:  Correction so validation only happens if there is a change.

--- xtupleserver/trunk/dbscripts/functions/updatecharassignment.sql
+++ xtupleserver/trunk/dbscripts/functions/updatecharassignment.sql
@@ -24,30 +24,32 @@ DECLARE
   pPrice ALIAS FOR $5;
   _charassid INTEGER;
   _charassprice NUMERIC;
+  _explodedJob BOOLEAN = FALSE;
+  _value TEXT;
 
 BEGIN
 
   -- Check for Valid Assignment
   IF (pTargetType=''SI'') THEN
-    IF (SELECT (item_type=''J'' AND wo_status != ''O'')
+    SELECT (item_type=''J'' AND wo_status != ''O'') INTO _explodedJob
         FROM coitem,itemsite,item,wo
         WHERE ((coitem_id=pTargetId)
         AND (itemsite_id=coitem_itemsite_id)
         AND (item_id=itemsite_item_id)
         AND (wo_ordtype=''S'')
-        AND (wo_ordid=coitem_id)))  THEN
-      RAISE EXCEPTION  ''Characteristic may not be updated for Job Item with exploded Work Order.'';
-    END IF;
+        AND (wo_ordid=coitem_id));
   END IF;
   
-  SELECT charass_id INTO _charassid
+  SELECT charass_id,charass_value INTO _charassid, _value
     FROM charass
    WHERE ((charass_target_type=pTargetType)
      AND  (charass_target_id=pTargetId)
      AND  (charass_char_id=pCharId) )
    LIMIT 1;
   IF (FOUND) THEN
-    IF(COALESCE(pValue, '''')!='''') THEN
+    IF (_explodedJob AND pValue != _value) THEN
+      RAISE EXCEPTION  ''Characteristic may not be updated for Job Item with exploded Work Order.'';
+    ELSIF(COALESCE(pValue, '''')!='''') THEN
         UPDATE charass
         SET charass_value = pValue,
             charass_price = pPrice
@@ -59,7 +61,9 @@ BEGIN
       _charassid := 0;
     END IF;
   ELSE
-    IF(COALESCE(pValue, '''')!='''') THEN
+    IF (_explodedJob) THEN
+      RAISE EXCEPTION  ''Characteristics may not be updated for Job Item with exploded Work Order.'';
+    ELSIF(COALESCE(pValue, '''')!='''') THEN
       SELECT nextval(''charass_charass_id_seq'') INTO _charassid;
       INSERT INTO charass
             (charass_id, charass_target_type, charass_target_id,
