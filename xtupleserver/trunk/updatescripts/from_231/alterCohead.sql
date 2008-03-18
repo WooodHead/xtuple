16751abdf9e950275e830ac2f2feb12411be6fb9
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-03-18 20:14:10 +0000

    Fix casting problems for related to receipt posting.

---
+++ xtupleserver/trunk/updatescripts/from_231/alterCohead.sql
@@ -0,0 +1,13 @@
+BEGIN;
+
+-- Make the transition to pg 8.3 easier
+DROP VIEW api.salesorder;
+DROP VIEW api.salesorderline;
+DROP VIEW api.saleslinechar;
+DROP VIEW api.salesordercomment;
+DROP VIEW api.saleslinecomment;
+DROP VIEW orderhead;
+DROP VIEW billingeditlist;
+ALTER TABLE cohead ALTER COLUMN cohead_number TYPE text;
+
+COMMIT;
