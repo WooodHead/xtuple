b1292b2c56bd998bff7e54dc17d1be73c356aef3
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-03-18 16:44:19 +0000

    New Privs for maintianing scripts.

---
+++ xtupleserver/trunk/updatescripts/from_231/addPrivs.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO priv
+  (priv_module, priv_name, priv_descrip)
+VALUES
+  ('System', 'MaintainScripts',
+   'Can Change or Create Scripts that are executed by windows when they are opened.');
