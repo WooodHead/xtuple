3fab34cbe66221d09b55d2b15ac6fcde52fbc9c2
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-03-31 13:21:05 +0000

    Tagged release 2.3.2 for internal testing with intention of release.

---
+++ xtuple/tags/R2_3_2/guiclient/dspTimePhasedProductionByItem.cpp
@@ -0,0 +1,247 @@
+/*
+ * Common Public Attribution License Version 1.0. 
+ * 
+ * The contents of this file are subject to the Common Public Attribution 
+ * License Version 1.0 (the "License"); you may not use this file except 
+ * in compliance with the License. You may obtain a copy of the License 
+ * at http://www.xTuple.com/CPAL.  The License is based on the Mozilla 
+ * Public License Version 1.1 but Sections 14 and 15 have been added to 
+ * cover use of software over a computer network and provide for limited 
+ * attribution for the Original Developer. In addition, Exhibit A has 
+ * been modified to be consistent with Exhibit B.
+ * 
+ * Software distributed under the License is distributed on an "AS IS" 
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See 
+ * the License for the specific language governing rights and limitations 
+ * under the License. 
+ * 
+ * The Original Code is PostBooks Accounting, ERP, and CRM Suite. 
+ * 
+ * The Original Developer is not the Initial Developer and is __________. 
+ * If left blank, the Original Developer is the Initial Developer. 
+ * The Initial Developer of the Original Code is OpenMFG, LLC, 
+ * d/b/a xTuple. All portions of the code written by xTuple are Copyright 
+ * (c) 1999-2007 OpenMFG, LLC, d/b/a xTuple. All Rights Reserved. 
+ * 
+ * Contributor(s): ______________________.
+ * 
+ * Alternatively, the contents of this file may be used under the terms 
+ * of the xTuple End-User License Agreeement (the xTuple License), in which 
+ * case the provisions of the xTuple License are applicable instead of 
+ * those above.  If you wish to allow use of your version of this file only 
+ * under the terms of the xTuple License and not to allow others to use 
+ * your version of this file under the CPAL, indicate your decision by 
+ * deleting the provisions above and replace them with the notice and other 
+ * provisions required by the xTuple License. If you do not delete the 
+ * provisions above, a recipient may use your version of this file under 
+ * either the CPAL or the xTuple License.
+ * 
+ * EXHIBIT B.  Attribution Information
+ * 
+ * Attribution Copyright Notice: 
+ * Copyright (c) 1999-2007 by OpenMFG, LLC, d/b/a xTuple
+ * 
+ * Attribution Phrase: 
+ * Powered by PostBooks, an open source solution from xTuple
+ * 
+ * Attribution URL: www.xtuple.org 
+ * (to be included in the "Community" menu of the application if possible)
+ * 
+ * Graphic Image as provided in the Covered Code, if any. 
+ * (online at www.xtuple.com/poweredby)
+ * 
+ * Display of Attribution Information is required in Larger Works which 
+ * are defined in the CPAL as a work which combines Covered Code or 
+ * portions thereof with code not governed by the terms of the CPAL.
+ */
+
+#include "dspTimePhasedProductionByItem.h"
+
+#include <QVariant>
+#include <QStatusBar>
+#include <QWorkspace>
+#include <QMessageBox>
+#include <QMenu>
+#include <openreports.h>
+#include <datecluster.h>
+#include "dspInventoryHistoryByItem.h"
+#include "OpenMFGGUIClient.h"
+#include "submitReport.h"
+
+/*
+ *  Constructs a dspTimePhasedProductionByItem as a child of 'parent', with the
+ *  name 'name' and widget flags set to 'f'.
+ *
+ */
+dspTimePhasedProductionByItem::dspTimePhasedProductionByItem(QWidget* parent, const char* name, Qt::WFlags fl)
+    : QMainWindow(parent, name, fl)
+{
+  setupUi(this);
+
+  (void)statusBar();
+
+  // signals and slots connections
+  connect(_query, SIGNAL(clicked()), this, SLOT(sCalculate()));
+  connect(_print, SIGNAL(clicked()), this, SLOT(sPrint()));
+  connect(_submit, SIGNAL(clicked()), this, SLOT(sSubmit()));
+  connect(_production, SIGNAL(populateMenu(QMenu*,QTreeWidgetItem*,int)), this, SLOT(sPopulateMenu(QMenu*,QTreeWidgetItem*,int)));
+  connect(_close, SIGNAL(clicked()), this, SLOT(close()));
+  connect(_calendar, SIGNAL(newCalendarId(int)), _periods, SLOT(populate(int)));
+  connect(_calendar, SIGNAL(select(ParameterList&)), _periods, SLOT(load(ParameterList&)));
+
+  _plannerCode->setType(PlannerCode);
+
+  _production->addColumn(tr("Item Number"), _itemColumn, Qt::AlignLeft   );
+  _production->addColumn(tr("Whs."),        _whsColumn,  Qt::AlignCenter );
+  _production->addColumn(tr("UOM"),         _uomColumn,  Qt::AlignLeft   );
+
+  if (!_metrics->boolean("EnableBatchManager"))
+    _submit->hide();
+}
+
+/*
+ *  Destroys the object and frees any allocated resources
+ */
+dspTimePhasedProductionByItem::~dspTimePhasedProductionByItem()
+{
+  // no need to delete child widgets, Qt does it all for us
+}
+
+/*
+ *  Sets the strings of the subwidgets using the current
+ *  language.
+ */
+void dspTimePhasedProductionByItem::languageChange()
+{
+    retranslateUi(this);
+}
+
+void dspTimePhasedProductionByItem::sPrint()
+{
+  if (_periods->isPeriodSelected())
+  {
+    orReport report("TimePhasedProductionByItem", buildParameters());
+    if (report.isValid())
+      report.print();
+    else
+    {
+      report.reportError(this);
+      return;
+    }
+  }
+  else
+    QMessageBox::critical( this, tr("Incomplete criteria"),
+                           tr( "The criteria you specified is not complete. Please make sure all\n"
+                               "fields are correctly filled out before running the report." ) );
+}
+
+void dspTimePhasedProductionByItem::sSubmit()
+{
+  if (_periods->isPeriodSelected())
+  {
+    ParameterList params(buildParameters());
+    params.append("report_name", "TimePhasedProductionByItem");
+
+    submitReport newdlg(this, "", TRUE);
+    newdlg.set(params);
+
+    if (newdlg.check() == cNoReportDefinition)
+      QMessageBox::critical( this, tr("Report Definition Not Found"),
+                             tr( "The report defintions for this report, \"TimePhasedProductionByItem\" cannot be found.\n"
+                                 "Please contact your Systems Administrator and report this issue." ) );
+    else
+      newdlg.exec();
+  }
+  else
+    QMessageBox::critical( this, tr("Incomplete criteria"),
+                           tr( "The criteria you specified is not complete. Please make sure all\n"
+                               "fields are correctly filled out before running the report." ) );
+}
+
+ParameterList dspTimePhasedProductionByItem::buildParameters()
+{
+  ParameterList params;
+
+  _plannerCode->appendValue(params);
+  _warehouse->appendValue(params);
+
+  QList<QTreeWidgetItem*> selected = _periods->selectedItems();
+  QList<QVariant> periodList;
+  for (int i = 0; i < selected.size(); i++)
+    periodList.append(((XTreeWidgetItem*)selected[i])->id());
+  params.append("period_id_list", periodList);
+
+  return params;
+}
+
+void dspTimePhasedProductionByItem::sViewTransactions()
+{
+  ParameterList params;
+  params.append("itemsite_id", _production->id());
+  params.append("startDate", _columnDates[_column - 3].startDate);
+  params.append("endDate", _columnDates[_column - 3].endDate);
+  params.append("transtype", "R");
+  params.append("run");
+  _warehouse->appendValue(params);
+
+  dspInventoryHistoryByItem *newdlg = new dspInventoryHistoryByItem();
+  newdlg->set(params);
+  omfgThis->handleNewWindow(newdlg);
+}
+
+void dspTimePhasedProductionByItem::sPopulateMenu(QMenu *menu, QTreeWidgetItem *, int pColumn)
+{
+  int menuItem;
+
+  _column = pColumn;
+  if (_column > 1)
+  {
+    menuItem = menu->insertItem(tr("View Transactions..."), this, SLOT(sViewTransactions()), 0);
+    if (!_privleges->check("ViewInventoryHistory"))
+      menu->setItemEnabled(menuItem, FALSE);
+  }
+}
+
+void dspTimePhasedProductionByItem::sCalculate()
+{
+  _columnDates.clear();
+  _production->setColumnCount(3);
+
+  QString sql("SELECT itemsite_id, item_number, warehous_code, uom_name");
+
+  int columns = 1;
+  QList<QTreeWidgetItem*> selected = _periods->selectedItems();
+  for (int i = 0; i < selected.size(); i++)
+  {
+    PeriodListViewItem *cursor = (PeriodListViewItem*)selected[i];
+    sql += QString(", formatQty(summProd(itemsite_id, %2)) AS bucket%1")
+	   .arg(columns++)
+	   .arg(cursor->id());
+
+    _production->addColumn(formatDate(cursor->startDate()), _qtyColumn, Qt::AlignRight);
+    _columnDates.append(DatePair(cursor->startDate(), cursor->endDate()));
+  }
+
+  sql += " FROM itemsite, item, uom, warehous "
+         "WHERE ((itemsite_item_id=item_id)"
+         " AND (item_inv_uom_id=uom_id)"
+         " AND (itemsite_warehous_id=warehous_id)";
+
+  if (_warehouse->isSelected())
+    sql += " AND (itemsite_warehous_id=:warehous_id)";
+ 
+  if (_plannerCode->isSelected())
+    sql += "AND (itemsite_plancode_id=:plancode_id)";
+  else if (_plannerCode->isPattern())
+    sql += "AND (itemsite_plancode_id IN (SELECT plancode_id FROM plancode WHERE (plancode_code ~ :plancode_pattern))) ";
+
+  sql += ") "
+         "ORDER BY item_number;";
+
+  q.prepare(sql);
+  _warehouse->bindValue(q);
+  _plannerCode->bindValue(q);
+  q.exec();
+  _production->populate(q);
+}
+
