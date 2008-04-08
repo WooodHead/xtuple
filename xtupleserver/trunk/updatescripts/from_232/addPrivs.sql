170a2cfac4ff3c7027899875a478cd1db83bd6b4
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-04-08 21:03:55 +0000

    Added database structures for uiforms.

--- xtupleserver/trunk/updatescripts/from_232/addPrivs.sql
+++ xtupleserver/trunk/updatescripts/from_232/addPrivs.sql
@@ -4,3 +4,10 @@ INSERT INTO priv
 VALUES
   ('System', 'MaintainScripts',
    'Can Change or Create Scripts that are executed by windows when they are opened.');
+
+INSERT INTO priv
+  (priv_module, priv_name, priv_descrip)
+VALUES
+  ('System', 'MaintainUIForms',
+   'Can Change or Create UI Forms that are executed by custom windows when they are opened.');
+
