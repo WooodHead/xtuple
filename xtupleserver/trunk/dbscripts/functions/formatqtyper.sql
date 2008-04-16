d6e46b1d1b9684af0e309b574389515d5e08748c
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-04-16 18:03:27 +0000

    table model/view - partially address bug #3308 by moving locale-checking from xtreewidget to format.cpp and changing many of the format* stored procedures to get scale from locale; change bom.cpp to use the new xtreewidget changes and added requisite support to the xtreewidget to help make the conversion more complete (xtnullrole and row-wide qt roles)

--- xtupleserver/trunk/dbscripts/functions/formatqtyper.sql
+++ xtupleserver/trunk/dbscripts/functions/formatqtyper.sql
@@ -1,8 +1,20 @@
-
 CREATE OR REPLACE FUNCTION formatQtyPer(NUMERIC) RETURNS TEXT IMMUTABLE AS '
-SELECT LTRIM(TO_CHAR(COALESCE($1, 0), ( SELECT locale_qtyperformat
-                                        FROM locale, usr
-                                        WHERE ( (usr_locale_id=locale_id)
-                                         AND (usr_username=CURRENT_USER) ) ) ), '' '') AS result
-' LANGUAGE 'sql';
+DECLARE
+  _fmt          TEXT := ''FM999G999G999G990D'';
+  _counter      INTEGER;
+  _scale        INTEGER;
+BEGIN
+  SELECT locale_qtyper_scale INTO _scale
+  FROM locale, usr
+  WHERE ((usr_locale_id=locale_id)
+     AND (usr_username=CURRENT_USER));
+  IF (NOT FOUND OR _scale IS NULL) THEN
+    _scale := 2;
+  END IF;
 
+  FOR _counter IN 1 .. _scale LOOP
+    _fmt := _fmt || ''0'';
+  END LOOP;
+
+  RETURN TO_CHAR(COALESCE($1, 0), _fmt);
+END;' LANGUAGE 'plpgsql';
