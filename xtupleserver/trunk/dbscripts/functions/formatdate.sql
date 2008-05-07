2ada0c7814adea32c5504bac8ee74427fe33a5ff
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-05-07 15:45:16 +0000

    supply a fallback date format until the entire app is migrated away from using this proc

--- xtupleserver/trunk/dbscripts/functions/formatdate.sql
+++ xtupleserver/trunk/dbscripts/functions/formatdate.sql
@@ -1,17 +1,19 @@
 
 CREATE OR REPLACE FUNCTION formatDate(TIMESTAMP WITH TIME ZONE) RETURNS TEXT IMMUTABLE AS '
-SELECT TO_CHAR($1, ( SELECT locale_dateformat
-                     FROM locale, usr
-                     WHERE ((usr_locale_id=locale_id)
-                      AND (usr_username=CURRENT_USER)) ) ) AS result
+SELECT TO_CHAR($1, COALESCE((SELECT locale_dateformat
+                             FROM locale, usr
+                             WHERE ((usr_locale_id=locale_id)
+                              AND (usr_username=CURRENT_USER)) ),
+                            ''yyyy-mm-dd'' )) AS result
 ' LANGUAGE 'sql';
 
 
 CREATE OR REPLACE FUNCTION formatDate(DATE) RETURNS TEXT IMMUTABLE AS '
-SELECT TO_CHAR($1, ( SELECT locale_dateformat
-                     FROM locale, usr
-                     WHERE ((usr_locale_id=locale_id)
-                      AND (usr_username=CURRENT_USER)) ) ) AS result
+SELECT TO_CHAR($1, COALESCE((SELECT locale_dateformat
+                             FROM locale, usr
+                             WHERE ((usr_locale_id=locale_id)
+                              AND (usr_username=CURRENT_USER)) ),
+                            ''yyyy-mm-dd'') ) AS result
 ' LANGUAGE 'sql';
 
 CREATE OR REPLACE FUNCTION formatDate(DATE, TEXT) RETURNS TEXT IMMUTABLE AS '
