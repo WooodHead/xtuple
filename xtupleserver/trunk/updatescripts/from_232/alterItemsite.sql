f526575608cf3a73596a8ce2a23fc8465e7540a9
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-05-13 15:44:54 +0000

    Modified constraint check to include T type for ABC classes. Issue #6623.

--- xtupleserver/trunk/updatescripts/from_232/alterItemsite.sql
+++ xtupleserver/trunk/updatescripts/from_232/alterItemsite.sql
@@ -3,4 +3,7 @@ BEGIN;
 ALTER TABLE itemsite ADD COLUMN itemsite_warrpurc BOOLEAN NOT NULL DEFAULT FALSE;
 ALTER TABLE itemsite ADD COLUMN itemsite_autoreg BOOLEAN DEFAULT FALSE;
 
+ALTER TABLE itemsite DROP CONSTRAINT itemsite_itemsite_abcclass_check;
+ALTER TABLE itemsite ADD CHECK (itemsite_abcclass IN ('A','B','C','T'));
+
 COMMIT;
