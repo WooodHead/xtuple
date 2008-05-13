f4c2c20fa8d40d2fd33166d493351c7fa29fa51e
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-05-13 20:50:41 +0000

    Added in submitted api views for invoice and invoiceline from dmiller. Issue #6583.

--- xtupleserver/trunk/updatescripts/from_232/alterInvchead.sql
+++ xtupleserver/trunk/updatescripts/from_232/alterInvchead.sql
@@ -4,4 +4,6 @@ BEGIN;
 ALTER TABLE invchead ALTER COLUMN invchead_invcnumber TYPE text;
 ALTER TABLE invchead ALTER COLUMN invchead_ordernumber TYPE text;
 
+ALTER TABLE invchead ADD CONSTRAINT invchead_invcnumber_unique UNIQUE (invchead_invcnumber);
+
 COMMIT;
