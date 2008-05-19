6cd496ac2183bb9fd655d2e57aefdfd99d3f3f4e
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-05-19 16:26:36 +0000

    issue #6548:  Reverse out change that has to be done in different version.

--- xtupleserver/trunk/updatescripts/from_232/alterItemloc.sql
+++ xtupleserver/trunk/updatescripts/from_232/alterItemloc.sql
@@ -2,6 +2,5 @@ BEGIN;
 
 ALTER TABLE itemloc ADD COLUMN itemloc_ls_id INTEGER;
 ALTER TABLE itemloc ALTER COLUMN itemloc_lotserial DROP NOT NULL;
-ALTER TABLE itemloc ADD COLUMN itemloc_warrpurc DATE;
 
 COMMIT;
