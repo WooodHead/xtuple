8397bad033846d8e80d485a0418a8b1bdcb2579f
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-05-19 15:54:35 +0000

    issue #6548:  purchase warranty data for loc/lot serial control.

--- xtupleserver/trunk/updatescripts/from_232/alterItemloc.sql
+++ xtupleserver/trunk/updatescripts/from_232/alterItemloc.sql
@@ -2,5 +2,6 @@ BEGIN;
 
 ALTER TABLE itemloc ADD COLUMN itemloc_ls_id INTEGER;
 ALTER TABLE itemloc ALTER COLUMN itemloc_lotserial DROP NOT NULL;
+ALTER TABLE itemloc ADD COLUMN itemloc_warrpurc DATE;
 
 COMMIT;
