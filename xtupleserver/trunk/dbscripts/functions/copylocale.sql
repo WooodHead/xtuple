05feaa56e6e891108a71b86b57a9d8f63234add8
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-05-22 13:37:16 +0000

    Fixed some problems with a locale format function. Issue #6758. Also fixed the copy locale function.

--- xtupleserver/trunk/dbscripts/functions/copylocale.sql
+++ xtupleserver/trunk/dbscripts/functions/copylocale.sql
@@ -1,31 +1,83 @@
 
-CREATE OR REPLACE FUNCTION copyLocale(INTEGER) RETURNS INTEGER AS '
+CREATE OR REPLACE FUNCTION copyLocale(INTEGER) RETURNS INTEGER AS $$
 DECLARE
   pLocaleid ALIAS FOR $1;
   _localeid INTEGER;
 
 BEGIN
 
-  SELECT NEXTVAL(''locale_locale_id_seq'') INTO _localeid;
+  SELECT NEXTVAL('locale_locale_id_seq') INTO _localeid;
 
   INSERT INTO locale
-  ( locale_id, locale_code, locale_descrip, locale_lang,
-    locale_qtyformat, locale_qtyperformat,
-    locale_currformat, locale_salespriceformat, locale_purchpriceformat, locale_extpriceformat,
-    locale_dateformat, locale_timeformat, locale_timestampformat,
-    locale_uomratioformat, locale_intervalformat,
-    locale_costformat, locale_comments )
-  SELECT _localeid, '''', '''', locale_lang,
-         locale_qtyformat, locale_qtyperformat,
-         locale_currformat, locale_salespriceformat, locale_purchpriceformat, locale_extpriceformat,
-         locale_dateformat, locale_timeformat, locale_timestampformat,
-         locale_uomratioformat, locale_intervalformat,
-         locale_costformat, locale_comments
-  FROM locale
-  WHERE (locale_id=pLocaleid);
+        (locale_id, locale_code, locale_descrip,
+         locale_lang_file,
+         locale_dateformat,
+         locale_currformat,
+         locale_qtyformat,
+         locale_comments,
+         locale_qtyperformat,
+         locale_salespriceformat,
+         locale_extpriceformat,
+         locale_timeformat,
+         locale_timestampformat,
+         local_costformat,
+         locale_costformat,
+         locale_purchpriceformat,
+         locale_uomratioformat,
+         locale_intervalformat,
+         locale_lang_id,
+         locale_country_id,
+         locale_error_color,
+         locale_warning_color,
+         locale_emphasis_color,
+         locale_altemphasis_color,
+         locale_expired_color,
+         locale_future_color,
+         locale_curr_scale,
+         locale_salesprice_scale,
+         locale_purchprice_scale,
+         locale_extprice_scale,
+         locale_cost_scale,
+         locale_qty_scale,
+         locale_qtyper_scale,
+         locale_uomratio_scale)
+  SELECT _localeid, '', '',
+         locale_lang_file,
+         locale_dateformat,
+         locale_currformat,
+         locale_qtyformat,
+         locale_comments,
+         locale_qtyperformat,
+         locale_salespriceformat,
+         locale_extpriceformat,
+         locale_timeformat,
+         locale_timestampformat,
+         local_costformat,
+         locale_costformat,
+         locale_purchpriceformat,
+         locale_uomratioformat,
+         locale_intervalformat,
+         locale_lang_id,
+         locale_country_id,
+         locale_error_color,
+         locale_warning_color,
+         locale_emphasis_color,
+         locale_altemphasis_color,
+         locale_expired_color,
+         locale_future_color,
+         locale_curr_scale,
+         locale_salesprice_scale,
+         locale_purchprice_scale,
+         locale_extprice_scale,
+         locale_cost_scale,
+         locale_qty_scale,
+         locale_qtyper_scale,
+         locale_uomratio_scale
+    FROM locale
+   WHERE(locale_id=pLocaleid);
 
   RETURN _localeid;
 
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
 
--- xtupleserver/trunk/dbscripts/functions/formatprice.sql
+++ xtupleserver/trunk/dbscripts/functions/formatprice.sql
@@ -1,8 +1,4 @@
 
-CREATE OR REPLACE FUNCTION formatPrice(NUMERIC) RETURNS TEXT IMMUTABLE AS '
-  SELECT LTRIM(TO_CHAR($1, ( SELECT locale_salespriceformat
-                             FROM locale, usr
-                             WHERE ( (usr_locale_id=locale_id)
-                              AND (usr_username=CURRENT_USER) ) ) ), '' '') AS result;
-' LANGUAGE 'sql';
-
+CREATE OR REPLACE FUNCTION formatPrice(NUMERIC) RETURNS TEXT IMMUTABLE AS $$
+  SELECT formatSalesPrice($1) AS result;
+$$ LANGUAGE 'sql';
