e86d2489d731a8de6c902a69da1f516431181b25
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-06-05 13:55:50 +0000

    fixes bug #4997 - add shipment number to shipdata and shipdatasum tables. as long as the xtuple-assigned shipment number is entered in worldship and mapped to the correct field then ship order should retrieve the correct tracking number. also added a maintenance window for shipdata to help correct problems that may arise.

--- xtupleserver/trunk/dbscripts/triggers/shipdata.sql
+++ xtupleserver/trunk/dbscripts/triggers/shipdata.sql
@@ -1,15 +1,12 @@
 CREATE OR REPLACE FUNCTION _shipdatatrigger() RETURNS TRIGGER AS '
 DECLARE
-  _oldShipdata_cohead_number TEXT;
   _newShipdata_cohead_number INTEGER;
   _shipdatasum_shipper TEXT;
+  _rows			INTEGER;
 
 BEGIN
-
-  _oldShipdata_cohead_number := trim(both '' '' from NEW.shipdata_cohead_number);
-  _newShipdata_cohead_number := cast(_oldShipdata_cohead_number AS INTEGER);
-
---  This is where the shipper is identified and may need to be changed
+  --  This is where the shipper is identified and may need to be changed
+  NEW.shipdata_cohead_number := TRIM(NEW.shipdata_cohead_number);
 
   IF (substring(NEW.shipdata_cosmisc_tracknum from 1 for 2) = ''1Z'') THEN
     _shipdatasum_shipper := ''UPS'';
@@ -17,26 +14,67 @@ BEGIN
     _shipdatasum_shipper := ''UNKNOWN'';
   END IF;
 
+  IF (LENGTH(TRIM(NEW.shipdata_shiphead_number)) = 0) THEN
+    NEW.shipdata_shiphead_number := NULL;
+  END IF;
+
   IF (NEW.shipdata_cosmisc_tracknum = NEW.shipdata_cosmisc_packnum_tracknum) THEN
-     IF (NEW.shipdata_void_ind = ''Y'') THEN
---  Delete the current shipdatasum
-       DELETE FROM shipdatasum
-        WHERE ((shipdatasum_cohead_number = _newShipdata_cohead_number)
-          AND  (shipdatasum_cosmisc_tracknum = NEW.shipdata_cosmisc_tracknum));
-     ELSE
---  Insert the current shipdatasum
-       INSERT INTO shipdatasum
-              (shipdatasum_cohead_number, shipdatasum_cosmisc_tracknum,
-               shipdatasum_cosmisc_packnum_tracknum, shipdatasum_weight,
-               shipdatasum_base_freight, shipdatasum_total_freight,
-               shipdatasum_shipper, shipdatasum_billing_option,
-               shipdatasum_package_type)
-       VALUES (_newShipdata_cohead_number, NEW.shipdata_cosmisc_tracknum,
-               NEW.shipdata_cosmisc_packnum_tracknum, NEW.shipdata_weight,
-               NEW.shipdata_base_freight, NEW.shipdata_total_freight,
-               _shipdatasum_shipper, NEW.shipdata_billing_option,
-               NEW.shipdata_package_type);
-     END IF;
+    IF (NEW.shipdata_void_ind = ''Y'') THEN
+      --  Delete the current shipdatasum
+      DELETE FROM shipdatasum
+      WHERE ((shipdatasum_cohead_number = NEW.shipdata_cohead_number)
+	AND  (shipdatasum_cosmisc_tracknum = NEW.shipdata_cosmisc_tracknum));
+
+    ELSIF (TG_OP = ''INSERT'') THEN
+      INSERT INTO shipdatasum
+	      (shipdatasum_cohead_number, shipdatasum_cosmisc_tracknum,
+	       shipdatasum_cosmisc_packnum_tracknum, shipdatasum_weight,
+	       shipdatasum_base_freight, shipdatasum_total_freight,
+	       shipdatasum_base_freight_curr_id, shipdatasum_total_freight_curr_id,
+	       shipdatasum_shipper, shipdatasum_billing_option,
+	       shipdatasum_package_type, shipdatasum_shiphead_number)
+       VALUES (NEW.shipdata_cohead_number, NEW.shipdata_cosmisc_tracknum,
+	       NEW.shipdata_cosmisc_packnum_tracknum, NEW.shipdata_weight,
+	       NEW.shipdata_base_freight, NEW.shipdata_total_freight,
+	       NEW.shipdata_base_freight_curr_id, NEW.shipdata_total_freight_curr_id,
+	       _shipdatasum_shipper, NEW.shipdata_billing_option,
+	       NEW.shipdata_package_type, NEW.shipdata_shiphead_number);
+
+    ELSIF (TG_OP = ''UPDATE'') THEN
+       UPDATE shipdatasum SET
+	      shipdatasum_cohead_number=NEW.shipdata_cohead_number,
+	      shipdatasum_cosmisc_tracknum=NEW.shipdata_cosmisc_tracknum,
+	      shipdatasum_cosmisc_packnum_tracknum=NEW.shipdata_cosmisc_packnum_tracknum,
+	      shipdatasum_weight=NEW.shipdata_weight,
+	      shipdatasum_base_freight=NEW.shipdata_base_freight,
+	      shipdatasum_total_freight=NEW.shipdata_total_freight,
+	      shipdatasum_base_freight_curr_id=NEW.shipdata_base_freight_curr_id,
+	      shipdatasum_total_freight_curr_id=NEW.shipdata_total_freight_curr_id,
+	      shipdatasum_shipper=_shipdatasum_shipper,
+	      shipdatasum_billing_option=NEW.shipdata_billing_option,
+	      shipdatasum_package_type=NEW.shipdata_package_type,
+	      shipdatasum_shiphead_number=NEW.shipdata_shiphead_number
+       WHERE ((shipdatasum_cohead_number=OLD.shipdata_cohead_number)
+	  AND (shipdatasum_cosmisc_tracknum=OLD.shipdata_cosmisc_tracknum)
+	  AND (shipdatasum_cosmisc_packnum_tracknum=OLD.shipdata_cosmisc_packnum_tracknum));
+
+       GET DIAGNOSTICS _rows = ROW_COUNT;
+       IF (_rows <= 0) THEN
+	 INSERT INTO shipdatasum
+		(shipdatasum_cohead_number, shipdatasum_cosmisc_tracknum,
+		 shipdatasum_cosmisc_packnum_tracknum, shipdatasum_weight,
+		 shipdatasum_base_freight, shipdatasum_total_freight,
+		 shipdatasum_base_freight_curr_id, shipdatasum_total_freight_curr_id,
+		 shipdatasum_shipper, shipdatasum_billing_option,
+		 shipdatasum_package_type, shipdatasum_shiphead_number)
+	 VALUES (NEW.shipdata_cohead_number, NEW.shipdata_cosmisc_tracknum,
+		 NEW.shipdata_cosmisc_packnum_tracknum, NEW.shipdata_weight,
+		 NEW.shipdata_base_freight, NEW.shipdata_total_freight,
+		 NEW.shipdata_base_freight_curr_id, NEW.shipdata_total_freight_curr_id,
+		 _shipdatasum_shipper, NEW.shipdata_billing_option,
+		 NEW.shipdata_package_type, NEW.shipdata_shiphead_number);
+       END IF;
+    END IF;
   END IF;
 
   RETURN NEW;
@@ -45,5 +83,5 @@ END;
 '
   LANGUAGE 'plpgsql';
 
-DROP TRIGGER shipdatatrigger ON shipdata;
+SELECT dropIfExists('TRIGGER', 'shipdatatrigger');
 CREATE TRIGGER shipdatatrigger BEFORE INSERT OR UPDATE ON shipdata FOR EACH ROW EXECUTE PROCEDURE _shipdatatrigger();
