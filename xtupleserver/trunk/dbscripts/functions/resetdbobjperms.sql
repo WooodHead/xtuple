e1ffd67339858be6def3f84b11e1a417ce9b810c
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-06-11 12:55:55 +0000

    change ownership as well as privs, avoid ambiguity by specifying schema as well as relname, and handle views

--- xtupleserver/trunk/dbscripts/functions/resetdbobjperms.sql
+++ xtupleserver/trunk/dbscripts/functions/resetdbobjperms.sql
@@ -2,6 +2,7 @@ CREATE OR REPLACE FUNCTION resetDBObjPerms(TEXT) RETURNS INTEGER AS '
 DECLARE
   pObjname      ALIAS FOR $1;
 BEGIN
+  EXECUTE ''ALTER TABLE ''   || pObjname || '' OWNER TO '' || CURRENT_USER || '';'';
   EXECUTE ''REVOKE ALL ON '' || pObjname || '' FROM PUBLIC;'';
   EXECUTE ''GRANT  ALL ON '' || pObjname || '' TO GROUP openmfg;'';
   RETURN 1;
@@ -12,9 +13,9 @@ CREATE OR REPLACE FUNCTION resetDBObjPerms() RETURNS INTEGER AS '
 DECLARE
   _count        INTEGER := 0;
 BEGIN
-  SELECT SUM(resetDBObjPerms(relname)) INTO _count
+  SELECT SUM(resetDBObjPerms(nspname || ''.'' || relname)) INTO _count
   FROM pg_catalog.pg_class, pg_catalog.pg_namespace
-  WHERE (relkind IN (''r'', ''S'')
+  WHERE (relkind IN (''r'', ''S'', ''v'')
     AND  (relnamespace=pg_namespace.oid)
     AND  (nspname IN (''public'', ''api'')));
 
