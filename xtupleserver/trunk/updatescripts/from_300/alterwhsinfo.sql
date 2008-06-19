9fde0b0192fdd3171035f3bde842eaea629cbb41
ghartnett <ghartnett@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-06-19 14:40:58 +0000

    issue #6816:first pass at new site/sitezone/location api views

---
+++ xtupleserver/trunk/updatescripts/from_300/alterwhsinfo.sql
@@ -0,0 +1,6 @@
+BEGIN;
+
+ALTER TABLE whsinfo add constraint whsinfo_warehous_cntct_id_fkey foreign key (warehous_cntct_id) references cntct (cntct_id); 
+ALTER TABLE whsinfo add constraint whsinfo_warehous_accnt_id_fkey foreign key (warehous_default_accnt_id) references accnt (accnt_id); 
+
+COMMIT;
