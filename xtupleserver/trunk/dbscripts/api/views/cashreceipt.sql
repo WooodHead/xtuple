d45d01bab86feb01e0c11f4066f2225d19cccd62
ghartnett <ghartnett@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-06-25 19:09:13 +0000

    issue #6727:first pass for cashreceipt/cashreceiptapply/cashreceiptapplymisc views

---
+++ xtupleserver/trunk/dbscripts/api/views/cashreceipt.sql
@@ -0,0 +1,221 @@
+BEGIN;
+
+CREATE OR REPLACE VIEW api.cashreceipt AS
+  SELECT
+    cust_number AS customer_number,
+    cust_name AS customer_name,
+    cust_address1 AS customer_address,
+    curr_abbr AS currency,
+    cashrcpt_amount AS amount_received,
+    CASE
+      WHEN cashrcpt_fundstype='C' THEN
+        'Check'
+      WHEN cashrcpt_fundstype='T' THEN
+        'Certified Check'
+      WHEN cashrcpt_fundstype='M' THEN
+        'Master Card'
+      WHEN cashrcpt_fundstype='V' THEN
+        'Visa'
+      WHEN cashrcpt_fundstype='A' THEN
+        'American Express'
+      WHEN cashrcpt_fundstype='D' THEN
+        'Discover Card'
+      WHEN cashrcpt_fundstype='R' THEN
+        'Other Credit Card'
+      WHEN cashrcpt_fundstype='K' THEN
+        'Cash'
+      WHEN cashrcpt_fundstype='W' THEN
+        'Wire Transfer'
+      WHEN cashrcpt_fundstype='O' THEN
+        'Other'
+    END AS funds_type,
+    cashrcpt_docnumber AS check_document_number,
+    bankaccnt_name AS post_to,
+    formatDate(cashrcpt_distdate) AS distribution_date,
+    CASE
+      WHEN cashrcpt_usecustdeposit THEN
+        'Customer Deposit'
+      ELSE
+        'Credit Memo'
+    END AS apply_balance_as,
+    salescat_name AS sales_category,
+    cashrcpt_notes AS notes
+  FROM cashrcpt
+    LEFT OUTER JOIN cust ON (cust_id=cashrcpt_cust_id)
+    LEFT OUTER JOIN curr_symbol ON (curr_id=cashrcpt_curr_id)
+    LEFT OUTER JOIN bankaccnt ON (bankaccnt_id=cashrcpt_bankaccnt_id)
+    LEFT OUTER JOIN salescat ON (salescat_id=cashrcpt_salescat_id);
+	
+GRANT ALL ON TABLE api.cashreceipt TO openmfg;
+COMMENT ON VIEW api.cashreceipt IS '
+This view can be used as an interface to import Cash Receipt data directly  
+into the system.  Required fields will be checked and default values will be 
+populated';
+
+--Rules
+
+CREATE OR REPLACE RULE "_INSERT" AS
+  ON INSERT TO api.cashreceipt DO INSTEAD
+
+  INSERT INTO cashrcpt (
+    cashrcpt_cust_id,
+    cashrcpt_amount,
+    cashrcpt_fundstype,
+    cashrcpt_docnumber,
+    cashrcpt_bankaccnt_id,
+    cashrcpt_notes,
+    cashrcpt_distdate,
+    cashrcpt_salescat_id,
+    cashrcpt_curr_id,
+    cashrcpt_usecustdeposit
+    )
+  VALUES (
+    getCustId(NEW.customer_number),
+    COALESCE(NEW.amount_received, 0),
+    CASE
+      WHEN NEW.funds_type='Check' THEN
+        'C'
+      WHEN NEW.funds_type='Certified Check' THEN
+        'T'
+      WHEN NEW.funds_type='Master Card' THEN
+        'M'
+      WHEN NEW.funds_type='Visa' THEN
+        'V'
+      WHEN NEW.funds_type='American Express' THEN
+        'A'
+      WHEN NEW.funds_type='Discover Card' THEN
+        'D'
+      WHEN NEW.funds_type='Other Credit Card' THEN
+        'R'
+      WHEN NEW.funds_type='Cash' THEN
+        'K'
+      WHEN NEW.funds_type='Wire Transfer' THEN
+        'W'
+      ELSE
+        'O'
+    END,
+    COALESCE(NEW.check_document_number, ''),
+    getBankAccntId(NEW.post_to),
+    COALESCE(NEW.notes, ''),
+    CASE
+      WHEN NEW.distribution_date > '' THEN
+        NEW.distribution_date::DATE
+      ELSE
+        now()
+    END,
+    COALESCE(getSalesCatId(NEW.sales_category), -1),
+    getCurrId(NEW.currency),
+    CASE
+      WHEN NEW.apply_balance_as='Customer Deposit' THEN
+        true
+      ELSE
+        false
+    END
+    );
+
+CREATE OR REPLACE RULE "_UPDATE" AS 
+  ON UPDATE TO api.cashreceipt DO INSTEAD
+
+  UPDATE cashrcpt SET
+    cashrcpt_amount=NEW.amount_received,
+    cashrcpt_fundstype=
+      CASE
+        WHEN NEW.funds_type='Check' THEN
+          'C'
+        WHEN NEW.funds_type='Certified Check' THEN
+          'T'
+        WHEN NEW.funds_type='Master Card' THEN
+          'M'
+        WHEN NEW.funds_type='Visa' THEN
+          'V'
+        WHEN NEW.funds_type='American Express' THEN
+          'A'
+        WHEN NEW.funds_type='Discover Card' THEN
+          'D'
+        WHEN NEW.funds_type='Other Credit Card' THEN
+          'R'
+        WHEN NEW.funds_type='Cash' THEN
+          'K'
+        WHEN NEW.funds_type='Wire Transfer' THEN
+          'W'
+        ELSE
+          'O'
+      END,
+    cashrcpt_docnumber=NEW.check_document_number,
+    cashrcpt_bankaccnt_id=getBankAccntId(NEW.post_to),
+    cashrcpt_notes=NEW.notes,
+    cashrcpt_distdate=
+      CASE
+        WHEN NEW.distribution_date > '' THEN
+          NEW.distribution_date::DATE
+        ELSE
+          NULL
+      END,
+    cashrcpt_salescat_id=getSalesCatId(NEW.sales_category),
+    cashrcpt_curr_id=getCurrId(NEW.currency),
+    cashrcpt_usecustdeposit=
+      CASE
+        WHEN NEW.apply_balance_as='Customer Deposit' THEN
+          true
+        ELSE
+          false
+      END
+    WHERE (cashrcpt_id=getCashrcptId(
+                       OLD.customer_number,
+                       CASE
+                         WHEN OLD.funds_type='Check' THEN
+                           'C'
+                         WHEN OLD.funds_type='Certified Check' THEN
+                           'T'
+                         WHEN OLD.funds_type='Master Card' THEN
+                           'M'
+                         WHEN OLD.funds_type='Visa' THEN
+                           'V'
+                         WHEN OLD.funds_type='American Express' THEN
+                           'A'
+                         WHEN OLD.funds_type='Discover Card' THEN
+                           'D'
+                         WHEN OLD.funds_type='Other Credit Card' THEN
+                           'R'
+                         WHEN OLD.funds_type='Cash' THEN
+                           'K'
+                         WHEN OLD.funds_type='Wire Transfer' THEN
+                           'W'
+                         ELSE
+                           'O'
+                       END,
+                       OLD.check_document_number) );
+
+
+CREATE OR REPLACE RULE "_DELETE" AS 
+  ON DELETE TO api.cashreceipt DO INSTEAD
+	
+    SELECT deleteCashrcpt(cashrcpt_id)
+	FROM cashrcpt
+    WHERE (cashrcpt_id=getCashrcptId(
+                       OLD.customer_number,
+                       CASE
+                         WHEN OLD.funds_type='Check' THEN
+                           'C'
+                         WHEN OLD.funds_type='Certified Check' THEN
+                           'T'
+                         WHEN OLD.funds_type='Master Card' THEN
+                           'M'
+                         WHEN OLD.funds_type='Visa' THEN
+                           'V'
+                         WHEN OLD.funds_type='American Express' THEN
+                           'A'
+                         WHEN OLD.funds_type='Discover Card' THEN
+                           'D'
+                         WHEN OLD.funds_type='Other Credit Card' THEN
+                           'R'
+                         WHEN OLD.funds_type='Cash' THEN
+                           'K'
+                         WHEN OLD.funds_type='Wire Transfer' THEN
+                           'W'
+                         ELSE
+                           'O'
+                       END,
+                       OLD.check_document_number) );
+
+COMMIT;
---
+++ xtupleserver/trunk/dbscripts/api/views/cashreceiptapplymisc.sql
@@ -0,0 +1,154 @@
+BEGIN;
+
+CREATE OR REPLACE VIEW api.cashreceiptapplymisc AS
+  SELECT
+    cust_number AS customer_number,
+    cust_name AS customer_name,
+    cust_address1 AS customer_address,
+    cashrcpt_docnumber AS check_document_number,
+    CASE
+      WHEN cashrcpt_fundstype='C' THEN
+        'Check'
+      WHEN cashrcpt_fundstype='T' THEN
+        'Certified Check'
+      WHEN cashrcpt_fundstype='M' THEN
+        'Master Card'
+      WHEN cashrcpt_fundstype='V' THEN
+        'Visa'
+      WHEN cashrcpt_fundstype='A' THEN
+        'American Express'
+      WHEN cashrcpt_fundstype='D' THEN
+        'Discover Card'
+      WHEN cashrcpt_fundstype='R' THEN
+        'Other Credit Card'
+      WHEN cashrcpt_fundstype='K' THEN
+        'Cash'
+      WHEN cashrcpt_fundstype='W' THEN
+        'Wire Transfer'
+      WHEN cashrcpt_fundstype='O' THEN
+        'Other'
+    END AS funds_type,
+    formatGLAccount(accnt_id) AS account,
+    accnt_descrip AS account_description,
+    curr_abbr AS currency,
+    cashrcptmisc_amount AS amount_to_distribute,
+    cashrcptmisc_notes AS notes
+  FROM cashrcptmisc
+    LEFT OUTER JOIN cashrcpt ON (cashrcpt_id=cashrcptmisc_cashrcpt_id)
+    LEFT OUTER JOIN cust ON (cust_id=cashrcpt_cust_id)
+    LEFT OUTER JOIN curr_symbol ON (curr_id=cashrcpt_curr_id)
+    LEFT OUTER JOIN accnt ON (accnt_id=cashrcptmisc_accnt_id);
+	
+GRANT ALL ON TABLE api.cashreceiptapplymisc TO openmfg;
+COMMENT ON VIEW api.cashreceiptapplymisc IS '
+This view can be used as an interface to import Cash Receipt Miscellaneous Application
+data directly into the system.  Required fields will be checked and default values
+will be populated';
+
+--Rules
+
+CREATE OR REPLACE RULE "_INSERT" AS
+  ON INSERT TO api.cashreceiptapplymisc DO INSTEAD
+
+  INSERT INTO cashrcptmisc (
+    cashrcptmisc_cashrcpt_id,
+    cashrcptmisc_accnt_id,
+    cashrcptmisc_amount,
+    cashrcptmisc_notes
+    )
+  VALUES (
+    getCashrcptId(NEW.customer_number,
+                  CASE
+                    WHEN NEW.funds_type='Check' THEN
+                      'C'
+                    WHEN NEW.funds_type='Certified Check' THEN
+                      'T'
+                    WHEN NEW.funds_type='Master Card' THEN
+                      'M'
+                    WHEN NEW.funds_type='Visa' THEN
+                      'V'
+                    WHEN NEW.funds_type='American Express' THEN
+                      'A'
+                    WHEN NEW.funds_type='Discover Card' THEN
+                      'D'
+                    WHEN NEW.funds_type='Other Credit Card' THEN
+                      'R'
+                    WHEN NEW.funds_type='Cash' THEN
+                      'K'
+                    WHEN NEW.funds_type='Wire Transfer' THEN
+                      'W'
+                    ELSE
+                      'O'
+                  END,
+                  NEW.check_document_number),
+    getGlAccntId(NEW.account),
+    COALESCE(NEW.amount_to_distribute, 0),
+    COALESCE(NEW.notes, '')
+    );
+
+CREATE OR REPLACE RULE "_UPDATE" AS 
+  ON UPDATE TO api.cashreceiptapplymisc DO INSTEAD
+
+  UPDATE cashrcptmisc SET
+    cashrcptmisc_accnt_id=getGlAccntId(NEW.account),
+    cashrcptmisc_amount=NEW.amount_to_distribute,
+    cashrcptmisc_notes=NEW.notes
+    WHERE ( (cashrcptmisc_cashrcpt_id=getCashrcptId(
+                       OLD.customer_number,
+                       CASE
+                         WHEN OLD.funds_type='Check' THEN
+                           'C'
+                         WHEN OLD.funds_type='Certified Check' THEN
+                           'T'
+                         WHEN OLD.funds_type='Master Card' THEN
+                           'M'
+                         WHEN OLD.funds_type='Visa' THEN
+                           'V'
+                         WHEN OLD.funds_type='American Express' THEN
+                           'A'
+                         WHEN OLD.funds_type='Discover Card' THEN
+                           'D'
+                         WHEN OLD.funds_type='Other Credit Card' THEN
+                           'R'
+                         WHEN OLD.funds_type='Cash' THEN
+                           'K'
+                         WHEN OLD.funds_type='Wire Transfer' THEN
+                           'W'
+                         ELSE
+                           'O'
+                       END,
+                       OLD.check_document_number))
+      AND   (cashrcptmisc_accnt_id=getGlAccntId(OLD.account)) );
+
+CREATE OR REPLACE RULE "_DELETE" AS 
+  ON DELETE TO api.cashreceiptapplymisc DO INSTEAD
+	
+    DELETE FROM cashrcptmisc
+    WHERE ( (cashrcptmisc_cashrcpt_id=getCashrcptId(
+                       OLD.customer_number,
+                       CASE
+                         WHEN OLD.funds_type='Check' THEN
+                           'C'
+                         WHEN OLD.funds_type='Certified Check' THEN
+                           'T'
+                         WHEN OLD.funds_type='Master Card' THEN
+                           'M'
+                         WHEN OLD.funds_type='Visa' THEN
+                           'V'
+                         WHEN OLD.funds_type='American Express' THEN
+                           'A'
+                         WHEN OLD.funds_type='Discover Card' THEN
+                           'D'
+                         WHEN OLD.funds_type='Other Credit Card' THEN
+                           'R'
+                         WHEN OLD.funds_type='Cash' THEN
+                           'K'
+                         WHEN OLD.funds_type='Wire Transfer' THEN
+                           'W'
+                         ELSE
+                           'O'
+                       END,
+                       OLD.check_document_number))
+      AND   (cashrcptmisc_accnt_id=getGlAccntId(OLD.account)) );
+
+COMMIT;
