cfacbfa4a944a387a639d1014e4cea5e3986ebbd
ghartnett <ghartnett@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-07-01 17:31:38 +0000

    issue #6816:enhance validations

--- xtupleserver/trunk/dbscripts/triggers/whsinfo.sql
+++ xtupleserver/trunk/dbscripts/triggers/whsinfo.sql
@@ -1,9 +1,71 @@
 CREATE OR REPLACE FUNCTION _warehousTrigger () RETURNS TRIGGER AS '
 DECLARE
   _cmnttypeid INTEGER;
+  _check      BOOLEAN;
+  _checkId    INTEGER;
 
 BEGIN
 
+  -- Checks
+  -- Start with privileges
+  IF (TG_OP = ''INSERT'') THEN
+    SELECT checkPrivilege(''MaintainWarehouses'') INTO _check;
+    IF NOT (_check) THEN
+      RAISE EXCEPTION ''You do not have privileges to add new Sites.'';
+    END IF;
+  ELSE
+    SELECT checkPrivilege(''MaintainWarehouses'') INTO _check;
+    IF NOT (_check) THEN
+      RAISE EXCEPTION ''You do not have privileges to alter a Site.'';
+    END IF;
+  END IF;
+
+  -- Code is required
+  IF (LENGTH(COALESCE(NEW.warehous_code,''''))=0) THEN
+    RAISE EXCEPTION ''You must supply a valid Site Code.'';
+  END IF;
+  
+  -- Sitetype is required
+  IF (NEW.warehous_sitetype_id IS NULL) THEN
+    RAISE EXCEPTION ''You must supply a valid Site Type.'';
+  END IF;
+
+  -- GL Account is required
+  IF (NEW.warehous_default_accnt_id IS NULL) THEN
+    RAISE EXCEPTION ''You must supply a valid Default GL Account.'';
+  END IF;
+
+  -- Cost Category is required for Transit types
+  IF ((NEW.warehous_transit) AND (NEW.warehous_costcat_id IS NULL)) THEN
+    RAISE EXCEPTION ''You must supply a valid Cost Category for Transit Sites.'';
+  END IF;
+
+  -- Code must be unique
+  SELECT warehous_id INTO _checkId
+  FROM whsinfo
+  WHERE ( (UPPER(warehous_code)=UPPER(NEW.warehous_code))
+    AND   (warehous_id<>NEW.warehous_id) );
+  IF (FOUND) THEN
+    RAISE EXCEPTION ''You must supply a unique Site Code.'';
+  END IF;
+  
+  -- Count Tag Prefix must be unique
+  IF (TG_OP = ''INSERT'') THEN
+    SELECT warehous_id INTO _checkId
+    FROM whsinfo
+    WHERE (warehous_counttag_prefix=NEW.warehous_counttag_prefix);
+  ELSE
+    SELECT warehous_id INTO _checkId
+    FROM whsinfo
+    WHERE ( (warehous_counttag_prefix=NEW.warehous_counttag_prefix)
+      AND   (warehous_id<>NEW.warehous_id) );
+  END IF;
+  IF (FOUND) THEN
+    RAISE EXCEPTION ''You must supply a unique Count Tag Prefix.'';
+  END IF;
+  
+  -- Check Complete
+  -- Change Log
   IF ( SELECT (metric_value=''t'')
        FROM metric
        WHERE (metric_name=''WarehouseChangeLog'') ) THEN
