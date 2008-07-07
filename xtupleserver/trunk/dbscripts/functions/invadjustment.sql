15710b47f7090154816073609294807124235b30
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-07-07 19:09:16 +0000

    Changes to support Average Costing on the Adjustment Trans and Material Receipt Trans screens. Issue #6789.

--- xtupleserver/trunk/dbscripts/functions/invadjustment.sql
+++ xtupleserver/trunk/dbscripts/functions/invadjustment.sql
@@ -1,35 +1,42 @@
-CREATE OR REPLACE FUNCTION invAdjustment(INTEGER, NUMERIC, TEXT, TEXT) RETURNS INTEGER AS '
+CREATE OR REPLACE FUNCTION invAdjustment(INTEGER, NUMERIC, TEXT, TEXT) RETURNS INTEGER AS $$
 BEGIN
-  RETURN invAdjustment($1, $2, $3, $4, CURRENT_TIMESTAMP);
+  RETURN invAdjustment($1, $2, $3, $4, CURRENT_TIMESTAMP, NULL);
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
 
-CREATE OR REPLACE FUNCTION invAdjustment(INTEGER, NUMERIC, TEXT, TEXT, TIMESTAMP WITH TIME ZONE) RETURNS INTEGER AS '
+CREATE OR REPLACE FUNCTION invAdjustment(INTEGER, NUMERIC, TEXT, TEXT, TIMESTAMP WITH TIME ZONE) RETURNS INTEGER AS $$
+BEGIN
+  RETURN invAdjustment($1, $2, $3, $4, $5, NULL);
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION invAdjustment(INTEGER, NUMERIC, TEXT, TEXT, TIMESTAMP WITH TIME ZONE, NUMERIC) RETURNS INTEGER AS $$
 DECLARE
   pItemsiteid     ALIAS FOR $1;
   pQty            ALIAS FOR $2;
   pDocumentNumber ALIAS FOR $3;
   pComments       ALIAS FOR $4;
   pGlDistTS       ALIAS FOR $5;
+  pCostValue      ALIAS FOR $6;
   _invhistid      INTEGER;
   _itemlocSeries  INTEGER;
 
 BEGIN
 
 --  Make sure the passed itemsite points to a real item
-  IF ( ( SELECT (item_type IN (''R'', ''F'', ''J''))
+  IF ( ( SELECT (item_type IN ('R', 'F', 'J'))
          FROM itemsite, item
          WHERE ( (itemsite_item_id=item_id)
           AND (itemsite_id=pItemsiteid) ) ) ) THEN
     RETURN 0;
   END IF;
 
-  SELECT NEXTVAL(''itemloc_series_seq'') INTO _itemlocSeries;
-  SELECT postInvTrans( itemsite_id, ''AD'', pQty,
-                       ''I/M'', '''', pDocumentNumber, '''',
-                       (E''Miscellaneous Adjustment\n'' ||  pComments),
+  SELECT NEXTVAL('itemloc_series_seq') INTO _itemlocSeries;
+  SELECT postInvTrans( itemsite_id, 'AD', pQty,
+                       'I/M', '', pDocumentNumber, '',
+                       (E'Miscellaneous Adjustment\n' ||  pComments),
                        costcat_asset_accnt_id, costcat_adjustment_accnt_id,
-                       _itemlocSeries, pGlDistTS) INTO _invhistid
+                       _itemlocSeries, pGlDistTS, pCostValue) INTO _invhistid
   FROM itemsite, costcat
   WHERE ( (itemsite_costcat_id=costcat_id)
    AND (itemsite_id=pItemsiteid) );
@@ -37,4 +44,4 @@ BEGIN
   RETURN _itemlocSeries;
 
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
--- xtupleserver/trunk/dbscripts/functions/invreceipt.sql
+++ xtupleserver/trunk/dbscripts/functions/invreceipt.sql
@@ -1,10 +1,16 @@
-CREATE OR REPLACE FUNCTION invReceipt(INTEGER, NUMERIC, TEXT, TEXT, TEXT) RETURNS INTEGER AS '
+CREATE OR REPLACE FUNCTION invReceipt(INTEGER, NUMERIC, TEXT, TEXT, TEXT) RETURNS INTEGER AS $$
 BEGIN
-  RETURN invReceipt($1, $2, $3, $4, $5, CURRENT_TIMESTAMP);
+  RETURN invReceipt($1, $2, $3, $4, $5, CURRENT_TIMESTAMP, NULL);
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
 
-CREATE OR REPLACE FUNCTION invReceipt(INTEGER, NUMERIC, TEXT, TEXT, TEXT, TIMESTAMP WITH TIME ZONE) RETURNS INTEGER AS '
+CREATE OR REPLACE FUNCTION invReceipt(INTEGER, NUMERIC, TEXT, TEXT, TEXT, TIMESTAMP WITH TIME ZONE) RETURNS INTEGER AS $$
+BEGIN
+  RETURN invReceipt($1, $2, $3, $4, $5, $6, NULL);
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION invReceipt(INTEGER, NUMERIC, TEXT, TEXT, TEXT, TIMESTAMP WITH TIME ZONE, NUMERIC) RETURNS INTEGER AS $$
 DECLARE
   pItemsiteid ALIAS FOR $1;
   pQty ALIAS FOR $2;
@@ -12,25 +18,26 @@ DECLARE
   pDocumentNumber ALIAS FOR $4;
   pComments ALIAS FOR $5;
   pGlDistTS     ALIAS FOR $6;
+  pCostValue ALIAS FOR $7;
   _invhistid INTEGER;
   _itemlocSeries INTEGER;
 
 BEGIN
 
 --  Make sure the passed itemsite points to a real item
-  IF ( ( SELECT (item_type IN (''R'', ''F'', ''J''))
+  IF ( ( SELECT (item_type IN ('R', 'F', 'J'))
          FROM itemsite, item
          WHERE ( (itemsite_item_id=item_id)
           AND (itemsite_id=pItemsiteid) ) ) ) THEN
     RETURN 0;
   END IF;
 
-  SELECT NEXTVAL(''itemloc_series_seq'') INTO _itemlocSeries;
-  SELECT postInvTrans( itemsite_id, ''RX'', pQty,
-                       ''I/M'', '''', pDocumentNumber, '''',
-                       (E''Miscellaneous Receipt\n'' ||  pComments),
+  SELECT NEXTVAL('itemloc_series_seq') INTO _itemlocSeries;
+  SELECT postInvTrans( itemsite_id, 'RX', pQty,
+                       'I/M', '', pDocumentNumber, '',
+                       (E'Miscellaneous Receipt\n' ||  pComments),
                        costcat_asset_accnt_id, costcat_liability_accnt_id,
-                       _itemlocSeries, pGlDistTS) INTO _invhistid
+                       _itemlocSeries, pGlDistTS, pCostValue) INTO _invhistid
   FROM itemsite, costcat
   WHERE ( (itemsite_costcat_id=costcat_id)
    AND (itemsite_id=pItemsiteid) );
@@ -38,4 +45,4 @@ BEGIN
   RETURN _itemlocSeries;
 
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
