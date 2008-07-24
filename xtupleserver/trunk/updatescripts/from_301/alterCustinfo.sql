43fc596464e03958b26f56ee8313df808d34639b
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-07-24 16:43:56 +0000

    Added code to allow marking a batch job to send email as a multi-part html. Issu e #6807.

--- xtupleserver/trunk/updatescripts/from_301/alterCustinfo.sql
+++ xtupleserver/trunk/updatescripts/from_301/alterCustinfo.sql
@@ -2,4 +2,14 @@ BEGIN;
 
 ALTER TABLE custinfo ADD UNIQUE (cust_number);
 
+ALTER TABLE custinfo ADD COLUMN cust_ediemailhtml BOOLEAN;
+ALTER TABLE custinfo ALTER COLUMN cust_ediemailhtml SET DEFAULT FALSE;
+UPDATE custinfo SET cust_ediemailhtml=false WHERE cust_ediemailhtml IS NULL;
+ALTER TABLE custinfo ALTER COLUMN cust_ediemailhtml SET NOT NULL;
+
+ALTER TABLE custinfo ADD COLUMN cust_soediemailhtml BOOLEAN;
+ALTER TABLE custinfo ALTER COLUMN cust_soediemailhtml SET DEFAULT FALSE;
+UPDATE custinfo SET cust_soediemailhtml=false WHERE cust_soediemailhtml IS NULL;
+ALTER TABLE custinfo ALTER COLUMN cust_soediemailhtml SET NOT NULL;
+
 COMMIT;
