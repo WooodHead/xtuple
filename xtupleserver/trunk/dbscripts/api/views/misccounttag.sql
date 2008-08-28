c9553955233def12c8f43c458c4c8ba4d118759c
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-08-28 18:26:03 +0000

    issue #7231:  Add misc. count tag view.

---
+++ xtupleserver/trunk/dbscripts/api/views/misccounttag.sql
@@ -0,0 +1,37 @@
+BEGIN;
+
+-- Simple Journal Entry
+SELECT dropifexists('view','misccounttag','api');
+CREATE VIEW api.misccounttag
+AS 
+  SELECT
+    'This view is for inserts only'::text as site,
+    'This view is for inserts only'::text as item_number,
+    0::numeric as quantity,
+    'This view is for inserts only'::text as comment;
+
+GRANT ALL ON TABLE api.misccounttag TO openmfg;
+COMMENT ON VIEW api.misccounttag IS 'Miscellaneous Count Tag';
+
+--Rules
+
+CREATE OR REPLACE RULE "_INSERT" AS
+    ON INSERT TO api.misccounttag DO INSTEAD
+
+SELECT postMiscCount( 
+  getItemsiteId(NEW.site,NEW.item_number),
+  NEW.quantity,
+  NEW.comment
+     );
+
+CREATE OR REPLACE RULE "_UPDATE" AS 
+    ON UPDATE TO api.misccounttag DO INSTEAD
+
+  NOTHING;
+           
+CREATE OR REPLACE RULE "_DELETE" AS 
+    ON DELETE TO api.misccounttag DO INSTEAD
+
+  NOTHING;
+
+COMMIT;
