5097c07ec11184e8bebb7f864bf3d50f3e8e3215
nmonday <nmonday@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-08-29 14:38:49 +0000

    Transfering 2 files from 'from_310beta' to 'from_310beta2'

--- xtupleserver/trunk/updatescripts/from_310beta/updatePriv.sql
+++
@@ -1,6 +0,0 @@
-BEGIN;
-
-DELETE FROM usrpriv WHERE usrpriv_priv_id IN (SELECT priv_id FROM priv WHERE priv_name IN ('CreateCRMA','MaintainCRMA','ViewCRMA'));
-DELETE FROM priv WHERE priv_module='CRM' AND priv_name IN ('CreateCRMA','MaintainCRMA','ViewCRMA');
-
-COMMIT;
