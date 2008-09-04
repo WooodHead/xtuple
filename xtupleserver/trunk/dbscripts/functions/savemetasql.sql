8f4c8d2da4a82065129084cb937dda44ade2229e
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-09-04 16:05:54 +0000

    make fewer assumptions about where metasql statements should live - pkg-specific vs. public

--- xtupleserver/trunk/dbscripts/functions/savemetasql.sql
+++ xtupleserver/trunk/dbscripts/functions/savemetasql.sql
@@ -1,10 +1,10 @@
-CREATE OR REPLACE FUNCTION saveMetasql(TEXT, TEXT, TEXT, TEXT) RETURNS INTEGER AS '
+CREATE OR REPLACE FUNCTION saveMetasql(TEXT, TEXT, TEXT, TEXT) RETURNS INTEGER AS $$
 BEGIN
   RETURN saveMetasql($1, $2, $3, $4, true);
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
 
-CREATE OR REPLACE FUNCTION saveMetasql(TEXT,TEXT,TEXT,TEXT,BOOL) RETURNS INTEGER AS '
+CREATE OR REPLACE FUNCTION saveMetasql(TEXT,TEXT,TEXT,TEXT,BOOL) RETURNS INTEGER AS $$
 DECLARE
   pGroup	ALIAS FOR $1;
   pName 	ALIAS FOR $2;
@@ -12,55 +12,41 @@ DECLARE
   pQuery	ALIAS FOR $4;
   pSystem       ALIAS FOR $5;
   _metasqlid	INTEGER;
+  _tableoid     INTEGER;
+  _schema       TEXT;
   _debug        BOOL    := true;
   
 BEGIN
 
   --See if Query already exists
-  IF (pSystem) THEN
-    SELECT metasql_id INTO _metasqlid
-    FROM metasql
-    WHERE ((metasql_group=pGroup)
-    AND (metasql_name=pName));
-  ELSE
-    SELECT metasql_id INTO _metasqlid
-    FROM pkgmetasql
-    WHERE ((metasql_group=pGroup)
-    AND (metasql_name=pName));
-  END IF;
+  SELECT tableoid, metasql_id INTO _metasqlid, _tableoid
+  FROM metasql
+  WHERE ((metasql_group=pGroup)
+     AND (metasql_name=pName));
 
   IF (FOUND) THEN
-    IF (pSystem) THEN
-      IF (_debug) THEN RAISE NOTICE ''update metasql''; END IF;
-      UPDATE metasql SET
-        metasql_group=pGroup,
-        metasql_name=pName,
-        metasql_notes=pNotes,
-        metasql_query=pQuery
-      WHERE (metasql_id=_metasqlid);
-    ELSE
-      IF (_debug) THEN RAISE NOTICE ''update pkgmetasql''; END IF;
-      UPDATE pkgmetasql SET
-        metasql_group=pGroup,
-        metasql_name=pName,
-        metasql_notes=pNotes,
-        metasql_query=pQuery
-      WHERE (metasql_id=_metasqlid);
-    END IF;
+    IF (_debug) THEN RAISE NOTICE 'update metasql'; END IF;
+    UPDATE metasql SET
+      metasql_group=pGroup,
+      metasql_name=pName,
+      metasql_notes=pNotes,
+      metasql_query=pQuery
+    WHERE (metasql_id=_metasqlid);
   ELSE
-    _metasqlid := NEXTVAL(''metasql_metasql_id_seq'');
+    _metasqlid := NEXTVAL('metasql_metasql_id_seq');
 
-    IF (pSystem) THEN
-      IF (_debug) THEN RAISE NOTICE ''insert metasql''; END IF;
-      INSERT INTO metasql 
-      VALUES (_metasqlid,pGroup,pName,pNotes,pQuery);
-    ELSE
-      IF (_debug) THEN RAISE NOTICE ''insert pkgmetasql''; END IF;
+    BEGIN
+      IF (_debug) THEN RAISE NOTICE 'attempting to insert into pkgmetasql'; END IF;
       INSERT INTO pkgmetasql 
       VALUES (_metasqlid,pGroup,pName,pNotes,pQuery);
-    END IF;
+    EXCEPTION WHEN undefined_table THEN
+                    IF (_debug) THEN RAISE NOTICE 'insert metasql'; END IF;
+                    INSERT INTO metasql 
+                    VALUES (_metasqlid,pGroup,pName,pNotes,pQuery);
+              WHEN OTHERS THEN RAISE EXCEPTION '% %', SQLSTATE, SQLERRM;
+    END;
   END IF;
  
   RETURN _metasqlid;
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
