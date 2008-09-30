c4c0ae71a5363fae3981ddc15cc2dd80d4dd1ecf
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-09-30 17:11:30 +0000

    remove no-longer-useful notices

--- xtupleserver/trunk/dbscripts/functions/dropifexists.sql
+++ xtupleserver/trunk/dbscripts/functions/dropifexists.sql
@@ -1,11 +1,11 @@
-CREATE OR REPLACE FUNCTION dropIfExists(TEXT, TEXT) RETURNS INTEGER AS '
+CREATE OR REPLACE FUNCTION dropIfExists(TEXT, TEXT) RETURNS INTEGER AS $$
 BEGIN
-  RETURN dropIfExists($1, $2, ''public'');
+  RETURN dropIfExists($1, $2, 'public');
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
 
 
-CREATE OR REPLACE FUNCTION dropIfExists(TEXT, TEXT, TEXT) RETURNS INTEGER AS '
+CREATE OR REPLACE FUNCTION dropIfExists(TEXT, TEXT, TEXT) RETURNS INTEGER AS $$
 DECLARE
   pType         ALIAS FOR $1;
   pObject       ALIAS FOR $2;
@@ -13,60 +13,55 @@ DECLARE
   _table	TEXT;
   _query	TEXT;
 BEGIN
-  IF (UPPER(pType) = ''TABLE'') THEN
-    _query = ''DROP TABLE '' || quote_ident(LOWER(pSchema)) || ''.'' || quote_ident(LOWER(pObject));
+  IF (UPPER(pType) = 'TABLE') THEN
+    _query = 'DROP TABLE ' || quote_ident(LOWER(pSchema)) || '.' || quote_ident(LOWER(pObject));
     BEGIN
       EXECUTE _query;
     EXCEPTION WHEN undefined_table OR invalid_schema_name THEN
-		RAISE NOTICE ''No table % to drop'', pObject;
 		RETURN 0;
-	      WHEN OTHERS THEN RAISE EXCEPTION ''% %'', SQLSTATE, SQLERRM;
+	      WHEN OTHERS THEN RAISE EXCEPTION '% %', SQLSTATE, SQLERRM;
     END;
 
-  ELSIF (UPPER(pType) = ''VIEW'') THEN
-    _query = ''DROP VIEW '' || quote_ident(LOWER(pSchema)) || ''.'' || quote_ident(LOWER(pObject));
+  ELSIF (UPPER(pType) = 'VIEW') THEN
+    _query = 'DROP VIEW ' || quote_ident(LOWER(pSchema)) || '.' || quote_ident(LOWER(pObject));
     BEGIN
       EXECUTE _query;
     EXCEPTION WHEN undefined_table OR invalid_schema_name THEN
-		RAISE NOTICE ''No view % to drop'', pObject;
 		RETURN 0;
-	      WHEN OTHERS THEN RAISE EXCEPTION ''% %'', SQLSTATE, SQLERRM;
+	      WHEN OTHERS THEN RAISE EXCEPTION '% %', SQLSTATE, SQLERRM;
     END;
 
-  ELSIF (UPPER(pType) = ''TRIGGER'') THEN
+  ELSIF (UPPER(pType) = 'TRIGGER') THEN
     SELECT relname INTO _table
     FROM pg_trigger, pg_class
     WHERE ((tgrelid=pg_class.oid)
       AND  (UPPER(tgname)=UPPER(pObject)));
     IF (NOT FOUND) THEN
-      _table := ''[no table]'';
+      _table := '[no table]';
     END IF;
 
-    _query = ''DROP TRIGGER '' || quote_ident(LOWER(pObject)) ||
-	     '' ON '' || quote_ident(LOWER(pSchema)) || ''.'' || quote_ident(LOWER(_table));
+    _query = 'DROP TRIGGER ' || quote_ident(LOWER(pObject)) ||
+	     ' ON ' || quote_ident(LOWER(pSchema)) || '.' || quote_ident(LOWER(_table));
     BEGIN
       EXECUTE _query;
     EXCEPTION WHEN undefined_object THEN
-		RAISE NOTICE ''%'', SQLERRM;
 		RETURN 0;
 	      WHEN undefined_table OR invalid_schema_name THEN
-		RAISE NOTICE ''Table % for trigger % does not exist'', _table, pObject;
 		RETURN 0;
-	      WHEN OTHERS THEN RAISE EXCEPTION ''% %'', SQLSTATE, SQLERRM;
+	      WHEN OTHERS THEN RAISE EXCEPTION '% %', SQLSTATE, SQLERRM;
     END;
 
-  ELSIF (UPPER(pType) = ''FUNCTION'') THEN
-    _query = ''DROP FUNCTION '' || (LOWER(pSchema)) || ''.'' ||
+  ELSIF (UPPER(pType) = 'FUNCTION') THEN
+    _query = 'DROP FUNCTION ' || (LOWER(pSchema)) || '.' ||
                                    (LOWER(pObject));
     BEGIN
       EXECUTE _query;
     EXCEPTION WHEN undefined_function THEN
-		RAISE NOTICE ''%'', SQLERRM;
 		RETURN 0;
-	      WHEN OTHERS THEN RAISE EXCEPTION ''% %'', SQLSTATE, SQLERRM;
+	      WHEN OTHERS THEN RAISE EXCEPTION '% %', SQLSTATE, SQLERRM;
     END;
 
-  ELSIF (UPPER(pType) = ''CONSTRAINT'') THEN
+  ELSIF (UPPER(pType) = 'CONSTRAINT') THEN
     SELECT relname INTO _table
     FROM pg_constraint, pg_class, pg_namespace
     WHERE ((conrelid=pg_class.oid)
@@ -74,47 +69,41 @@ BEGIN
       AND  (conname=pObject)
       AND  (nspname=pSchema));
     IF (NOT FOUND) THEN
-      RAISE NOTICE ''No constraint % to drop'', pObject;
       RETURN 0;
     END IF;
-    _query = ''ALTER TABLE '' || quote_ident(LOWER(pSchema)) || ''.'' || quote_ident(LOWER(_table))
-             || '' DROP CONSTRAINT '' || quote_ident(LOWER(pObject));
+    _query = 'ALTER TABLE ' || quote_ident(LOWER(pSchema)) || '.' || quote_ident(LOWER(_table))
+             || ' DROP CONSTRAINT ' || quote_ident(LOWER(pObject));
     BEGIN
       EXECUTE _query;
     EXCEPTION WHEN undefined_table OR invalid_schema_name THEN
-		RAISE NOTICE ''No table % to alter'', _table;
 		RETURN 0;
-	      WHEN OTHERS THEN RAISE EXCEPTION ''% %'', SQLSTATE, SQLERRM;
+	      WHEN OTHERS THEN RAISE EXCEPTION '% %', SQLSTATE, SQLERRM;
     END;
 
-  ELSIF (UPPER(pType) = ''SCHEMA'') THEN
-    _query = ''DROP SCHEMA '' || quote_ident(LOWER(pObject));
+  ELSIF (UPPER(pType) = 'SCHEMA') THEN
+    _query = 'DROP SCHEMA ' || quote_ident(LOWER(pObject));
     BEGIN
       EXECUTE _query;
     EXCEPTION WHEN invalid_schema_name THEN
-                RAISE NOTICE ''No table % to alter'', _table;
                 RETURN 0;
-              WHEN OTHERS THEN RAISE EXCEPTION ''% %'', SQLSTATE, SQLERRM;
+              WHEN OTHERS THEN RAISE EXCEPTION '% %', SQLSTATE, SQLERRM;
     END;
 
-  ELSIF (UPPER(pType) = ''TYPE'') THEN
-    _query = ''DROP TYPE '' || quote_ident(LOWER(pSchema)) || ''.'' ||
+  ELSIF (UPPER(pType) = 'TYPE') THEN
+    _query = 'DROP TYPE ' || quote_ident(LOWER(pSchema)) || '.' ||
                                quote_ident(LOWER(pObject));
     BEGIN
       EXECUTE _query;
     EXCEPTION WHEN undefined_object THEN
-                RAISE NOTICE ''No type %.% to alter'', pSchema, pObject;
                 RETURN 0;
-              WHEN OTHERS THEN RAISE EXCEPTION ''% %'', SQLSTATE, SQLERRM;
+              WHEN OTHERS THEN RAISE EXCEPTION '% %', SQLSTATE, SQLERRM;
     END;
 
   ELSE
-    RAISE EXCEPTION ''dropIfExists(%, %): unknown pType %'', pType, pObject, pType;
+    RAISE EXCEPTION 'dropIfExists(%, %): unknown pType %', pType, pObject, pType;
   END IF;
-
-  -- RAISE NOTICE ''%'', _query;
 
   RETURN 1;
 
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
