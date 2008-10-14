b8d7abf4d68515e18de37e5dda3bf315b3f50149
nmonday <nmonday@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-10-14 19:14:29 +0000

    Issue #7253 DB changes for contacts in sales order screen.

--- xtupleserver/trunk/updatescripts/from_310/alterCohead.sql
+++ xtupleserver/trunk/updatescripts/from_310/alterCohead.sql
@@ -5,4 +5,26 @@ UPDATE cohead SET cohead_calcfreight=FALSE;
 ALTER TABLE cohead ALTER cohead_calcfreight SET NOT NULL;
 ALTER TABLE cohead ALTER cohead_calcfreight SET DEFAULT FALSE;
 
+ALTER TABLE cohead ADD COLUMN cohead_shipto_cntct_id INT;
+ALTER TABLE cohead ADD COLUMN cohead_shipto_cntct_honorific text;
+ALTER TABLE cohead ADD COLUMN cohead_shipto_cntct_first_name text;
+ALTER TABLE cohead ADD COLUMN cohead_shipto_cntct_middle text;
+ALTER TABLE cohead ADD COLUMN cohead_shipto_cntct_last_name text;
+ALTER TABLE cohead ADD COLUMN cohead_shipto_cntct_suffix text;
+ALTER TABLE cohead ADD COLUMN cohead_shipto_cntct_phone text;
+ALTER TABLE cohead ADD COLUMN cohead_shipto_cntct_title text;
+ALTER TABLE cohead ADD COLUMN cohead_shipto_cntct_fax text;
+ALTER TABLE cohead ADD COLUMN cohead_shipto_cntct_email text;
+
+ALTER TABLE cohead ADD COLUMN cohead_billto_cntct_id INT;
+ALTER TABLE cohead ADD COLUMN cohead_billto_cntct_honorific text;
+ALTER TABLE cohead ADD COLUMN cohead_billto_cntct_first_name text;
+ALTER TABLE cohead ADD COLUMN cohead_billto_cntct_middle text;
+ALTER TABLE cohead ADD COLUMN cohead_billto_cntct_last_name text;
+ALTER TABLE cohead ADD COLUMN cohead_billto_cntct_suffix text;
+ALTER TABLE cohead ADD COLUMN cohead_billto_cntct_phone text;
+ALTER TABLE cohead ADD COLUMN cohead_billto_cntct_title text;
+ALTER TABLE cohead ADD COLUMN cohead_billto_cntct_fax text;
+ALTER TABLE cohead ADD COLUMN cohead_billto_cntct_email text;
+
 COMMIT;
