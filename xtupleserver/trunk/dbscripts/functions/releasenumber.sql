6f530b9b5cb2263533aa8ff1e3a5c6d9dd237b1f
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-10-28 18:37:27 +0000

    issue #6909 - implement outbound ACH check support

--- xtupleserver/trunk/dbscripts/functions/releasenumber.sql
+++ xtupleserver/trunk/dbscripts/functions/releasenumber.sql
@@ -1,7 +1,8 @@
-CREATE OR REPLACE FUNCTION releaseNumber(TEXT, INTEGER) RETURNS INTEGER AS '
+CREATE OR REPLACE FUNCTION releaseNumber(TEXT, INTEGER) RETURNS INTEGER AS $$
 DECLARE
   psequence	ALIAS FOR $1;
   pnumber	ALIAS FOR $2;
+  _isManual     BOOLEAN;
   _test		INTEGER;
   _number	TEXT;
   _table	TEXT;
@@ -14,14 +15,14 @@ BEGIN
   FROM orderseq
   WHERE (orderseq_name=psequence);
   IF (NOT FOUND) THEN
-    RAISE EXCEPTION ''Invalid orderseq_name %'', psequence;
+    RAISE EXCEPTION 'Invalid orderseq_name %', psequence;
   END IF;
 
   -- check if an order exists with the given order number
-  EXECUTE ''SELECT '' || quote_ident(_numcol) ||
-	  '' FROM ''  || quote_ident(_table) ||
-	  '' WHERE ('' || quote_ident(_numcol) || ''='' ||
-          quote_literal(_number) || '');''
+  EXECUTE 'SELECT ' || quote_ident(_numcol) ||
+	  ' FROM '  || quote_ident(_table) ||
+	  ' WHERE (' || quote_ident(_numcol) || '=' ||
+          quote_literal(_number) || ');'
   INTO _test;
 
   IF (NOT FOUND) THEN
@@ -30,18 +31,29 @@ BEGIN
 
   -- check if order seq has been incremented past the given order number
   -- S/O code reads: IF ((_test - 1) <> pSoNumber) THEN
-  -- but the following /should/ address bug 4020 (can''t reproduce it to test)
+  -- but the following /should/ address bug 4020 (can't reproduce it to test)
   IF ((_test - 1) > pnumber) THEN
     RETURN 0;
   END IF;
 
-  PERFORM *
+  SELECT metric_value = 'M' INTO _isManual
   FROM metric
-  WHERE metric_value = ''M''
-  AND metric_name = ''TONumberGeneration'';
+  WHERE (metric_name = CASE WHEN psequence='CmNumber' THEN 'CMNumberGeneration'
+                            WHEN psequence='SoNumber' THEN 'CONumberGeneration'
+                            WHEN psequence='InvcNumber' THEN 'InvcNumberGeneration'
+                            WHEN psequence='PoNumber' THEN 'PONumberGeneration'
+                            WHEN psequence='PrNumber' THEN 'PrNumberGeneration'
+                            WHEN psequence='QuNumber' THEN 'QUNumberGeneration'
+                            WHEN psequence='RaNumber' THEN 'RANumberGeneration'
+                       --   WHEN psequence='??Number' THEN 'ShipmentNumberGeneration'
+                            WHEN psequence='ToNumber' THEN 'TONumberGeneration'
+                            WHEN psequence='VcNumber' THEN 'VoucherNumberGeneration'
+                            WHEN psequence='WoNumber' THEN 'WONumberGeneration'
+                            ELSE NULL
+                       END);
 
   -- release the given order number for reuse
-  IF (NOT FOUND) THEN
+  IF (NOT FOUND OR NOT _isManual) THEN
     UPDATE orderseq
     SET orderseq_number = (orderseq_number - 1)
     WHERE (orderseq_name=psequence);
@@ -50,4 +62,4 @@ BEGIN
   RETURN 1;
 
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
--- xtupleserver/trunk/updatescripts/from_310/alterBankaccnt.sql
+++ xtupleserver/trunk/updatescripts/from_310/alterBankaccnt.sql
@@ -2,4 +2,13 @@ BEGIN;
 
 ALTER TABLE bankaccnt ADD UNIQUE (bankaccnt_name);
 
+ALTER TABLE bankaccnt ADD bankaccnt_notes          TEXT;
+ALTER TABLE bankaccnt ADD bankaccnt_routing        TEXT NOT NULL DEFAULT '';
+ALTER TABLE bankaccnt ADD bankaccnt_ach_enabled BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE bankaccnt ADD bankaccnt_ach_defaultorigin BOOLEAN NOT NULL DEFAULT true;
+ALTER TABLE bankaccnt ADD bankaccnt_ach_origin     TEXT NOT NULL DEFAULT '';
+ALTER TABLE bankaccnt ADD bankaccnt_ach_genchecknum BOOLEAN NOT NULL DEFAULT fales;
+ALTER TABLE bankaccnt ADD bankaccnt_ach_leadtime   INTEGER;
+ALTER TABLE bankaccnt ADD bankaccnt_ach_lastdate   DATE;
+ALTER TABLE bankaccnt ADD bankaccnt_ach_lastfileid CHARACTER(1);
 COMMIT;
