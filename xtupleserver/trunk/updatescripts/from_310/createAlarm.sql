29eac3a1b1ba3945fdf667efa95178559f7b2360
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-10-29 17:11:30 +0000

    Issue #7252:add alarm event types

--- xtupleserver/trunk/updatescripts/from_310/createAlarm.sql
+++ xtupleserver/trunk/updatescripts/from_310/createAlarm.sql
@@ -2,7 +2,9 @@ BEGIN;
 
 CREATE TABLE alarm (
   alarm_id SERIAL PRIMARY KEY NOT NULL,
+  alarm_number TEXT NOT NULL,
   alarm_type TEXT,
+  alarm_trigger TIMESTAMP WITH TIME ZONE,
   alarm_time TIMESTAMP WITH TIME ZONE,
   alarm_time_offset INTEGER,
   alarm_time_qualifier TEXT,
@@ -19,6 +21,9 @@ REVOKE ALL ON alarm_alarm_id_seq FROM PUBLIC;
 GRANT ALL ON alarm_alarm_id_seq TO GROUP openmfg;
 
 COMMENT ON TABLE alarm IS 'This table is the open alarms.';
+
+INSERT INTO orderseq (orderseq_name,orderseq_number,orderseq_table,orderseq_numcol)
+VALUES ('AlarmNumber',1,'alarm','alarm_number');
 
 COMMIT;
 
