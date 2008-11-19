204bd8622bfaacd9837fe056a4d833d8c63071fd
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-11-19 15:37:00 +0000

    Issue #7731:additional constraints for api

--- xtupleserver/trunk/updatescripts/from_320alpha/alterAROpen.sql
+++ xtupleserver/trunk/updatescripts/from_320alpha/alterAROpen.sql
@@ -1,5 +1,9 @@
 BEGIN;
 
+ALTER TABLE aropen ALTER COLUMN aropen_docdate SET NOT NULL;
+ALTER TABLE aropen ALTER COLUMN aropen_duedate SET NOT NULL;
+ALTER TABLE aropen ALTER COLUMN aropen_amount SET NOT NULL;
+
 ALTER TABLE aropen ADD COLUMN aropen_distdate DATE;
 
 UPDATE aropen SET aropen_distdate=(SELECT gltrans_date
