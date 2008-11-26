367095a1bedbe33065d8e4a359fdda5c1f7b7930
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-11-26 13:21:12 +0000

    issue #7810:  Fix union query that was missing values on single level BOM.

--- xtupleserver/trunk/dbscripts/functions/singlelevelbom.sql
+++ xtupleserver/trunk/dbscripts/functions/singlelevelbom.sql
@@ -69,7 +69,7 @@ BEGIN
               itemcost_stdcost AS stdunitcost,
               currToBase(itemcost_curr_id, itemcost_actcost, CURRENT_DATE) AS actextendedcost,
               itemcost_stdcost AS stdextendedcost,
-              NULL, NULL
+              NULL, NULL, NULL, NULL
        FROM itemcost, costelem 
        WHERE ( (itemcost_costelem_id=costelem_id)
        AND (NOT itemcost_lowlevel)
@@ -148,7 +148,7 @@ BEGIN
               bomhist_stdunitcost AS stdunitcost,
               bomhist_actunitcost AS actextendedcost,
               bomhist_stdunitcost AS stdextendedcost,
-              NULL, NULL
+              NULL, NULL, NULL, NULL
        FROM bomhist, costelem 
        WHERE ( (bomhist_item_id=costelem_id)
        AND (bomhist_item_type=''E'')
