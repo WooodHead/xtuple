a3f1f7ac4c14d0ad6a55a61d6efcd615082331b3
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2008-12-29 20:14:17 +0000

    Added new overload of dropifexists to include relation argument for constraint types.

--- xtupleserver/trunk/dbscripts/functions/dropifexists.sql
+++ xtupleserver/trunk/dbscripts/functions/dropifexists.sql
@@ -62,6 +62,15 @@ BEGIN
     END;
 
   ELSIF (UPPER(pType) = 'CONSTRAINT') THEN
+    IF( (SELECT count(*)
+           FROM pg_constraint, pg_class, pg_namespace
+          WHERE((conrelid=pg_class.oid)
+            AND (connamespace=pg_namespace.oid)
+            AND (conname=pObject)
+            AND (nspname=pSchema))
+         ) > 1 ) THEN
+      RAISE EXCEPTION 'dropIfExists called on constraint name that matches more than 1 constraint.';
+    END IF;
     SELECT relname INTO _table
     FROM pg_constraint, pg_class, pg_namespace
     WHERE ((conrelid=pg_class.oid)
@@ -107,3 +116,41 @@ BEGIN
 
 END;
 $$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION dropIfExists(TEXT, TEXT, TEXT, TEXT) RETURNS INTEGER AS $$
+DECLARE
+  pType         ALIAS FOR $1;
+  pObject       ALIAS FOR $2;
+  pSchema       ALIAS FOR $3;
+  pRelation     ALIAS FOR $4;
+  _table        TEXT;
+  _query	TEXT;
+BEGIN
+  IF (UPPER(pType) = 'CONSTRAINT') THEN
+    SELECT relname INTO _table
+    FROM pg_constraint, pg_class, pg_namespace
+    WHERE ((conrelid=pg_class.oid)
+      AND  (connamespace=pg_namespace.oid)
+      AND  (conname=pObject)
+      AND  (relname=pRelation)
+      AND  (nspname=pSchema));
+    IF (NOT FOUND) THEN
+      RETURN 0;
+    END IF;
+    _query = 'ALTER TABLE ' || quote_ident(LOWER(pSchema)) || '.' || quote_ident(LOWER(pRelation))
+             || ' DROP CONSTRAINT ' || quote_ident(LOWER(pObject));
+    BEGIN
+      EXECUTE _query;
+    EXCEPTION WHEN undefined_table OR invalid_schema_name THEN
+		RETURN 0;
+	      WHEN OTHERS THEN RAISE EXCEPTION '% %', SQLSTATE, SQLERRM;
+    END;
+
+  ELSE
+    RAISE EXCEPTION 'dropIfExists(%, %, %, %): pType % is not supported when relation is specified', pType, pObject, pSchema, pRelation, pType;
+  END IF;
+
+  RETURN 1;
+END;
+$$ LANGUAGE 'plpgsql';
+
