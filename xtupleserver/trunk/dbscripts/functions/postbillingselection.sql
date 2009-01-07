4f4299da3da4b7a3e0d1a528924a2ceb59ba5aad
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-01-07 00:07:43 +0000

    Issue #8159:remove obsolete code

--- xtupleserver/trunk/dbscripts/functions/postbillingselection.sql
+++ xtupleserver/trunk/dbscripts/functions/postbillingselection.sql
@@ -135,14 +135,15 @@ BEGIN
       _r.coitem_id );
 
 --  Find and mark any Lot/Serial invdetail records associated with this bill
-    UPDATE invdetail SET invdetail_invcitem_id = _invcitemid
-     WHERE (invdetail_id IN (SELECT invdetail_id
-                               FROM coitem, cohead, invhist, invdetail
-                              WHERE ((coitem_cohead_id=cohead_id)
-                                AND  (invdetail_invhist_id=invhist_id)
-                                AND  (invhist_ordnumber = text(cohead_number||''-''||formatSoLineNumber(coitem_id)))
-                                AND  (invdetail_invcitem_id IS NULL)
-                                AND  (coitem_id=_r.coitem_id)) ) );
+--  This is obsolete, replaced by shipitem_invcitem_id
+--    UPDATE invdetail SET invdetail_invcitem_id = _invcitemid
+--     WHERE (invdetail_id IN (SELECT invdetail_id
+--                               FROM coitem, cohead, invhist, invdetail
+--                              WHERE ((coitem_cohead_id=cohead_id)
+--                                AND  (invdetail_invhist_id=invhist_id)
+--                                AND  (invhist_ordnumber = text(cohead_number||''-''||formatSoLineNumber(coitem_id)))
+--                                AND  (invdetail_invcitem_id IS NULL)
+--                                AND  (coitem_id=_r.coitem_id)) ) );
 
 --  Mark any shipped, uninvoiced shipitems for the current coitem as invoiced
     _qtyToInvoice :=  _r.cobill_qty;
