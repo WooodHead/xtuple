afd582fa287f90570ee4fc1248cf0d6505c16edf
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-02-18 21:07:21 +0000

    Issue #8482:test for numeric before cast

--- xtupleserver/trunk/dbscripts/functions/fetchmetricvalue.sql
+++ xtupleserver/trunk/dbscripts/functions/fetchmetricvalue.sql
@@ -3,7 +3,9 @@ DECLARE
   _pMetricName ALIAS FOR $1;
   _returnVal INTEGER;
 BEGIN
-  SELECT metric_value::INTEGER INTO _returnVal
+  SELECT CASE WHEN (isNumeric(metric_value)) THEN metric_value::INTEGER
+              ELSE NULL
+         END INTO _returnVal
     FROM metric
    WHERE metric_name = _pMetricName;
   RETURN _returnVal;
