846654929db6793d702bcd12a713b48bf1d38f09
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-02-20 19:55:24 +0000

    Changes to the way user information is stored and logins are handled. Issue #8502, Issue #7726.

--- xtupleserver/trunk/dbscripts/functions/login.sql
+++ xtupleserver/trunk/dbscripts/functions/login.sql
@@ -1,11 +1,11 @@
 
-CREATE OR REPLACE FUNCTION login() RETURNS integer AS '
+CREATE OR REPLACE FUNCTION login() RETURNS integer AS $$
 DECLARE 
   _p RECORD;
 
 BEGIN
 
-  SELECT usr_id, usr_active INTO _p
+  SELECT usr_id, userCanLogin(usr_username) AS usr_active INTO _p
   FROM usr
   WHERE (usr_username=CURRENT_USER);
 
@@ -13,13 +13,17 @@ BEGIN
     RETURN -1;
 
   ELSIF (NOT _p.usr_active) THEN
+    IF(SELECT metric_value='AdminOnly'
+         FROM metric
+        WHERE metric_name='AllowedUserLogins') THEN
+      RETURN -3;
+    END IF;
     RETURN -2;
-
   ELSE
     RETURN _p.usr_id;
   END IF;
 
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
 
 
