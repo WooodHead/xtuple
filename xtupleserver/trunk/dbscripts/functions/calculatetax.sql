d99d339befa355d45b291ee2af1b03b51b8fbfaf
sidharthb <sidharthb@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-05-21 16:09:11 +0000

    Issue 0008554: Enhanced Tax Internationalization Two new functions "calculatesubtax()" and "calculatetaxdetail()" added and "calculatetax()" modified.

--- xtupleserver/trunk/dbscripts/functions/calculatetax.sql
+++ xtupleserver/trunk/dbscripts/functions/calculatetax.sql
@@ -1,74 +1,22 @@
-CREATE OR REPLACE FUNCTION calculateTax(INTEGER, NUMERIC, NUMERIC) RETURNS NUMERIC AS '
+CREATE OR REPLACE FUNCTION calculatetax(integer, integer, date, integer, numeric)
+  RETURNS numeric AS
+$BODY$
 DECLARE
-  pTaxid ALIAS FOR $1;
-  pSubtotal ALIAS FOR $2;
-  pFreight ALIAS FOR $3;
-BEGIN
-  RETURN calculateTax(pTaxid, pSubtotal, pFreight, ''T'');
-END;
-' LANGUAGE 'plpgsql';
-
-CREATE OR REPLACE FUNCTION calculateTax(INTEGER, NUMERIC, NUMERIC, CHAR) RETURNS NUMERIC AS '
-DECLARE
-  pTaxid ALIAS FOR $1;
-  pSubtotal ALIAS FOR $2;
-  pFreight ALIAS FOR $3;
-  pRate ALIAS FOR $4;
-  _params RECORD;
-  _value NUMERIC;
-  _tax NUMERIC;
-  _taxA NUMERIC;
-  _taxB NUMERIC;
-  _taxC NUMERIC;
+  pTaxZoneId ALIAS FOR  $1;
+  pTaxTypeId ALIAS FOR  $2;
+  pDate ALIAS FOR  $3;
+  pCurrId ALIAS FOR $4;
+  pAmount ALIAS FOR $5;
+  _tottax numeric := 0;  -- total tax
+  
 BEGIN
 
-  SELECT tax_ratea, tax_rateb, tax_ratec,
-         tax_freight, tax_cumulative INTO _params
-  FROM tax
-  WHERE (tax_id=pTaxid);
+  SELECT SUM(taxdetail_tax)
+    INTO _tottax 
+  FROM calculateTaxDetail(pTaxZoneId, pTaxTypeId, pDate, pCurrId, pAmount);
 
-  IF (FOUND) THEN
-    _value := pSubtotal;
-
---    IF (_params.tax_freight) THEN
---      _value := _value + pFreight;
---    END IF;
-
-    _taxA := COALESCE(_params.tax_ratea,0.0) * _value;
-
-    IF (_params.tax_cumulative) THEN
-      _value := _value + _taxA;
-    END IF;
-
-    _taxB := COALESCE(_params.tax_rateb,0.0) * _value;
-
-    IF (_params.tax_cumulative) THEN
-      _value := _value + _taxB;
-    END IF;
-
-    _taxC := COALESCE(_params.tax_ratec,0.0) * _value;
-
-  ELSE
-    _taxA := 0;
-    _taxB := 0;
-    _taxC := 0;
-  END IF;
-
-  _tax := _taxA + _taxB + _taxC;
-
-  IF (UPPER(pRate) = ''A'') THEN
-    RETURN _taxA;
-  END IF;
-
-  IF (UPPER(pRate) = ''B'') THEN
-    RETURN _taxB;
-  END IF;
-
-  IF (UPPER(pRate) = ''C'') THEN
-    RETURN _taxC;
-  END IF;
-
-  RETURN _tax;
-
+  RETURN _tottax;
+  
 END;
-' LANGUAGE 'plpgsql';
+$BODY$
+LANGUAGE 'plpgsql' VOLATILE;
