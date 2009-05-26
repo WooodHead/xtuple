82ee4d9bddfe1a0dabf38b55d235463293fe9d3d
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-05-26 23:53:07 +0000

    issue #8867: Make sure accounts that never have activity still get a trial balance.

--- xtupleserver/trunk/dbscripts/functions/forwardupdateaccount.sql
+++ xtupleserver/trunk/dbscripts/functions/forwardupdateaccount.sql
@@ -1,8 +1,9 @@
 
-CREATE OR REPLACE FUNCTION forwardUpdateAccount(INTEGER) RETURNS INTEGER AS '
+CREATE OR REPLACE FUNCTION forwardUpdateAccount(INTEGER) RETURNS INTEGER AS $$
 DECLARE
   pAccntid ALIAS FOR $1;
   _r RECORD;
+  _trialbalid INTEGER;
 
 BEGIN
   SELECT trialbal_id INTO _r
@@ -13,9 +14,26 @@ BEGIN
    LIMIT 1;
   IF (FOUND) THEN
     RETURN forwardUpdateTrialBalance(_r.trialbal_id);
+  ELSE
+      _trialbalid := nextval('trialbal_trialbal_id_seq');
+      
+      INSERT INTO trialbal
+      ( trialbal_id,
+        trialbal_period_id, trialbal_accnt_id,
+        trialbal_beginning, trialbal_ending,
+        trialbal_debits, trialbal_credits, trialbal_dirty )
+      SELECT
+        _trialbalid,
+        period_id, pAccntid,
+        0, 0,
+        0, 0, FALSE
+      FROM period
+      ORDER BY period_start LIMIT 1;
+
+      RETURN forwardUpdateTrialBalance(_trialbalid);
   END IF;
 
   RETURN -1;
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
 
