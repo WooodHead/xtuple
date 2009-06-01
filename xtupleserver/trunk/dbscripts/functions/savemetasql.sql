fe053dec71e6fea9d6654caa9f2560acb382a3f1
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-06-01 13:58:38 +0000

    add ability to import metasql into a particular schema. if the named schema is null or an empty string, use the current search_path

--- xtupleserver/trunk/dbscripts/functions/savemetasql.sql
+++ xtupleserver/trunk/dbscripts/functions/savemetasql.sql
@@ -5,15 +5,22 @@ END;
 $$ LANGUAGE 'plpgsql';
 
 CREATE OR REPLACE FUNCTION saveMetasql(TEXT,TEXT,TEXT,TEXT,BOOL) RETURNS INTEGER AS $$
+BEGIN
+  RETURN saveMetasql($1, $2, $3, $4, $5, NULL);
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION saveMetasql(TEXT,TEXT,TEXT,TEXT,BOOL,TEXT) RETURNS INTEGER AS $$
 DECLARE
   pGroup	ALIAS FOR $1;
   pName 	ALIAS FOR $2;
   pNotes	ALIAS FOR $3;
   pQuery	ALIAS FOR $4;
   pSystem       ALIAS FOR $5;
+  pSchema       ALIAS FOR $6;
   _metasqlid	INTEGER;
-  _schema       TEXT;
-  _debug        BOOL    := true;
+  _debug        BOOL    := false;
+  _insertstr    TEXT;
   
 BEGIN
 
@@ -35,9 +42,24 @@ BEGIN
     _metasqlid := NEXTVAL('metasql_metasql_id_seq');
 
     BEGIN
-      IF (_debug) THEN RAISE NOTICE 'attempting to insert into pkgmetasql'; END IF;
-      INSERT INTO pkgmetasql 
-      VALUES (_metasqlid,pGroup,pName,pNotes,pQuery);
+      IF (_debug) THEN
+        RAISE NOTICE 'attempting to insert into %.pkgmetasql', pSchema;
+      END IF;
+
+      IF (COALESCE(pSchema, '') = '') THEN
+        INSERT INTO pkgmetasql
+        VALUES (_metasqlid,pGroup,pName,pNotes,pQuery);
+      ELSE
+        _insertstr := 'INSERT INTO ' ||
+                      quote_ident(pSchema) || '.pkgmetasql VALUES (' ||
+                      _metasqlid || ',' ||
+                      quote_literal(pGroup) || ',' || quote_literal(pName) || ',' ||
+                      quote_literal(pNotes) || ',' || quote_literal(pQuery) ||
+                      ');' ;
+        IF (_debug) THEN RAISE NOTICE '%', _insertstr; END IF;
+        EXECUTE _insertstr;
+      END IF;
+
     EXCEPTION WHEN undefined_table THEN
                     IF (_debug) THEN RAISE NOTICE 'insert metasql'; END IF;
                     INSERT INTO metasql 
