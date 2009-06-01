ada645bf52506ec79e1669f6852c258d6ed678f5
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-06-01 14:31:39 +0000

    issue #8866: Insert legacy rows for credit memos with zero amounts.

--- xtupleserver/trunk/updatescripts/from_322/insertAropenZeroAmt.sql
+++ xtupleserver/trunk/updatescripts/from_322/insertAropenZeroAmt.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
---  This query inserts records into aropen for invoices with zero amounts so 
+--  This query inserts records into aropen for invoices and credit memos with zero amounts so 
 --  we can streamline queries concerning aropen history
 
 INSERT INTO aropen
@@ -26,7 +26,36 @@ FROM (SELECT *
       FROM invchead
         LEFT OUTER JOIN aropen ON ((aropen_docnumber=invchead_invcnumber)
                                AND (aropen_doctype='I'))
-      WHERE (aropen_id IS NULL)) AS data
+      WHERE ((invchead_posted)
+      AND (aropen_id IS NULL))) AS data
 WHERE (invoicetotal(invchead_id) = 0);
+
+INSERT INTO aropen
+  ( aropen_id, aropen_username, 
+    aropen_open, aropen_posted,
+    aropen_cust_id, aropen_ponumber,
+    aropen_docnumber, aropen_applyto, aropen_doctype,
+    aropen_docdate, aropen_duedate, aropen_distdate, aropen_terms_id,
+    aropen_amount, aropen_paid,
+    aropen_salesrep_id, aropen_commission_due, aropen_commission_paid,
+    aropen_ordernumber, aropen_notes,
+    aropen_rsncode_id, aropen_curr_id, aropen_closedate )
+SELECT nextval('aropen_aropen_id_seq'), CURRENT_USER,
+    FALSE, FALSE,
+    cmhead_cust_id, cmhead_custponumber,
+    cmhead_number, cmhead_number, 'C',
+    cmhead_docdate, cmhead_docdate, cmhead_gldistdate, -1,
+    0, 0, 
+    cmhead_salesrep_id, 0, FALSE,
+    cmhead_number::text, cmhead_comments,
+    cmhead_rsncode_id, cmhead_curr_id, cmhead_docdate
+FROM (SELECT * 
+      FROM cmhead
+        LEFT OUTER JOIN aropen ON ((aropen_docnumber=cmhead_invcnumber)
+                               AND (aropen_doctype='C'))
+      WHERE ((cmhead_posted)
+      AND (aropen_id IS NULL))) AS data
+WHERE (creditmemototal(cmhead_id) = 0);
+
 
 COMMIT;
