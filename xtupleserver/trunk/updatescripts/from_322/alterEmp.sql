5aaa5ae0d74ca84f1bf9b416d7804d88b40035db
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-06-03 14:54:15 +0000

    Added in some code to prevent unnecessary errors.

--- xtupleserver/trunk/updatescripts/from_322/alterEmp.sql
+++ xtupleserver/trunk/updatescripts/from_322/alterEmp.sql
@@ -2,9 +2,13 @@ BEGIN;
 
 SELECT dropIfExists('VIEW', 'employee', 'api');
 
+ALTER TABLE emp DISABLE TRIGGER ALL;
+
 ALTER TABLE emp ADD COLUMN emp_username TEXT;
 UPDATE emp SET emp_username = (SELECT usr_username FROM usr WHERE usr_id=emp_usr_id);
 ALTER TABLE emp DROP COLUMN emp_usr_id;
+
+ALTER TABLE emp ENABLE TRIGGER ALL;
 
 COMMIT;
 
