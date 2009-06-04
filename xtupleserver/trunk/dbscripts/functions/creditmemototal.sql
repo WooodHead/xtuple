c8a13a703dff6dfdb4b13e48f9014e4093996f6f
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-06-04 13:36:48 +0000

    issue #8554: Update credit memo total to use new tax calculations.

--- xtupleserver/trunk/dbscripts/functions/creditmemototal.sql
+++ xtupleserver/trunk/dbscripts/functions/creditmemototal.sql
@@ -6,9 +6,18 @@ DECLARE
 BEGIN
 
   -- TO DO:  Add in line item taxes
-  SELECT COALESCE(cmhead_freight,0.0) + COALESCE(cmhead_tax,0.0) + COALESCE(cmhead_misc,0.0) +
-         ( SELECT COALESCE(SUM((cmitem_qtycredit * cmitem_qty_invuomratio) * cmitem_unitprice / cmitem_price_invuomratio), 0.0)
+  SELECT COALESCE(cmhead_freight,0.0) + COALESCE(cmhead_misc,0.0) +
+         ( SELECT COALESCE(SUM((cmitem_qtycredit * cmitem_qty_invuomratio) * cmitem_unitprice / cmitem_price_invuomratio), 0.0) 
              FROM cmitem
+            WHERE (cmitem_cmhead_id=pCreditmemoId)
+           ) +
+         ( SELECT COALESCE(SUM(taxhist_tax),0.0) 
+             FROM cmheadtax
+            WHERE (taxhist_parent_id=pCreditmemoId)
+           ) +
+         ( SELECT COALESCE(SUM(taxhist_tax), 0.0) 
+             FROM cmitemtax
+               JOIN cmitem ON (cmitem_id=taxhist_parent_id)
             WHERE (cmitem_cmhead_id=pCreditmemoId)
            ) INTO _result
   FROM cmhead
