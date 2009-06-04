32556ec8da36d2b44d280283dd37531065fba3b3
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-06-04 16:15:13 +0000

    Fixed a couple issues including some from issue #8905 for the 3.3.0Alpha update package.

--- xtupleserver/trunk/updatescripts/from_322/updateAropen.sql
+++ xtupleserver/trunk/updatescripts/from_322/updateAropen.sql
@@ -1,5 +1,7 @@
 BEGIN;
 
+ALTER TABLE aropen DISABLE TRIGGER ALL;
+
 --Populate historic close dates
 UPDATE aropen SET aropen_closedate=(SELECT MAX(arapply_postdate) FROM arapply WHERE arapply_target_aropen_id=aropen_id)
 WHERE ((NOT aropen_open)
@@ -10,5 +12,7 @@ UPDATE aropen SET aropen_closedate=(SELECT MAX(arapply_postdate) FROM arapply WH
 WHERE ((NOT aropen_open)
 AND (aropen_closedate IS NULL)
 AND (aropen_doctype IN ('C','R')));
+
+ALTER TABLE aropen ENABLE TRIGGER ALL;
 
 COMMIT;
