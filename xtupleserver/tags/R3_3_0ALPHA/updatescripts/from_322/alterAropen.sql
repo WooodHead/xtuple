bfe34f116b7a68d5c161e7e8b3b74dcbccdb0a8f
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-06-04 16:15:30 +0000

    Fixed a couple issues including some from issue #8905 for the 3.3.0Alpha update package. (on tag)

--- xtupleserver/tags/R3_3_0ALPHA/updatescripts/from_322/alterAropen.sql
+++ xtupleserver/tags/R3_3_0ALPHA/updatescripts/from_322/alterAropen.sql
@@ -1,5 +1,7 @@
 BEGIN;
 
+ALTER TABLE aropen DISABLE TRIGGER ALL;
+
 ALTER TABLE aropen ADD COLUMN aropen_curr_rate NUMERIC(11,6);
 UPDATE aropen SET
   aropen_curr_rate = curr_rate
@@ -7,5 +9,7 @@ FROM curr_rate
 WHERE ( (aropen_curr_id = curr_rate.curr_id)
   AND (aropen_docdate BETWEEN curr_effective AND curr_expires) );
 ALTER TABLE aropen ALTER COLUMN aropen_curr_rate SET NOT NULL;
+
+ALTER TABLE aropen ENABLE TRIGGER ALL;
 
 COMMIT;
