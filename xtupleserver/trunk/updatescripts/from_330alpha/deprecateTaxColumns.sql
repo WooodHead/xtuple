7d5c96ff348cfc341a365504f3edf7917655020d
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-06-04 01:03:46 +0000

    issue #8550: Remove obsolete functions, columns and deprecate fields.

---
+++ xtupleserver/trunk/updatescripts/from_330alpha/deprecateTaxColumns.sql
@@ -0,0 +1,8 @@
+BEGIN;
+
+-- Drop columns in tax table
+
+COMMENT ON COLUMN cmhead.cmhead_adjtaxtype_id IS 'Deprecated column - DO NOT USE';
+COMMENT ON COLUMN cmhead.cmhead_freighttaxtype_id IS 'Deprecated column - DO NOT USE';
+
+COMMIT;
