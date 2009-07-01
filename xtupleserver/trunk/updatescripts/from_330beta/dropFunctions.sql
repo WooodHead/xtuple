dfaa1dbbb9e28e11838dc9d0377c0e3a9258a50b
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-07-01 12:55:33 +0000

    issue #8554:  Remove uneeded functions.

--- xtupleserver/trunk/updatescripts/from_330beta/dropFunctions.sql
+++ xtupleserver/trunk/updatescripts/from_330beta/dropFunctions.sql
@@ -2,5 +2,7 @@ BEGIN;
 
 SELECT dropIfExists('FUNCTION', 'calculatetaxbasis( integer, integer, date, integer, numeric )');
 SELECT dropIfExists('FUNCTION', 'createardebitmemo( integer, text, text, text, date, numeric, text, integer, integer, integer, date, integer, integer, numeric, integer )');
+SELECT dropIfExists('FUNCTION', 'changeinvoicetaxzone(integer, integer)');
+SELECT dropIfExists('FUNCTION', 'changecobtaxzone(integer, integer)');
 
 COMMIT;
