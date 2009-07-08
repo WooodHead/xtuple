04f971791b00f06e68df110850384a660165386e
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-07-08 18:15:20 +0000

    Incorporated changes from patch to fix FK error when data is left remaining. Issue #8851.

--- xtupleserver/trunk/dbscripts/functions/returncompleteshipment.sql
+++ xtupleserver/trunk/dbscripts/functions/returncompleteshipment.sql
@@ -1,15 +1,19 @@
-CREATE OR REPLACE FUNCTION returnCompleteShipment(INTEGER) RETURNS INTEGER AS '
+CREATE OR REPLACE FUNCTION returnCompleteShipment(INTEGER) RETURNS INTEGER AS $$
 BEGIN
   RETURN returnCompleteShipment($1, 0, CURRENT_TIMESTAMP);
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
 
-CREATE OR REPLACE FUNCTION returnCompleteShipment(INTEGER, INTEGER, TIMESTAMP WITH TIME ZONE) RETURNS INTEGER AS '
+CREATE OR REPLACE FUNCTION returnCompleteShipment(INTEGER, INTEGER, TIMESTAMP WITH TIME ZONE) RETURNS INTEGER AS $$
 DECLARE
   pshipheadid		ALIAS FOR $1;
-  _itemlocSeries	INTEGER			 := $2;
+  _itemlocSeries	INTEGER := $2;
   _timestamp		TIMESTAMP WITH TIME ZONE := $3;
   _r RECORD;
+  _result 		RECORD;
+  _shiphead_number	TEXT := '';
+  _count		INTEGER := 0;
+  _countsum		INTEGER := 0;
 
 BEGIN
   FOR _r IN SELECT shipitem_id
@@ -20,6 +24,29 @@ BEGIN
     _itemlocSeries := returnShipmentTransaction(_r.shipitem_id, _itemlocSeries, _timestamp);
   END LOOP;
 
+  FOR _result IN SELECT shiphead_number
+                   FROM shiphead
+                  WHERE ( (shiphead_id=pshipheadid) ) LOOP
+    _shiphead_number := _result.shiphead_number;
+  END LOOP;
+
+  SELECT COUNT(*) INTO _count
+    FROM shipdata
+   WHERE(shipdata_shiphead_number=_shiphead_number);
+
+  SELECT COUNT(*) INTO _countsum
+    FROM shipdatasum
+   WHERE(shipdatasum_shiphead_number=_shiphead_number);
+
+  IF (_count > 0) THEN
+    DELETE FROM shipdata
+     WHERE(shipdata_shiphead_number=_shiphead_number);
+  END IF;
+  IF (_countsum > 0) THEN
+    DELETE FROM shipdatasum
+     WHERE(shipdatasum_shiphead_number=_shiphead_number);
+  END IF;
+
   DELETE FROM pack
    WHERE(pack_shiphead_id=pshipheadid);
   DELETE FROM shiphead
@@ -28,4 +55,4 @@ BEGIN
   RETURN _itemlocSeries;
 
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
