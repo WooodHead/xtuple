02de72a46921fa5d93d0ffc2e90a235fd49bb3be
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-07-24 13:03:36 +0000

    Change to source file that was part of fix in a package. Issue #9231.

--- xtupleserver/trunk/updatescripts/from_330alpha/alterPoitem.sql
+++ xtupleserver/trunk/updatescripts/from_330alpha/alterPoitem.sql
@@ -1,7 +1,6 @@
 BEGIN;
 
-ALTER TABLE poitem ADD COLUMN poitem_tax_recoverable BOOLEAN;
-UPDATE poitem SET poitem_tax_recoverable=true;
+ALTER TABLE poitem ADD COLUMN poitem_tax_recoverable BOOLEAN DEFAULT true;
 ALTER TABLE poitem ALTER COLUMN poitem_tax_recoverable SET NOT NULL;
 ALTER TABLE poitem ALTER COLUMN poitem_tax_recoverable SET DEFAULT true;
 
