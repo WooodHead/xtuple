75509f4c9130217b6116f8f395931e43d07057ec
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-08-17 12:41:43 +0000

    Creating a new RC2 tag.

---
+++ xtuple/tags/R3_3_0RC2/guiclient/createPlannedOrdersByPlannerCode.cpp
@@ -0,0 +1,101 @@
+/*
+ * This file is part of the xTuple ERP: PostBooks Edition, a free and
+ * open source Enterprise Resource Planning software suite,
+ * Copyright (c) 1999-2009 by OpenMFG LLC, d/b/a xTuple.
+ * It is licensed to you under the Common Public Attribution License
+ * version 1.0, the full text of which (including xTuple-specific Exhibits)
+ * is available at www.xtuple.com/CPAL.  By using this software, you agree
+ * to be bound by its terms.
+ */
+
+#include "createPlannedOrdersByPlannerCode.h"
+
+#include <QVariant>
+#include <QMessageBox>
+#include <QSqlError>
+#include "submitAction.h"
+#include <metasql.h>
+#include "mqlutil.h"
+
+createPlannedOrdersByPlannerCode::createPlannedOrdersByPlannerCode(QWidget* parent, const char* name, bool modal, Qt::WFlags fl)
+  : XDialog(parent, name, modal, fl)
+{
+  setupUi(this);
+
+
+  // signals and slots connections
+  connect(_close, SIGNAL(clicked()), this, SLOT(reject()));
+  connect(_create, SIGNAL(clicked()), this, SLOT(sCreate()));
+  connect(_submit, SIGNAL(clicked()), this, SLOT(sSubmit()));
+
+  if (!_metrics->boolean("EnableBatchManager"))
+    _submit->hide();
+
+  _plannerCode->setType(ParameterGroup::PlannerCode);
+}
+
+createPlannedOrdersByPlannerCode::~createPlannedOrdersByPlannerCode()
+{
+  // no need to delete child widgets, Qt does it all for us
+}
+
+void createPlannedOrdersByPlannerCode::languageChange()
+{
+  retranslateUi(this);
+}
+
+void createPlannedOrdersByPlannerCode::sCreate()
+{
+  ParameterList params;
+  if (! setParams(params))
+    return;
+  MetaSQLQuery mql = mqlLoad("schedule", "create");
+  q = mql.toQuery(params);
+  if (q.lastError().type() != QSqlError::NoError)
+  {
+    systemError(this, q.lastError().databaseText(), __FILE__, __LINE__);
+    return;
+  }
+
+  accept();
+}
+
+
+void createPlannedOrdersByPlannerCode::sSubmit()
+{
+  ParameterList params;
+  if (! setParams(params))
+    return;
+
+  submitAction newdlg(this, "", TRUE);
+  newdlg.set(params);
+
+  if (newdlg.exec() == XDialog::Accepted)
+    accept();
+}
+
+bool createPlannedOrdersByPlannerCode::setParams(ParameterList &pParams)
+{
+  if (!_cutOffDate->isValid())
+  {
+    QMessageBox::warning( this, tr("Enter Cut Off Date"),
+                          tr( "You must enter a valid Cut Off Date before\n"
+                              "creating Planned Orders." ));
+    _cutOffDate->setFocus();
+    return false;
+  }
+
+  pParams.append("action_name", "RunMRP");
+
+  pParams.append("MPS", QVariant(false));
+  pParams.append("planningType", "M");
+  _plannerCode->appendValue(pParams);
+  _warehouse->appendValue(pParams); 
+  
+  pParams.append("cutOffDate", _cutOffDate->date());
+  pParams.append("cutoff_offset", QDate::currentDate().daysTo(_cutOffDate->date()));
+  pParams.append("deleteFirmed", QVariant(_deleteFirmed->isChecked()));
+
+  return true;
+}
+
---
+++ xtuple/tags/R3_3_0RC2/guiclient/itemSources.h
@@ -0,0 +1,44 @@
+/*
+ * This file is part of the xTuple ERP: PostBooks Edition, a free and
+ * open source Enterprise Resource Planning software suite,
+ * Copyright (c) 1999-2009 by OpenMFG LLC, d/b/a xTuple.
+ * It is licensed to you under the Common Public Attribution License
+ * version 1.0, the full text of which (including xTuple-specific Exhibits)
+ * is available at www.xtuple.com/CPAL.  By using this software, you agree
+ * to be bound by its terms.
+ */
+
+#ifndef ITEMSOURCES_H
+#define ITEMSOURCES_H
+
+#include "guiclient.h"
+#include "xwidget.h"
+
+#include "ui_itemSources.h"
+
+class itemSources : public XWidget, public Ui::itemSources
+{
+    Q_OBJECT
+
+public:
+    itemSources(QWidget* parent = 0, const char* name = 0, Qt::WFlags fl = Qt::Window);
+    ~itemSources();
+
+protected slots:
+    virtual void languageChange();
+
+    virtual void init();
+    virtual void sPrint();
+    virtual void sNew();
+    virtual void sEdit();
+    virtual void sView();
+    virtual void sCopy();
+    virtual void sDelete();
+    virtual void sPopulateMenu( QMenu * menuThis );
+    virtual void sFillList();
+    virtual void sSearch( const QString & pTarget );
+
+
+};
+
+#endif // ITEMSOURCES_H
