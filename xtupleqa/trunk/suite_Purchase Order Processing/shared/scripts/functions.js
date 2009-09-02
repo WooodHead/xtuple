ec66534af44de22ccd9ac5bc0f3e8d6619c010b5
srksharma <srksharma@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-09-02 13:07:12 +0000

    Initiated creating Test Suite for Purchase Order Processing per the Use Cases

---
+++ xtupleqa/trunk/suite_Purchase Order Processing/shared/scripts/functions.js
@@ -0,0 +1,72 @@
+//-***-This script doc contains all the common code libraries/functions required by the Main driver script-***-
+
+
+
+//--------Login into Appl----------
+function loginAppl(userrole)
+{
+   
+    var set = testData.dataset("login.tsv");
+    var url, db, port, pwd,realname,username;
+    db=pwd=port=url=realname=username="";
+    for(var records in set)
+    {
+        url=testData.field(set[records],"HOST");
+        db=testData.field(set[records],"DB");
+        port=testData.field(set[records],"PORT");
+        pwd=testData.field(set[records],"PASSWORD");
+        role=testData.field(set[records],"ROLE");
+        username=testData.field(set[records],"USERNAME");
+        realname=testData.field(set[records],"REALNAME");
+        if(userrole==role) break;
+              
+    }
+
+    if(userrole!=role)
+    {
+        test.fatal("Please enter user details in login.tsv for the role: "+userrole);
+        exit(1);
+    }
+ 
+    
+    waitForObject(":Log In.Options..._QPushButton");
+    clickButton(":Log In.Options..._QPushButton");
+    waitForObject(":_server_QLineEdit");
+    if(findObject(":_server_QLineEdit").text!= url)
+    {
+        findObject(":_server_QLineEdit").text=url;
+        test.log("URL Changed to: "+url);
+    }
+    if(findObject(":_database_QLineEdit").text!=db)
+    {
+        findObject(":_database_QLineEdit").text=db;
+        test.log("Database Changed to: "+db);
+    }
+    if(findObject(":_port_QLineEdit").text!=port)
+    {
+        findObject(":_port_QLineEdit").text=port;
+        test.log("Port Changed to:" + port);
+    }
+    clickButton(":Login Options.Save_QPushButton");
+    waitForObject(":_username_QLineEdit");    
+    type(":_username_QLineEdit", username);
+    waitForObject(":_username_QLineEdit");
+    type(":_username_QLineEdit", "<Tab>");
+    waitForObject(":_password_QLineEdit");
+    type(":_password_QLineEdit", pwd);
+    waitForObject(":_password_QLineEdit");
+    type(":_password_QLineEdit", "<Return>");
+    test.log("Logged in Application");
+       
+}
+
+
+
+
+
+
+
+
+
+
+
