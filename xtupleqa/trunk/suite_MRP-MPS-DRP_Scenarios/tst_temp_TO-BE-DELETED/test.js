f3272d27b37e2d29f5bb49e3c6ade8caff3a6d27
srksharma <srksharma@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-09-10 14:11:13 +0000

    Automation Test Suite for MRP-MPS-DRP testing

---
+++ xtupleqa/trunk/suite_MRP-MPS-DRP_Scenarios/tst_temp_TO-BE-DELETED/test.js
@@ -0,0 +1,18 @@
+function main()
+{
+    var sWidgetTreeControl = ":List Profit Centers._prftcntr_XTreeWidget";
+    waitForObject(sWidgetTreeControl);
+    var obj_TreeWidget = findObject(sWidgetTreeControl);
+    var obj_TreeRootItem=obj_TreeWidget.invisibleRootItem();
+    var iNumberOfRootItems = obj_TreeRootItem.childCount();
+    var obj_TreeTopLevelItem = obj_TreeRootItem.child(0);
+    var sNameOfRootItem = obj_TreeTopLevelItem.text(0);
+    for(i=1;sNameOfRootItem!="01" || i<iNumberOfRootItems ;i++)
+    {
+        obj_TreeTopLevelItem = obj_TreeRootItem.child(i);
+        sNameOfRootItem = obj_TreeTopLevelItem.text(0);
+    }
+    if(sNameOfRootItem=="01")
+        test.pass("Profit Center Number: 01 created");
+
+}
