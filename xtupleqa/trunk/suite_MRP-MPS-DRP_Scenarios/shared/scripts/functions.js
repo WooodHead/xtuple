96b756959321606158a4ca36d747b315b9c49a76
srksharma <srksharma@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-09-16 14:21:28 +0000

    Develped new scenarios - completed till MRP MULTILEVEL DEMAND – SIMPLE

--- xtupleqa/trunk/suite_MRP-MPS-DRP_Scenarios/shared/scripts/functions.js
+++ xtupleqa/trunk/suite_MRP-MPS-DRP_Scenarios/shared/scripts/functions.js
@@ -212,7 +212,7 @@ function NewSO(item, quant)
     clickButton(":frame.New_QPushButton");
     waitForObject(":_headerPage...._QPushButton");
     clickButton(":_headerPage...._QPushButton");
-    doubleClickItem(":_listTab_XTreeWidget", "TTOYS", 44, 15, 0, Qt.LeftButton);
+    doubleClickItem(":_listTab_XTreeWidget", "TTOYS", 0, 0, 0, Qt.LeftButton);
     waitForObject(":Sales Order.qt_tabwidget_tabbar_QTabBar");
     clickTab(":Sales Order.qt_tabwidget_tabbar_QTabBar", "Line Items");
     waitForObject(":_lineItemsPage.New_QPushButton");
@@ -510,3 +510,50 @@ function FirmPlndOrder()
     clickButton(":Planned Orders by Planner Code.Close_QPushButton");
 
 }
+
+
+
+function replaceSubstring(inputString, fromString, toString) {
+   // Goes through the inputString and replaces every occurrence of fromString with toString
+   var temp = inputString;
+   if (fromString == "") {
+      return inputString;
+   }
+   if (toString.indexOf(fromString) == -1) { // If the string being replaced is not a part of the replacement string (normal situation)
+      while (temp.indexOf(fromString) != -1) {
+         var toTheLeft = temp.substring(0, temp.indexOf(fromString));
+         var toTheRight = temp.substring(temp.indexOf(fromString)+fromString.length, temp.length);
+         temp = toTheLeft + toString + toTheRight;
+      }
+   } else { // String being replaced is part of replacement string (like "+" being replaced with "++") - prevent an infinite loop
+      var midStrings = new Array("~", "`", "_", "^", "#");
+      var midStringLen = 1;
+      var midString = "";
+      // Find a string that doesn't exist in the inputString to be used
+      // as an "inbetween" string
+      while (midString == "") {
+         for (var i=0; i < midStrings.length; i++) {
+            var tempMidString = "";
+            for (var j=0; j < midStringLen; j++) { tempMidString += midStrings[i]; }
+            if (fromString.indexOf(tempMidString) == -1) {
+               midString = tempMidString;
+               i = midStrings.length + 1;
+            }
+         }
+      } // Keep on going until we build an "inbetween" string that doesn't exist
+      // Now go through and do two replaces - first, replace the "fromString" with the "inbetween" string
+      while (temp.indexOf(fromString) != -1) {
+         var toTheLeft = temp.substring(0, temp.indexOf(fromString));
+         var toTheRight = temp.substring(temp.indexOf(fromString)+fromString.length, temp.length);
+         temp = toTheLeft + midString + toTheRight;
+      }
+      // Next, replace the "inbetween" string with the "toString"
+      while (temp.indexOf(midString) != -1) {
+         var toTheLeft = temp.substring(0, temp.indexOf(midString));
+         var toTheRight = temp.substring(temp.indexOf(midString)+midString.length, temp.length);
+         temp = toTheLeft + toString + toTheRight;
+      }
+   } // Ends the check to see if the string being replaced is part of the replacement string or not
+   return temp; // Send the updated string back to the user
+} // Ends the "replaceSubstring" function
+
