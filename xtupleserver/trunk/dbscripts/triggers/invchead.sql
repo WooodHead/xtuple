c1ffb9e92844704eb6827508ee5bee7bce6f378c
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-09-24 18:56:53 +0000

    Issue #9493:tweak conditions that cause tax to be recalculated

--- xtupleserver/trunk/dbscripts/triggers/invchead.sql
+++ xtupleserver/trunk/dbscripts/triggers/invchead.sql
@@ -45,16 +45,11 @@ BEGIN
 -- Update row
   IF (TG_OP = 'UPDATE') THEN
 
-  -- Calculate Tax
-    IF (COALESCE(NEW.invchead_taxzone_id,-1) <> COALESCE(OLD.invchead_taxzone_id,-1)) THEN
-      UPDATE invcitem SET invcitem_taxtype_id=getItemTaxType(invcitem_item_id,NEW.invchead_taxzone_id)
-      WHERE (invcitem_invchead_id=NEW.invchead_id);
-    END IF;
-  
     IF ( (NEW.invchead_freight <> OLD.invchead_freight) OR
          (COALESCE(NEW.invchead_taxzone_id,-1) <> COALESCE(OLD.invchead_taxzone_id,-1)) OR
          (NEW.invchead_invcdate <> OLD.invchead_invcdate) OR
          (NEW.invchead_curr_id <> OLD.invchead_curr_id) ) THEN
+  -- Calculate invchead Tax
       PERFORM calculateTaxHist( 'invcheadtax',
                                 NEW.invchead_id,
                                 NEW.invchead_taxzone_id,
@@ -62,17 +57,30 @@ BEGIN
                                 NEW.invchead_invcdate,
                                 NEW.invchead_curr_id,
                                 NEW.invchead_freight );
-      PERFORM calculateTaxHist( 'invcitemtax',
-                                invcitem_id,
-                                NEW.invchead_taxzone_id,
-                                invcitem_taxtype_id,
-                                NEW.invchead_invcdate,
-                                NEW.invchead_curr_id,
-                                (invcitem_billed * invcitem_qty_invuomratio) *
-                                (invcitem_price / invcitem_price_invuomratio) )
-      FROM invcitem
-      WHERE (invcitem_invchead_id = NEW.invchead_id);
     END IF;
+
+    IF ( (COALESCE(NEW.invchead_taxzone_id,-1) <> COALESCE(OLD.invchead_taxzone_id,-1)) OR
+         (NEW.invchead_invcdate <> OLD.invchead_invcdate) OR
+         (NEW.invchead_curr_id <> OLD.invchead_curr_id) ) THEN
+  -- Calculate invcitem Tax
+      IF (COALESCE(NEW.invchead_taxzone_id,-1) <> COALESCE(OLD.invchead_taxzone_id,-1)) THEN
+    -- Invcitem trigger will calculate tax
+        UPDATE invcitem SET invcitem_taxtype_id=getItemTaxType(invcitem_item_id,NEW.invchead_taxzone_id)
+        WHERE (invcitem_invchead_id=NEW.invchead_id);
+      ELSE
+        PERFORM calculateTaxHist( 'invcitemtax',
+                                  invcitem_id,
+                                  NEW.invchead_taxzone_id,
+                                  invcitem_taxtype_id,
+                                  NEW.invchead_invcdate,
+                                  NEW.invchead_curr_id,
+                                  (invcitem_billed * invcitem_qty_invuomratio) *
+                                  (invcitem_price / invcitem_price_invuomratio) )
+        FROM invcitem
+        WHERE (invcitem_invchead_id = NEW.invchead_id);
+      END IF;
+    END IF;
+
   END IF;
 
   RETURN NEW;
