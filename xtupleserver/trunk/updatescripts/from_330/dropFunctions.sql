73360c2470b0d2a06b4fde8e5d73cf727d21e473
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-09-30 14:01:32 +0000

    Moved xtmfg only functionn to xtmfg package.

--- xtupleserver/trunk/updatescripts/from_330/dropFunctions.sql
+++ xtupleserver/trunk/updatescripts/from_330/dropFunctions.sql
@@ -2,6 +2,7 @@ BEGIN;
 
 SELECT dropIfExists('FUNCTION', 'deleteworkcenter(integer)');
 SELECT dropIfExists('FUNCTION', 'explodePlannedOrder(INTEGER, BOOLEAN)');
+SELECT dropIfExists('FUNCTION', 'checkBOOSitePrivs(INTEGER)');
 
 COMMIT;
 
