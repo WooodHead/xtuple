71f72430a55487ed98742bbbee4a61b416a2e904
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-10-16 15:11:34 +0000

    issue #8333 - move postProduction to xtmfg

--- xtupleserver/trunk/updatescripts/from_330/dropFunctions.sql
+++ xtupleserver/trunk/updatescripts/from_330/dropFunctions.sql
@@ -3,7 +3,7 @@ BEGIN;
 SELECT dropIfExists('FUNCTION', 'deleteworkcenter(integer)');
 SELECT dropIfExists('FUNCTION', 'explodePlannedOrder(INTEGER, BOOLEAN)');
 SELECT dropIfExists('FUNCTION', 'checkBOOSitePrivs(INTEGER)');
+SELECT dropIfExists('FUNCTION', 'calcwooperstart(INTEGER, INTEGER)');
 SELECT dropIfExists('FUNCTION', 'unWoClockOut(INTEGER)');
 
 COMMIT;
-
