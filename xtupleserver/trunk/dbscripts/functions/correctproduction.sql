7ca9c347a874ae578a972c26ac607dc5b6223f1c
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-10-20 20:32:32 +0000

    issue #8333 - move xtmfg-specific reference out of core

--- xtupleserver/trunk/dbscripts/functions/correctproduction.sql
+++ xtupleserver/trunk/dbscripts/functions/correctproduction.sql
@@ -1,35 +1,34 @@
 CREATE OR REPLACE FUNCTION correctProduction(INTEGER, NUMERIC, BOOLEAN, BOOLEAN) RETURNS INTEGER AS $$
-DECLARE
-  pWoid ALIAS FOR $1;
-  pQty ALIAS FOR $2;
-  pBackflush ALIAS FOR $3;
-  pBackflushOperations ALIAS FOR $4;
-
 BEGIN
-
-  RETURN  correctProduction(pWoid, pQty, pBackflush, pBackflushOperations, 0);
-
+  RAISE NOTICE 'correctProduction(INTEGER, NUMERIC, BOOLEAN, BOOLEAN) has been deprecated. Use corrrectProduction(INTEGER, NUMERIC, BOOLEAN, INTEGER) or a package-specific version instead.';
+  RETURN  correctProduction($1, $2, $3, 0);
 END;
 $$ LANGUAGE 'plpgsql';
 
 
 CREATE OR REPLACE FUNCTION correctProduction(INTEGER, NUMERIC, BOOLEAN, BOOLEAN, INTEGER) RETURNS INTEGER AS $$
+BEGIN
+  RAISE NOTICE 'correctProduction(INTEGER, NUMERIC, BOOLEAN, BOOLEAN, INTEGER) has been deprecated. Use corrrectProduction(INTEGER, NUMERIC, BOOLEAN, INTEGER) or a package-specific version instead.';
+  RETURN correctProduction($1, $2, $3, $5);
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION correctProduction(INTEGER, NUMERIC, BOOLEAN, INTEGER) RETURNS INTEGER AS $$
 DECLARE
-  pWoid ALIAS FOR $1;
-  pQty ALIAS FOR $2;
-  pBackflush ALIAS FOR $3;
-  pBackflushOperations ALIAS FOR $4;
-  pItemlocSeries ALIAS FOR $5;
-  _invhistid INTEGER;
-  _itemlocSeries INTEGER;
-  _r RECORD;
-  _wooperitem RECORD;
+  pWoid          ALIAS FOR $1;
+  pQty           ALIAS FOR $2;
+  pBackflush     ALIAS FOR $3;
+  pItemlocSeries ALIAS FOR $4;
+  _invhistid        INTEGER;
+  _itemlocSeries    INTEGER;
+  _r                RECORD;
   _parentWIPAccntid INTEGER;
-  _parentQty NUMERIC;
-  _qty NUMERIC;
-  _invqty NUMERIC;
-  _rntime NUMERIC;
-  _sense TEXT;
+  _parentQty        NUMERIC;
+  _qty              NUMERIC;
+  _invqty           NUMERIC;
+  _sense            TEXT;
+  _status           TEXT;
+  _type             TEXT;
 
 BEGIN
 
@@ -40,26 +39,21 @@ BEGIN
   ELSE
     _sense := 'to';
   END IF;
+
+  SELECT item_type, roundQty(item_fractional, pQty), wo_status
+    INTO _type, _parentQty, _status
+    FROM wo, itemsite, item
+   WHERE ((wo_itemsite_id=itemsite_id)
+      AND (itemsite_item_id=item_id)
+      AND (wo_id=pWoid));
   
-  IF ( ( SELECT (NOT (wo_status='I'))
-         FROM wo
-         WHERE (wo_id=pWoid) ) ) THEN
+  IF (_status != 'I') THEN
     RETURN -1;
   END IF;
 
-  IF ( ( SELECT (item_type IN ('B','J'))
-         FROM wo, itemsite, item
-         WHERE ( (wo_itemsite_id=itemsite_id)
-          AND (itemsite_item_id=item_id)
-          AND (wo_id=pWoid) ) ) ) THEN
+  IF (_type = 'J') THEN
     RETURN -2;
   END IF;
-
-  SELECT roundQty(item_fractional, pQty) INTO _parentQty
-  FROM wo, itemsite, item
-  WHERE ( (wo_itemsite_id=itemsite_id)
-   AND (itemsite_item_id=item_id)
-   AND (wo_id=pWoid) );
 
   IF (pItemlocSeries = 0) THEN
     SELECT NEXTVAL('itemloc_series_seq') INTO _itemlocSeries;
@@ -67,7 +61,7 @@ BEGIN
     _itemlocSeries := pItemlocSeries;
   END IF;
 
---  Cache the parent WIP account (No SELECT ... INTO x, ... INTO x;!)
+  --  Cache the parent WIP account (No SELECT ... INTO x, ... INTO x;!)
   SELECT costcat_wip_accnt_id INTO _parentWIPAccntid
   FROM wo, itemsite, costcat
   WHERE ( (wo_itemsite_id=itemsite_id)
@@ -88,7 +82,7 @@ BEGIN
                 AND (womatl_issuemethod = 'L')
                 AND (wo_id=pWoid) ) FOR UPDATE LOOP
 
---  Cache the qty to be issued
+      --  Cache the qty to be issued
       _qty = roundQty(_r.item_fractional, (_parentQty * _r.womatl_qtyper * (1 + _r.womatl_scrap)));
       _invqty = itemuomtouom(_r.item_id, _r.womatl_uom_id, NULL, (_parentQty * _r.womatl_qtyper * (1 + _r.womatl_scrap)));
 
@@ -105,7 +99,7 @@ BEGIN
        AND (itemsite_costcat_id=costcat_id)
        AND (itemsite_id=_r.itemsite_id) );
 
---  Decrease the parent W/O's WIP value by the value of the returned components
+      --  Decrease the parent W/O's WIP value by the value of the returned components
       UPDATE wo
       SET wo_wipvalue = (wo_wipvalue -
                          (CASE WHEN(_r.itemsite_costmethod='A')
@@ -126,39 +120,7 @@ BEGIN
 
   END IF;
 
---  If request, backflush the wooper load
-  IF (pBackflushOperations) THEN
-
-    FOR _wooperitem IN SELECT wooper_id, wooper_qtyrcv, wooper_sutime,
-                              wooper_suconsumed, wooper_sucomplete,
-                              wooper_rncomplete, wooper_rnqtyper,
-                              wooper_invproduomratio
-                       FROM wooper
-                       WHERE (wooper_wo_id=pWoid)
-                       ORDER BY wooper_seqnumber LOOP
-
---  If this is a complete return reverse backflush Setup Time for the reported Qty.
-
-        IF (COALESCE(_wooperitem.wooper_qtyrcv,0) = _parentQty) THEN
-          PERFORM postSutime(_wooperitem.wooper_id, _wooperitem.wooper_sutime * -1, TRUE) AS result;
-        END IF;
-
---  Reverse backflush Run Time for the reported Qty.
-
-        _rnTime := (_wooperitem.wooper_rnqtyper / _wooperitem.wooper_invproduomratio * _parentQty * -1);
-        PERFORM postRntime(wooper_id, _rnTime, TRUE, _wooperitem.wooper_invproduomratio * _parentQty * -1 )
-        FROM wooper
-        WHERE (wooper_id=_wooperitem.wooper_id);
-
-        UPDATE wooper
-        SET wooper_qtyrcv = (COALESCE(wooper_qtyrcv,0) - _parentQty)
-        WHERE (wooper_id=_wooperitem.wooper_id);
-
-    END LOOP;
-
-  END IF;
-
---  Post the inventory transaction
+  --  Post the inventory transaction
   SELECT postInvTrans( itemsite_id, 'RM', (_parentQty * -1),
                        'W/O', 'WO', formatwonumber(pWoid), '',
                        ('Correct Receive Inventory ' || item_number || ' ' || _sense || ' Manufacturing'),
@@ -173,7 +135,7 @@ BEGIN
    AND (itemsite_costcat_id=costcat_id)
    AND (wo_id=pWoid) );
 
---  Decrease this W/O's qty. received and increase its WIP value
+  --  Decrease this W/O's qty. received and increase its WIP value
   UPDATE wo
   SET wo_qtyrcv = (wo_qtyrcv - _parentQty),
       wo_wipvalue = (wo_wipvalue + (CASE WHEN(itemsite_costmethod='A') THEN ((wo_postedvalue - wo_wipvalue) / wo_qtyrcv) ELSE stdcost(itemsite_item_id) END * _parentQty))
