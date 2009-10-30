c7ae122cd5bf701cced8c369cbff41aee7109736
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-10-30 18:13:56 +0000

    Issue #9859:qualify xtmfg schema

--- xtupleserver/trunk/dbscripts/functions/formatwooperseq.sql
+++ xtupleserver/trunk/dbscripts/functions/formatwooperseq.sql
@@ -1,18 +1,18 @@
-CREATE OR REPLACE FUNCTION formatwooperseq(INTEGER) RETURNS TEXT AS '
+CREATE OR REPLACE FUNCTION formatwooperseq(INTEGER) RETURNS TEXT AS $$
 DECLARE
   pWooperId  ALIAS FOR $1;
-  _result TEXT;
+  _result    TEXT;
 
 BEGIN
 
   IF pWooperId = -1 THEN
-    RETURN '''';
+    RETURN '';
   ELSE
     SELECT wooper_seqnumber INTO _result
-    FROM wooper
+    FROM xtmfg.wooper
     WHERE (wooper_id=pWooperId);
   END IF;
 
   RETURN _result;
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
