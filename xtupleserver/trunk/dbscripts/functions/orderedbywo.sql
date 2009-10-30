a26374efd60351da1f858403199cc5ceee0cbf64
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-10-30 19:19:12 +0000

    Issue #9861:qualify xtmfg schema for brddist table

--- xtupleserver/trunk/dbscripts/functions/orderedbywo.sql
+++ xtupleserver/trunk/dbscripts/functions/orderedbywo.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION orderedByWo(INTEGER, INTEGER) RETURNS NUMERIC AS '
+CREATE OR REPLACE FUNCTION orderedByWo(INTEGER, INTEGER) RETURNS NUMERIC AS $$
 DECLARE
   pItemsiteid ALIAS FOR $1;
   pLookAheadDays ALIAS FOR $2;
@@ -8,10 +8,10 @@ BEGIN
   RETURN orderedByWo(pItemsiteid, startOfTime(), (CURRENT_DATE + pLookAheadDays));
 
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
 
 
-CREATE OR REPLACE FUNCTION orderedByWo(INTEGER, DATE, DATE) RETURNS NUMERIC AS '
+CREATE OR REPLACE FUNCTION orderedByWo(INTEGER, DATE, DATE) RETURNS NUMERIC AS $$
 DECLARE
   pItemsiteid ALIAS FOR $1;
   pStartDate ALIAS FOR $2;
@@ -26,17 +26,17 @@ BEGIN
   WHERE ( (itemsite_item_id=item_id)
    AND (itemsite_id=pItemsiteid) );
   
-  IF (_itemType = ''C'') THEN
+  IF (_itemType = 'C') THEN
     SELECT COALESCE(SUM((noNeg(wo_qtyord - wo_qtyrcv) * brddist_stdqtyper)), 0.0) INTO _qty
-    FROM wo, brddist
-    WHERE ( (wo_status <> ''C'')
+    FROM wo, xtmfg.brddist
+    WHERE ( (wo_status <> 'C')
      AND (brddist_wo_id=wo_id)
      AND (brddist_itemsite_id=pItemsiteid)
      AND (wo_duedate BETWEEN pStartDate AND pEndDate) );
   ELSE
     SELECT COALESCE(SUM(noNeg(wo_qtyord - wo_qtyrcv)), 0.0) INTO _qty
     FROM wo
-    WHERE ( (wo_status <> ''C'')
+    WHERE ( (wo_status <> 'C')
      AND (wo_itemsite_id=pItemsiteid)
      AND (wo_duedate BETWEEN pStartDate AND pEndDate) );
   END IF;
@@ -44,4 +44,4 @@ BEGIN
   RETURN _qty;
 
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
