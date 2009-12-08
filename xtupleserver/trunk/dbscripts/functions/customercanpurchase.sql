c54946e596b5a2480834972cbf0016f654fb5cec
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2009-12-08 20:55:39 +0000

    Changed the way the customercanpurchase function checked the tables to be more efficient. There are more queries now but each one runs significantly faster. Issue #10210.

--- xtupleserver/trunk/dbscripts/functions/customercanpurchase.sql
+++ xtupleserver/trunk/dbscripts/functions/customercanpurchase.sql
@@ -35,12 +35,25 @@ BEGIN
 
   IF(pShiptoid != -1) THEN
 --  Check for a shipto Assigned Price
-    SELECT ipsprice_id INTO _id
-      FROM ipsprice, ipshead, ipsass
-     WHERE((ipsprice_ipshead_id=ipshead_id)
+    SELECT ipsitem_id INTO _id
+      FROM ipsitem, ipshead, ipsass
+     WHERE((ipsitem_ipshead_id=ipshead_id)
        AND (ipsass_ipshead_id=ipshead_id)
        AND (CURRENT_DATE BETWEEN ipshead_effective AND (ipshead_expires - 1))
-       AND (ipsprice_item_id=pItemid)
+       AND (ipsitem_item_id=pItemid)
+       AND (ipsass_shipto_id != -1)
+       AND (ipsass_shipto_id=pShiptoid))
+     LIMIT 1;
+    IF (FOUND) THEN
+      RETURN TRUE;
+    END IF;
+    SELECT ipsprodcat_id INTO _id
+      FROM ipsprodcat, item, ipshead, ipsass
+     WHERE((ipsprodcat_ipshead_id=ipshead_id)
+       AND (ipsprodcat_prodcat_id = item_prodcat_id)
+       AND (ipsass_ipshead_id=ipshead_id)
+       AND (CURRENT_DATE BETWEEN ipshead_effective AND (ipshead_expires - 1))
+       AND (item_id=pItemid)
        AND (ipsass_shipto_id != -1)
        AND (ipsass_shipto_id=pShiptoid))
      LIMIT 1;
@@ -49,15 +62,30 @@ BEGIN
     END IF;
 
 --  Check for a Shipto Pattern Assigned Price
-    SELECT ipsprice_id INTO _id
-      FROM ipsprice, ipshead, ipsass, shipto
-     WHERE((ipsprice_ipshead_id=ipshead_id)
+    SELECT ipsitem_id INTO _id
+      FROM ipsitem, ipshead, ipsass, shipto
+     WHERE((ipsitem_ipshead_id=ipshead_id)
        AND (ipsass_ipshead_id=ipshead_id)
        AND (CURRENT_DATE BETWEEN ipshead_effective AND (ipshead_expires - 1))
        AND (COALESCE(length(ipsass_shipto_pattern), 0) > 0)
        AND (shipto_num ~ ipsass_shipto_pattern)
        AND (ipsass_cust_id=pCustid)
-       AND (ipsprice_item_id=pItemid)
+       AND (ipsitem_item_id=pItemid)
+       AND (shipto_id=pShiptoid))
+     LIMIT 1;
+    IF (FOUND) THEN
+      RETURN TRUE;
+    END IF;
+    SELECT ipsprodcat_id INTO _id
+      FROM ipsprodcat, item, ipshead, ipsass, shipto
+     WHERE((ipsprodcat_ipshead_id=ipshead_id)
+       AND (ipsprodcat_prodcat_id = item_prodcat_id)
+       AND (ipsass_ipshead_id=ipshead_id)
+       AND (CURRENT_DATE BETWEEN ipshead_effective AND (ipshead_expires - 1))
+       AND (COALESCE(length(ipsass_shipto_pattern), 0) > 0)
+       AND (shipto_num ~ ipsass_shipto_pattern)
+       AND (ipsass_cust_id=pCustid)
+       AND (item_id=pItemid)
        AND (shipto_id=pShiptoid))
      LIMIT 1;
     IF (FOUND) THEN
@@ -66,12 +94,25 @@ BEGIN
   END IF;
 
 --  Check for a Customer Assigned Price
-  SELECT ipsprice_id INTO _id
-    FROM ipsprice, ipshead, ipsass
-   WHERE((ipsprice_ipshead_id=ipshead_id)
+  SELECT ipsitem_id INTO _id
+    FROM ipsitem, ipshead, ipsass
+   WHERE((ipsitem_ipshead_id=ipshead_id)
      AND (ipsass_ipshead_id=ipshead_id)
      AND (CURRENT_DATE BETWEEN ipshead_effective AND (ipshead_expires - 1))
-     AND (ipsprice_item_id=pItemid)
+     AND (ipsitem_item_id=pItemid)
+     AND (COALESCE(length(ipsass_shipto_pattern), 0) = 0)
+     AND (ipsass_cust_id=pCustid))
+   LIMIT 1;
+  IF (FOUND) THEN
+    RETURN TRUE;
+  END IF;
+  SELECT ipsprodcat_id INTO _id
+    FROM ipsprodcat, item, ipshead, ipsass
+   WHERE((ipsprodcat_ipshead_id=ipshead_id)
+     AND (ipsprodcat_prodcat_id = item_prodcat_id)
+     AND (ipsass_ipshead_id=ipshead_id)
+     AND (CURRENT_DATE BETWEEN ipshead_effective AND (ipshead_expires - 1))
+     AND (item_id=pItemid)
      AND (COALESCE(length(ipsass_shipto_pattern), 0) = 0)
      AND (ipsass_cust_id=pCustid))
    LIMIT 1;
@@ -80,14 +121,28 @@ BEGIN
   END IF;
 
 --  Check for a Customer Type Assigned Price
-  SELECT ipsprice_id INTO _id
-    FROM ipsprice, ipshead, ipsass, cust
-   WHERE( (ipsprice_ipshead_id=ipshead_id)
+  SELECT ipsitem_id INTO _id
+    FROM ipsitem, ipshead, ipsass, cust
+   WHERE( (ipsitem_ipshead_id=ipshead_id)
      AND  (ipsass_ipshead_id=ipshead_id)
      AND  (ipsass_custtype_id != -1)
      AND  (cust_custtype_id = ipsass_custtype_id)
      AND  (CURRENT_DATE BETWEEN ipshead_effective AND (ipshead_expires - 1))
-     AND  (ipsprice_item_id=pItemid)
+     AND  (ipsitem_item_id=pItemid)
+     AND  (cust_id=pCustid))
+    LIMIT 1;
+  IF (FOUND) THEN
+    RETURN TRUE;
+  END IF;
+  SELECT ipsprodcat_id INTO _id
+    FROM ipsprodcat, item, ipshead, ipsass, cust
+   WHERE( (ipsprodcat_ipshead_id=ipshead_id)
+     AND  (ipsprodcat_prodcat_id = item_prodcat_id)
+     AND  (ipsass_ipshead_id=ipshead_id)
+     AND  (ipsass_custtype_id != -1)
+     AND  (cust_custtype_id = ipsass_custtype_id)
+     AND  (CURRENT_DATE BETWEEN ipshead_effective AND (ipshead_expires - 1))
+     AND  (item_id=pItemid)
      AND  (cust_id=pCustid))
     LIMIT 1;
   IF (FOUND) THEN
@@ -95,15 +150,30 @@ BEGIN
   END IF;
 
 --  Check for a Customer Type Pattern Assigned Price
-  SELECT ipsprice_id INTO _id
-    FROM ipsprice, ipshead, ipsass, custtype, cust
-   WHERE((ipsprice_ipshead_id=ipshead_id)
+  SELECT ipsitem_id INTO _id
+    FROM ipsitem, ipshead, ipsass, custtype, cust
+   WHERE((ipsitem_ipshead_id=ipshead_id)
      AND (ipsass_ipshead_id=ipshead_id)
      AND (coalesce(length(ipsass_custtype_pattern), 0) > 0)
      AND (custtype_code ~ ipsass_custtype_pattern)
      AND (cust_custtype_id=custtype_id)
      AND (CURRENT_DATE BETWEEN ipshead_effective AND (ipshead_expires - 1))
-     AND (ipsprice_item_id=pItemid)
+     AND (ipsitem_item_id=pItemid)
+     AND (cust_id=pCustid))
+   LIMIT 1;
+  IF (FOUND) THEN
+    RETURN TRUE;
+  END IF;
+  SELECT ipsprodcat_id INTO _id
+    FROM ipsprodcat, item, ipshead, ipsass, custtype, cust
+   WHERE((ipsprodcat_ipshead_id=ipshead_id)
+     AND (ipsprodcat_prodcat_id = item_prodcat_id)
+     AND (ipsass_ipshead_id=ipshead_id)
+     AND (coalesce(length(ipsass_custtype_pattern), 0) > 0)
+     AND (custtype_code ~ ipsass_custtype_pattern)
+     AND (cust_custtype_id=custtype_id)
+     AND (CURRENT_DATE BETWEEN ipshead_effective AND (ipshead_expires - 1))
+     AND (item_id=pItemid)
      AND (cust_id=pCustid))
    LIMIT 1;
   IF (FOUND) THEN
