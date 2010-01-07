12c4dd250ea278406bc9dc29b4f20a8a7530ccb4
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-01-07 21:45:51 +0000

    issue #7732 - database side of embedding metasql editor in xtuple erp app - add privs

---
+++ xtupleserver/trunk/updatescripts/from_340/insertPriv.sql
@@ -0,0 +1,3 @@
+INSERT INTO priv ( priv_module , priv_name, priv_descrip ) VALUES ('System' , 'MaintainMetaSQL' , 'User is allowed to edit MetaSQL statements' );
+INSERT INTO priv ( priv_module , priv_name, priv_descrip ) VALUES ('System' , 'ViewMetaSQL' , 'User is allowed to view MetaSQL statements' );
+
