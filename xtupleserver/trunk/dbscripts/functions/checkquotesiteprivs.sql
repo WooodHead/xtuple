f9e69651b20b61f620e51b75889871fdcb43f5a9
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-01-08 16:52:05 +0000

    Issue 10061:optimize checking quote site privs

--- xtupleserver/trunk/dbscripts/functions/checkquotesiteprivs.sql
+++ xtupleserver/trunk/dbscripts/functions/checkquotesiteprivs.sql
@@ -1,39 +1,51 @@
-CREATE OR REPLACE FUNCTION checkQuoteSitePrivs(INTEGER) RETURNS BOOLEAN AS '
+CREATE OR REPLACE FUNCTION checkQuoteSitePrivs(INTEGER) RETURNS BOOLEAN AS $$
 DECLARE
   pQuheadid ALIAS FOR $1;
-  _check    BOOLEAN;
-  _result   INTEGER;
 
 BEGIN
 
-  IF (NOT fetchMetricBool(''MultiWhs'')) THEN
-    RETURN true;
-  END IF;
+  RETURN checkQuoteSitePrivs(pQuheadid, NULL);
 
-  IF (NOT fetchUsrPrefBool(''selectedSites'')) THEN
-    RETURN true;
-  END IF;
-
-  SELECT COALESCE(COUNT(*), 0) INTO _result
-    FROM ( SELECT quitem_id
-             FROM quitem, itemsite
-            WHERE ( (quitem_quhead_id=pQuheadid)
-              AND   (quitem_itemsite_id=itemsite_id)
-              AND   (itemsite_warehous_id NOT IN (SELECT usrsite_warehous_id
-                                                    FROM usrsite
-                                                   WHERE (usrsite_username=current_user))) )
-           UNION
-           SELECT quhead_warehous_id
-             FROM quhead
-            WHERE ( (quhead_id=pQuheadid)
-              AND   (quhead_warehous_id NOT IN (SELECT usrsite_warehous_id
-                                                  FROM usrsite
-                                                 WHERE (usrsite_username=current_user))) )
-         ) AS data;
-  IF (_result > 0) THEN
-    RETURN false;
-  END IF;
-
-  RETURN true;
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION checkQuoteSitePrivs(INTEGER, INTEGER) RETURNS BOOLEAN AS $$
+DECLARE
+  pQuheadid ALIAS FOR $1;
+  pWarehousid ALIAS FOR $2;
+  _result   INTEGER := 0;
+
+BEGIN
+
+--  RAISE NOTICE 'checkQuoteSitePrivs, pQuheadid = %', pQuheadid;
+--  RAISE NOTICE 'checkQuoteSitePrivs, pWarehousid = %', pWarehousid;
+
+  IF (NOT fetchMetricBool('MultiWhs')) THEN
+    RETURN true;
+  END IF;
+
+  IF ( (NOT fetchUsrPrefBool('selectedSites')) AND (pWarehousid IS NULL) ) THEN
+    RETURN true;
+  END IF;
+
+  IF (pWarehousid IS NULL) THEN
+    SELECT COALESCE(COUNT(*), 0) INTO _result
+    FROM quitem JOIN itemsite ON (itemsite_id=quitem_itemsite_id)
+                JOIN site() ON (warehous_id=itemsite_warehous_id)
+    WHERE (quitem_quhead_id=pQuheadid);
+  ELSE
+    SELECT COALESCE(COUNT(*), 0) INTO _result
+    FROM quitem JOIN itemsite ON (itemsite_id=quitem_itemsite_id)
+                JOIN site() ON (warehous_id=itemsite_warehous_id)
+    WHERE ( (quitem_quhead_id=pQuheadid)
+      AND   (itemsite_warehous_id=pWarehousid) );
+  END IF;
+
+  IF (_result > 0) THEN
+    RETURN true;
+  END IF;
+
+  RETURN false;
+
+END;
+$$ LANGUAGE 'plpgsql';
