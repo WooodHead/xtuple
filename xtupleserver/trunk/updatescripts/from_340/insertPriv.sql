889710bc016f212056289f38d42d4e9dc99a5d79
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-01-13 19:19:12 +0000

    issue #7732 - add test mode to metasql execution to roll back changes and a priv that controls whether the user can turn it off when embedded in xtuple guiclient

--- xtupleserver/trunk/updatescripts/from_340/insertPriv.sql
+++ xtupleserver/trunk/updatescripts/from_340/insertPriv.sql
@@ -1,3 +1,3 @@
-INSERT INTO priv ( priv_module , priv_name, priv_descrip ) VALUES ('System' , 'MaintainMetaSQL' , 'User is allowed to edit MetaSQL statements' );
-INSERT INTO priv ( priv_module , priv_name, priv_descrip ) VALUES ('System' , 'ViewMetaSQL' , 'User is allowed to view MetaSQL statements' );
-
+INSERT INTO priv ( priv_module , priv_name, priv_descrip ) VALUES ('System' , 'MaintainMetaSQL' , 'User is allowed to edit MetaSQL statements with the MetaSQL editor.' );
+INSERT INTO priv ( priv_module , priv_name, priv_descrip ) VALUES ('System' , 'ViewMetaSQL' , 'User is allowed to view MetaSQL statements with the MetaSQL editor.' );
+INSERT INTO priv ( priv_module , priv_name, priv_descrip ) VALUES ('System' , 'ExecuteMetaSQL' , 'User is allowed to execute MetaSQL statements with the MetaSQL editor that change the database.' );
