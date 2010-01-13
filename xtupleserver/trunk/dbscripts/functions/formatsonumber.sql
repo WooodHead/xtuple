0eeb39e7add7b5065274d32e75f29767c4fe06c1
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-01-13 13:38:49 +0000

    bug #10413, bug #10395 - marking formatsonumber as stable changes the query plan of the invhist query in deletesoitem from seq scan to index scan if there is an index on invhist_docnumber

--- xtupleserver/trunk/dbscripts/functions/formatsonumber.sql
+++ xtupleserver/trunk/dbscripts/functions/formatsonumber.sql
@@ -2,4 +2,4 @@ CREATE OR REPLACE FUNCTION formatSoNumber(INTEGER) RETURNS TEXT AS '
 SELECT COALESCE((SELECT (text(cohead_number) || ''-'' || formatSoLineNumber(coitem_id))
                    FROM coitem JOIN cohead ON (coitem_cohead_id=cohead_id)
                   WHERE (coitem_id=($1))),''DELETED'');
-' LANGUAGE 'SQL';
+' LANGUAGE 'SQL' STABLE;
