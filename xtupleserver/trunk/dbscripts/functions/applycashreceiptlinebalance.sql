0c633129f7154a1eebd85994f69d76a3f1530cc0
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-01-19 22:08:30 +0000

    issue #9675:  More changes to get discount working on Cash Receipt.  Almost there.

--- xtupleserver/trunk/dbscripts/functions/applycashreceiptlinebalance.sql
+++ xtupleserver/trunk/dbscripts/functions/applycashreceiptlinebalance.sql
@@ -13,7 +13,7 @@ DECLARE
 
 BEGIN
 
---  All calculation performed in currency of Cash Receipt
+--  All calculations performed in currency of Cash Receipt
 
 --  Clear previously applied
   DELETE FROM cashrcptitem WHERE ((cashrcptitem_cashrcpt_id=pCashrcptId) AND (cashrcptitem_aropen_id=pAropenId));
@@ -35,43 +35,50 @@ BEGIN
   END IF;
 
 --  Determine Line balance
-        SELECT currToCurr(aropen_curr_id, cashrcpt_curr_id,
-               aropen_amount - aropen_paid, aropen_docdate) -
-               COALESCE((SELECT (SUM(cashrcptitem_amount) + SUM(cashrcptitem_discount))
-                           FROM cashrcptitem, cashrcpt
-                           WHERE ((cashrcpt_id=cashrcptitem_cashrcpt_id)
-                             AND  (NOT cashrcpt_void)
-                             AND  (NOT cashrcpt_posted)
-                             AND  (cashrcpt_id != pCashrcptId)
-                             AND  (cashrcptitem_aropen_id=pAropenId))), 0) INTO _balance
-            FROM aropen, cashrcpt
-            WHERE ((aropen_id=pAropenId)
-            AND (cashrcpt_id=pCashrcptId));
-
---  Determine Discount as per Terms
-        SELECT  noNeg(least(_amount,_balance) * 
-              CASE WHEN (CURRENT_DATE <= (aropen_docdate + terms_discdays)) THEN terms_discprcnt 
-              ELSE 0.00 END - applied) INTO _discount
-              FROM aropen LEFT OUTER JOIN terms ON (aropen_terms_id=terms_id), 
-                   (SELECT COALESCE(SUM(arapply_applied), 0.00) AS applied  
-		            FROM arapply, aropen 
-                    WHERE ((arapply_target_aropen_id=pAropenId) 
-                    AND (arapply_source_aropen_id=pAropenId) 
-                    AND  (aropen_discount) )
-                   ) AS data 
-              WHERE (aropen_id=pAropenId) ;
+  SELECT currToCurr(aropen_curr_id, cashrcpt_curr_id,
+         aropen_amount - aropen_paid, aropen_docdate) -
+         COALESCE((SELECT (SUM(cashrcptitem_amount) + SUM(cashrcptitem_discount))
+                   FROM cashrcptitem, cashrcpt
+                   WHERE ((cashrcpt_id=cashrcptitem_cashrcpt_id)
+                     AND  (NOT cashrcpt_void)
+                     AND  (NOT cashrcpt_posted)
+                     AND  (cashrcpt_id != pCashrcptId)
+                     AND  (cashrcptitem_aropen_id=pAropenId))), 0), aropen_amount INTO _balance
+         FROM aropen, cashrcpt
+           WHERE ((aropen_id=pAropenId)
+           AND (cashrcpt_id=pCashrcptId));
+            
+--  Determine Max Discount as per Terms
+  SELECT  noNeg(_balance * 
+          CASE WHEN (CURRENT_DATE <= (aropen_docdate + terms_discdays)) THEN terms_discprcnt 
+          ELSE 0.00 END - applied),  terms_discprcnt INTO _discount
+  FROM aropen LEFT OUTER JOIN terms ON (aropen_terms_id=terms_id), 
+       (SELECT COALESCE(SUM(arapply_applied), 0.00) AS applied  
+	FROM arapply, aropen 
+        WHERE ((arapply_target_aropen_id=pAropenId) 
+         AND (arapply_source_aropen_id=pAropenId) 
+         AND  (aropen_discount) )
+           ) AS data 
+  WHERE (aropen_id=pAropenId);
 
 --  Determine the amount to apply
-    _applyAmount := least(_amount, _balance - _discount);
+  IF (_amount + _discount = _balance) THEN
+    _applyAmount := _amount;
+  ELSIF (_amount + _discount > _balance) THEN
+    _applyAmount := _amount - _discount;
+  ELSE
+    _discount := _discount * (_amount / _balance);
+    _applyAmount := _amount;
+  END IF;
 
-    IF (_applyAmount > 0) THEN
+  IF (_applyAmount > 0) THEN
 --  Create a new cashrcptitem
-        INSERT INTO cashrcptitem
-        ( cashrcptitem_aropen_id, cashrcptitem_cashrcpt_id,
-          cashrcptitem_amount,cashrcptitem_discount )
-        VALUES
-        ( pAropenid, pCashrcptid, round(_applyAmount, 2), round(_discount, 2) );
-    END IF;
+      INSERT INTO cashrcptitem
+      ( cashrcptitem_aropen_id, cashrcptitem_cashrcpt_id,
+        cashrcptitem_amount,cashrcptitem_discount )
+      VALUES
+      ( pAropenid, pCashrcptid, round(_applyAmount, 2), round(_discount, 2) );
+  END IF;
 
   RETURN 1;
 
