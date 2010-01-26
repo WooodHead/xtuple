19ff22f18bcd3736ac113ed0fe666aec6664bc15
bixwilson <bixwilson@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-01-26 15:53:52 +0000

    Issue #10406 - Changed documents widget to allow relationships (parent, child, sibling) to internal documents, such as incidents, projects, CRM Accounts, Contacts, etc, to better mimic Mantis functionality. Currently, only Attach and Detach functions work, not Open, View, Edit or Print.

---
+++ xtupleserver/trunk/updatescripts/from_340/createSequenceDocassId.sql
@@ -0,0 +1,13 @@
+-- Sequence: docass_docass_id_seq
+
+-- DROP SEQUENCE docass_docass_id_seq;
+
+CREATE SEQUENCE docass_docass_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 20
+  CACHE 1;
+ALTER TABLE docass_docass_id_seq OWNER TO "admin";
+GRANT ALL ON TABLE docass_docass_id_seq TO "admin";
+GRANT ALL ON TABLE docass_docass_id_seq TO xtrole;
