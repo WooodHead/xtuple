e63c6b4b824a68f2b8a7ca10a2c4b7fc608bc2c0
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-01-26 15:32:55 +0000

    issue #9782 - database side - replace ccdefaultbank metric with table to associate bank accounts with diff credit card types

--- xtupleserver/trunk/dbscripts/functions/postcccredit.sql
+++ xtupleserver/trunk/dbscripts/functions/postcccredit.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION postCCcredit(INTEGER, TEXT, INTEGER) RETURNS INTEGER AS '
+CREATE OR REPLACE FUNCTION postCCcredit(INTEGER, TEXT, INTEGER) RETURNS INTEGER AS $$
 DECLARE
   pCCpay	ALIAS FOR $1;
   preftype      ALIAS FOR $2;
@@ -9,68 +9,69 @@ DECLARE
   _closed       BOOLEAN;
   _glaccnt	INTEGER;
   _journalNum	INTEGER;
-  _notes	TEXT := ''Credit Customer via Credit Card'';
+  _notes	TEXT := 'Credit Customer via Credit Card';
   _r		aropen%ROWTYPE;
   _sequence	INTEGER;
 
 BEGIN
-  SELECT bankaccnt_accnt_id INTO _glaccnt
-  FROM bankaccnt
-  WHERE bankaccnt_id = fetchMetricValue(''CCDefaultBank'');
-
-  IF (NOT FOUND) THEN
-    RETURN -1;
-  END IF;
-
-  IF ((preftype = ''cohead'') AND NOT EXISTS(SELECT cohead_id
+  IF ((preftype = 'cohead') AND NOT EXISTS(SELECT cohead_id
 					     FROM cohead
 					     WHERE (cohead_id=prefid))) THEN
     RETURN -2;
-  ELSIF ((preftype = ''aropen'') AND NOT EXISTS(SELECT aropen_id
+  ELSIF ((preftype = 'aropen') AND NOT EXISTS(SELECT aropen_id
                                                 FROM aropen
                                                 WHERE (aropen_id=prefid))) THEN
     RETURN -2;
   END IF;
 
   SELECT * INTO _c
-     FROM ccpay, ccard
-     WHERE ( (ccpay_id = pCCpay)
-       AND   (ccpay_ccard_id = ccard_id) );
+     FROM ccpay
+     JOIN ccard  ON (ccpay_ccard_id = ccard_id)
+     JOIN ccbank ON (ccard_type=ccbank_ccard_type)
+    WHERE (ccpay_id = pCCpay);
 
   IF (NOT FOUND) THEN
     RETURN -3;
   END IF;
 
-  IF (_c.ccpay_type != ''R'') THEN
+  SELECT bankaccnt_accnt_id INTO _glaccnt
+  FROM bankaccnt
+  WHERE (bankaccnt_id=_c.ccbank_bankaccnt_id);
+
+  IF (NOT FOUND) THEN
+    RETURN -1;
+  END IF;
+
+  IF (_c.ccpay_type != 'R') THEN
     RETURN -4;
   END IF;
 
   _sequence := fetchGLSequence();
 
   IF (_c.ccpay_r_ref IS NOT NULL) THEN
-    _ccOrderDesc := (_c.ccard_type || ''-'' || _c.ccpay_r_ref);
+    _ccOrderDesc := (_c.ccard_type || '-' || _c.ccpay_r_ref);
   ELSE
-    _ccOrderDesc := (_c.ccard_type || ''-'' || _c.ccpay_order_number::TEXT ||
-		     ''-'' || COALESCE(_c.ccpay_order_number_seq::TEXT, ''''));
+    _ccOrderDesc := (_c.ccard_type || '-' || _c.ccpay_order_number::TEXT ||
+		     '-' || COALESCE(_c.ccpay_order_number_seq::TEXT, ''));
   END IF;
 
-  PERFORM insertIntoGLSeries(_sequence, ''A/R'', ''CC'', _ccOrderDesc,
+  PERFORM insertIntoGLSeries(_sequence, 'A/R', 'CC', _ccOrderDesc,
 			     findARAccount(_c.ccpay_cust_id),
 			     ROUND(currToBase(_c.ccpay_curr_id,
 					      _c.ccpay_amount,
 					      _c.ccpay_transaction_datetime::DATE), 2) * -1,
 			     CURRENT_DATE, _notes );
 
-  PERFORM insertIntoGLSeries( _sequence, ''A/R'', ''CC'', _ccOrderDesc,
+  PERFORM insertIntoGLSeries( _sequence, 'A/R', 'CC', _ccOrderDesc,
 			      _glaccnt,
 			      ROUND(currToBase(_c.ccpay_curr_id,
 					       _c.ccpay_amount,
 					       _c.ccpay_transaction_datetime::DATE),2),
 			      CURRENT_DATE, _notes );
 
-  PERFORM postGLSeries(_sequence, fetchJournalNumber(''C/R'') );
+  PERFORM postGLSeries(_sequence, fetchJournalNumber('C/R') );
 
-  IF (preftype = ''aropen'') THEN
+  IF (preftype = 'aropen') THEN
     SELECT * INTO _r
     FROM aropen
     WHERE (aropen_id=prefid);
@@ -78,7 +79,7 @@ BEGIN
   ELSE
     SELECT aropen.* INTO _r
     FROM aropen
-    WHERE ((aropen_doctype IN (''C'', ''R''))
+    WHERE ((aropen_doctype IN ('C', 'R'))
       AND  (aropen_docnumber=_c.ccpay_r_ref)
       AND  (ROUND(aropen_amount - aropen_paid, 2) <=
                 ROUND(currToCurr(_c.ccpay_curr_id, aropen_curr_id,_c.ccpay_amount,
@@ -108,14 +109,14 @@ BEGIN
     VALUES
     ( _c.ccpay_cust_id,
       _r.aropen_id, _r.aropen_doctype, _r.aropen_docnumber,
-      -1, ''R'', ''Credit Card Credit'',
+      -1, 'R', 'Credit Card Credit',
       _c.ccard_type, _c.ccpay_order_number,
       ROUND(_c.ccpay_amount, 2), _closed,
-      CURRENT_DATE, _c.ccpay_transaction_datetime::DATE, fetchJournalNumber(''AR-CM''), CURRENT_USER,
+      CURRENT_DATE, _c.ccpay_transaction_datetime::DATE, fetchJournalNumber('AR-CM'), CURRENT_USER,
       _c.ccpay_curr_id );
   END IF;
 
-  IF (preftype = ''cohead'') THEN
+  IF (preftype = 'cohead') THEN
     INSERT INTO payco (
       payco_ccpay_id, payco_cohead_id, payco_amount, payco_curr_id 
     ) VALUES (
@@ -126,5 +127,5 @@ BEGIN
   RETURN 0;
 
 END;
-'
+$$
 LANGUAGE 'plpgsql';
