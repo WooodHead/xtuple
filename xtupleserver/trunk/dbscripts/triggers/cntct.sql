67889b10b9e994f844f7a9441961039b44b83d51
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-02-08 21:50:02 +0000

    issue #9920:  Remove honorifc from stored name format as this reduces the effectiveness of the list search.

--- xtupleserver/trunk/dbscripts/triggers/cntct.sql
+++ xtupleserver/trunk/dbscripts/triggers/cntct.sql
@@ -1,13 +1,13 @@
 CREATE OR REPLACE FUNCTION _cntctTrigger() RETURNS "trigger" AS $$
 BEGIN
    
-  NEW.cntct_name := formatCntctName(NEW.cntct_honorific, NEW.cntct_first_name, NEW.cntct_middle, NEW.cntct_last_name, NEW.cntct_suffix);
+  NEW.cntct_name := formatCntctName(NULL, NEW.cntct_first_name, NEW.cntct_middle, NEW.cntct_last_name, NEW.cntct_suffix);
   
   RETURN NEW;
 END;
 $$ LANGUAGE 'plpgsql';
 
-SELECT dropIfExists('TRIGGER', 'cntct');
+SELECT dropIfExists('TRIGGER', 'cntctTrigger');
 CREATE TRIGGER cntcttrigger
   BEFORE INSERT OR UPDATE
   ON cntct
