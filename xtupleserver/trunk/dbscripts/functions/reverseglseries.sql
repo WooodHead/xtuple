bfc15ecce3899791468f3ff4e16e1629e637280c
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-02-18 21:50:57 +0000

    issue #10512: Make reversing journals have their own sequence and journal number.

--- xtupleserver/trunk/dbscripts/functions/reverseglseries.sql
+++ xtupleserver/trunk/dbscripts/functions/reverseglseries.sql
@@ -1,27 +1,36 @@
 
-CREATE OR REPLACE FUNCTION reverseGLSeries(INTEGER, DATE, TEXT) RETURNS INTEGER AS '
+CREATE OR REPLACE FUNCTION reverseGLSeries(INTEGER, DATE, TEXT) RETURNS INTEGER AS $$
 DECLARE
   pSequence ALIAS FOR $1;
   pDistDate ALIAS FOR $2;
   pNotes ALIAS FOR $3;
+  _sequence INTEGER := fetchGLSequence();	
+  _journal INTEGER;
 
 BEGIN
+
+  SELECT fetchJournalNumber(jrnluse_use) INTO _journal
+  FROM gltrans
+    JOIN jrnluse ON (gltrans_journalnumber=jrnluse_number)
+  WHERE (gltrans_sequence=pSequence)
+  LIMIT 1;
+  
   INSERT INTO gltrans (gltrans_created, gltrans_posted, gltrans_exported,
                        gltrans_date, gltrans_sequence, gltrans_accnt_id,
                        gltrans_source, gltrans_docnumber, gltrans_misc_id,
                        gltrans_amount, gltrans_notes, gltrans_journalnumber,
                        gltrans_doctype)
                SELECT  CURRENT_TIMESTAMP, FALSE, FALSE,
-                       pDistDate, gltrans_sequence, gltrans_accnt_id,
+                       pDistDate, _sequence, gltrans_accnt_id,
                        gltrans_source, gltrans_docnumber, gltrans_misc_id,
-                       (gltrans_amount * -1), pNotes, gltrans_journalnumber,
+                       (gltrans_amount * -1), pNotes, _journal,
                        gltrans_doctype
                   FROM gltrans
                  WHERE (gltrans_sequence=pSequence);
 
-  PERFORM postIntoTrialBalance(pSequence);
+  PERFORM postIntoTrialBalance(_sequence);
 
-  RETURN pSequence;
+  RETURN _journal;
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
 
