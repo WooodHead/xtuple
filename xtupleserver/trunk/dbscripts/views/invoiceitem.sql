24d957bd58be75a7ced3aa32502f33a1c784ee81
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-02-18 18:55:31 +0000

    Issue #10215:populate cohist_ordernumber correctly for consolidated invoices

--- xtupleserver/trunk/dbscripts/views/invoiceitem.sql
+++ xtupleserver/trunk/dbscripts/views/invoiceitem.sql
@@ -1,6 +1,6 @@
 SELECT dropIfExists('view', 'invoiceitem'); 
 CREATE VIEW invoiceitem AS
-SELECT invcitem.*, itemsite_id,
+SELECT invcitem.*, itemsite_id, cohead_number,
        COALESCE((invcitem_billed * invcitem_qty_invuomratio), 0) AS qty,
        COALESCE((invcitem_price / invcitem_price_invuomratio), 0) AS unitprice,
        COALESCE(round((invcitem_billed * invcitem_qty_invuomratio) *
@@ -19,6 +19,8 @@ SELECT invcitem.*, itemsite_id,
            (invcitem_billed * invcitem_qty_invuomratio) 
          ELSE 1 END) AS unitcost
 FROM invcitem JOIN invchead ON (invchead_id = invcitem_invchead_id)
+              LEFT OUTER JOIN coitem ON (coitem_id=invcitem_coitem_id)
+              LEFT OUTER JOIN cohead ON (cohead_id=coitem_cohead_id)
               LEFT OUTER JOIN itemsite ON ( (itemsite_item_id = invcitem_item_id)
                                        AND  (itemsite_warehous_id = invcitem_warehous_id) );
 
