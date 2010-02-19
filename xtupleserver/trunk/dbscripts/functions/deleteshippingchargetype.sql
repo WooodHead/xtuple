53ea914bc8fd83c9a7a4086151fba93635849cee
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-02-19 03:00:14 +0000

    Issue #10446:new error conditions

--- xtupleserver/trunk/dbscripts/functions/deleteshippingchargetype.sql
+++ xtupleserver/trunk/dbscripts/functions/deleteshippingchargetype.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION deleteShippingChargeType (integer) RETURNS integer AS '
+CREATE OR REPLACE FUNCTION deleteShippingChargeType (integer) RETURNS integer AS $$
 DECLARE
   pShipchrgid ALIAS FOR $1;
   _check INTEGER;
@@ -7,11 +7,47 @@ BEGIN
 
 --  Check to see if the passed shipchrg is used as a default for any customers
   SELECT cust_id INTO _check
-  FROM cust
+  FROM custinfo
   WHERE (cust_shipchrg_id=pShipchrgid)
   LIMIT 1;
   IF (FOUND) THEN
     RETURN -1;
+  END IF;
+
+--  Check to see if the passed shipchrg is used as a default for any shiptos
+  SELECT shipto_id INTO _check
+  FROM shiptoinfo
+  WHERE (shipto_shipchrg_id=pShipchrgid)
+  LIMIT 1;
+  IF (FOUND) THEN
+    RETURN -2;
+  END IF;
+
+--  Check to see if the passed shipchrg is used on any sales orders
+  SELECT cohead_id INTO _check
+  FROM cohead
+  WHERE (cohead_shipchrg_id=pShipchrgid)
+  LIMIT 1;
+  IF (FOUND) THEN
+    RETURN -3;
+  END IF;
+
+--  Check to see if the passed shipchrg is used on any shippers
+  SELECT shiphead_id INTO _check
+  FROM shiphead
+  WHERE (shiphead_shipchrg_id=pShipchrgid)
+  LIMIT 1;
+  IF (FOUND) THEN
+    RETURN -4;
+  END IF;
+
+--  Check to see if the passed shipchrg is used on any invoices
+  SELECT invchead_id INTO _check
+  FROM invchead
+  WHERE (invchead_shipchrg_id=pShipchrgid)
+  LIMIT 1;
+  IF (FOUND) THEN
+    RETURN -5;
   END IF;
 
 --  Delete the passed shipchrg
@@ -21,4 +57,4 @@ BEGIN
   RETURN pShipchrgid;
 
 END;
-' LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
