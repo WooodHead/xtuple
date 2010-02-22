53a108e0fbb4a1624d95702c8944bf7b80bdb710
clinuz <clinuz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-02-22 20:55:42 +0000

    Issue# 10407: Added subscribers tab to the incident workbench ui allowing contacts and users to subscribe to individual incidents to be updated via the incident's EDI profile.
    Known bugs: 
    No redundancy check for subscribers,
    Error when adding subscribers to a new incident that has not been saved first,
    EDI profile still being tested with variable success
    
    To-do:
    Fix redundancy to disallow the addition of the same subscriber multiple times,
    Fix display in new subscribers dialog pop-up,
    Fix column-width issue allowing a horizontal scroll in the email address column,
    Fix EDI profile to include all subscribers

---
+++ xtupleserver/trunk/updatescripts/from_340beta/createIncdt_subs.sql
@@ -0,0 +1,23 @@
+﻿-- Table: incdt_subs
+
+-- DROP TABLE incdt_subs;
+
+CREATE TABLE incdt_subs
+(
+  incdt_subs_idx serial NOT NULL,
+  incdt_assoc_id integer NOT NULL,
+  subscriber_id integer NOT NULL,
+  subscriber_type text NOT NULL,
+  CONSTRAINT incdt_subs_pkey PRIMARY KEY (incdt_subs_idx),
+  CONSTRAINT incdt_subs_fkey_incdt_assoc_id FOREIGN KEY (incdt_assoc_id)
+      REFERENCES incdt (incdt_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE,
+  CONSTRAINT incdt_subs_enum_type CHECK (subscriber_type = ANY (ARRAY['contact'::text, 'user'::text]))
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE incdt_subs OWNER TO "admin";
+GRANT ALL ON TABLE incdt_subs TO "admin";
+GRANT ALL ON TABLE incdt_subs TO xtrole;
+COMMENT ON TABLE incdt_subs IS 'Incident Subscribers Helper Table';
