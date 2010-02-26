8183ce301b48f6a741620abd2c6cdb75baed05da
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-02-26 21:12:24 +0000

    Issue #9382:new edit to prevent releasing when no unreleased poitems

--- xtupleserver/trunk/dbscripts/functions/releasepurchaseorder.sql
+++ xtupleserver/trunk/dbscripts/functions/releasepurchaseorder.sql
@@ -1,24 +1,31 @@
-CREATE OR REPLACE FUNCTION releasePurchaseOrder(INTEGER) RETURNS INTEGER AS '
+CREATE OR REPLACE FUNCTION releasePurchaseOrder(INTEGER) RETURNS INTEGER AS $$
 DECLARE
   pPoheadid ALIAS FOR $1;
 
 BEGIN
 
-  IF ( ( SELECT (pohead_status=''U'')
+  IF ( ( SELECT (COUNT(*) = 0)
+         FROM poitem
+         WHERE ( (poitem_pohead_id=pPoheadid)
+           AND   (poitem_status='U') ) ) ) THEN
+    RETURN -1;
+  END IF;
+
+  IF ( ( SELECT (pohead_status='U')
          FROM pohead
          WHERE (pohead_id=pPoheadid) ) ) THEN
 
     UPDATE pohead
-    SET pohead_status=''O''
+    SET pohead_status='O'
     WHERE (pohead_id=pPoheadid);
 
   END IF;
 
   UPDATE poitem
-  SET poitem_status=''O''
+  SET poitem_status='O'
   WHERE (poitem_pohead_id=pPoheadid);
 
   RETURN 1;
 
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
