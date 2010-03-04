e798b1d5e90b811f4964465547ce3edee23d71f3
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-03-04 00:32:25 +0000

    issue #10800:  Make editing incident comments update the incident.

--- xtupleserver/trunk/dbscripts/triggers/comment.sql
+++ xtupleserver/trunk/dbscripts/triggers/comment.sql
@@ -1,11 +1,14 @@
-CREATE OR REPLACE FUNCTION _commentTrigger () RETURNS TRIGGER AS '
+CREATE OR REPLACE FUNCTION _commentTrigger () RETURNS TRIGGER AS $$
 BEGIN
   IF (NEW.comment_cmnttype_id IS NULL) THEN
-	RAISE EXCEPTION ''You must supply a valid Comment Type ID.'';
+	RAISE EXCEPTION 'You must supply a valid Comment Type ID.';
+  ELSIF (NEW.comment_source = 'INCDT') THEN
+    UPDATE incdt SET incdt_updated = now() WHERE incdt_id = NEW.comment_source_id;
   END IF;
+
   RETURN NEW;
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
 
 DROP TRIGGER commentTrigger ON comment;
-CREATE TRIGGER commentTrigger BEFORE INSERT OR UPDATE ON comment FOR EACH ROW EXECUTE PROCEDURE _commentTrigger();
+CREATE TRIGGER commentTrigger AFTER INSERT OR UPDATE ON comment FOR EACH ROW EXECUTE PROCEDURE _commentTrigger();
