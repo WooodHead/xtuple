5867c994b8242608fd10984f16c1cbca48da0241
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-03-21 20:52:22 +0000

    Backported issue #10943 to tag.

--- xtupleserver/tags/R3_5_0BETA3/updatescripts/from_350beta2/updateRecv.sql
+++ xtupleserver/tags/R3_5_0BETA3/updatescripts/from_350beta2/updateRecv.sql
@@ -1,1 +1,1 @@
-UPDATE recv SET recv_recvcost = recv_value / recv_qty WHERE recv_recvcost IS NULL;
+UPDATE recv SET recv_recvcost = recv_value / recv_qty WHERE recv_recvcost IS NULL AND (COALESCE(recv_qty, 0) <> 0);
