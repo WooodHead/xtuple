56ebd826e9bb21007753b4fd10e3f7d4f471af63
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-04-15 18:33:54 +0000

    Issue #10877:fix rounding of tax

--- xtupleserver/trunk/dbscripts/functions/creditmemototal.sql
+++ xtupleserver/trunk/dbscripts/functions/creditmemototal.sql
@@ -8,19 +8,16 @@ BEGIN
 
   -- TO DO:  Add in line item taxes
   SELECT COALESCE(cmhead_freight,0.0) + COALESCE(cmhead_misc,0.0) +
-         ( SELECT COALESCE(SUM((cmitem_qtycredit * cmitem_qty_invuomratio) * cmitem_unitprice / cmitem_price_invuomratio), 0.0) 
+         ( SELECT COALESCE(ROUND(SUM((cmitem_qtycredit * cmitem_qty_invuomratio) * cmitem_unitprice / cmitem_price_invuomratio), 2), 0.0) 
              FROM cmitem
             WHERE (cmitem_cmhead_id=pCreditmemoId)
            ) +
-         ( SELECT COALESCE(ROUND(SUM(taxhist_tax * -1.0), 2), 0.0) 
-             FROM cmheadtax
-            WHERE (taxhist_parent_id=pCreditmemoId)
-           ) +
-         ( SELECT COALESCE(ROUND(SUM(taxhist_tax * -1.0), 2), 0.0) 
-             FROM cmitemtax
-               JOIN cmitem ON (cmitem_id=taxhist_parent_id)
-            WHERE (cmitem_cmhead_id=pCreditmemoId)
-           ) INTO _result
+         ( SELECT SUM(tax) * -1 AS tax
+           FROM ( SELECT ROUND(SUM(taxdetail_tax),2) AS tax
+                  FROM tax
+                  JOIN calculateTaxDetailSummary('CM', pCreditmemoId, 'T') ON (taxdetail_tax_id=tax_id)
+                  GROUP BY tax_id) AS data )
+           INTO _result
   FROM cmhead
   WHERE (cmhead_id=pCreditmemoId);
 
