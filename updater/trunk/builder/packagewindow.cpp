96571ead341d7a14fa94009fdb8e1076bf1c2bf9
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-04-29 15:44:39 +0000

    Updated version and copyright in prep for release.

--- updater/trunk/builder/packagewindow.cpp
+++ updater/trunk/builder/packagewindow.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the xTuple ERP: PostBooks Edition, a free and
  * open source Enterprise Resource Planning software suite,
- * Copyright (c) 1999-2009 by OpenMFG LLC, d/b/a xTuple.
+ * Copyright (c) 1999-2010 by OpenMFG LLC, d/b/a xTuple.
  * It is licensed to you under the Common Public Attribution License
  * version 1.0, the full text of which (including xTuple-specific Exhibits)
  * is available at www.xtuple.com/CPAL.  By using this software, you agree
@@ -184,7 +184,7 @@ void PackageWindow::helpAbout()
 {
   QMessageBox::about(this, tr("Package Manager: Builder"),
     tr("Create package manager content files."
-       "\n\nCopyright (c) 2004 OpenMFG, LLC., All Rights Reserved"));
+       "\n\nCopyright (c) 1999-2010 by OpenMFG LLC, d/b/a xTuple, All Rights Reserved"));
 }
 
 void PackageWindow::sPrereqSelectionChanged()
