566503b4189135e3aeb06f55a91f786b22392bd0
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-04-29 15:44:39 +0000

    Updated version and copyright in prep for release.

--- updater/trunk/common/data.cpp
+++ updater/trunk/common/data.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the xTuple ERP: PostBooks Edition, a free and
  * open source Enterprise Resource Planning software suite,
- * Copyright (c) 1999-2009 by OpenMFG LLC, d/b/a xTuple.
+ * Copyright (c) 1999-2010 by OpenMFG LLC, d/b/a xTuple.
  * It is licensed to you under the Common Public Attribution License
  * version 1.0, the full text of which (including xTuple-specific Exhibits)
  * is available at www.xtuple.com/CPAL.  By using this software, you agree
@@ -23,8 +23,8 @@ QString _name = QObject::tr("Update Manager");
 #endif
 
 
-QString _copyright = QObject::tr("Copyright (c) 2004-2009 OpenMFG, LLC., d/b/a xTuple.");
-QString _version = QObject::tr("2.2.1wip");
+QString _copyright = QObject::tr("Copyright (c) 2004-2010 OpenMFG, LLC., d/b/a xTuple.");
+QString _version = QObject::tr("2.2.1");
 
 QString _user;
 int     _usrid;
