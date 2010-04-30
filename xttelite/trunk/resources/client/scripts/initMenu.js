2f7114e9fb78749c67bf35795e431d8d29c47246
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-04-30 14:33:45 +0000

    issue #11256:  Move Time Sheets menu item to CRM > Projects menu.

--- xttelite/trunk/resources/client/scripts/initMenu.js
+++ xttelite/trunk/resources/client/scripts/initMenu.js
@@ -1,47 +1,19 @@
+// Define menu and action variables
+var crmMenu = mainwindow.findChild("menu.crm.projects");
+crmMenu.addSeparator();
 
+var tesheetAction = crmMenu.addAction(qsTr("Time Sheets..."), mainwindow);
+tesheetAction.objectName = "pm.timesheets";
+tesheetAction.setData("MaintainTimeExpense");
+tesheetAction.enabled = privileges.value("MaintainTimeExpense");
+
+// Define function(s)
 function sOpenSheets()
 {
   var param = new Object;
-  var wind = toolbox.openWindow("tesheet", 0, Qt.NonModal, Qt.Window);
+  var wind = toolbox.openWindow("tesheet", mainwindow);
   wind.set(param);
 }
 
-// Sale menu references
-var saleMenu 	= mainwindow.findChild("menu.sales");
-var lookupMenu	= mainwindow.findChild("menu.sales.lookup");
-
-// Retail menu
-var teMenu 	= toolbox.menuInsertMenu(saleMenu, lookupMenu, "Time and Expense"); 
-
-// Separator
-toolbox.menuInsertSeparator(saleMenu, lookupMenu);
-
-// Open Sheets action
-var tesheetAction	= toolbox.menuAddAction(teMenu, "Open Sheets...", 
-              		(privileges.value("MaintainTimeExpense")));
-
-tesheetAction.setData("MaintainTimeExpense");
-tesheetAction.objectName = "so.listOpenSheets";
+// Connect Action(s)
 tesheetAction.triggered.connect(sOpenSheets);
-
-
-// Separator
-//toolbox.menuAddSeparator(teMenu);
-
-
-
-// Remove custom command actions
-
-var tesheetAction	= mainwindow.findChild("custom.tesheet");
-
-var customSalesMenu	= mainwindow.findChild("menu.sales.custom");
-
-/*
-toolbox.menuRemove(customSalesMenu,tesheetAction);
-
-
-// Remove custom menu if no custom commands left
-if (!toolbox.menuActionCount(customSalesMenu))
-  toolbox.menuRemove(saleMenu,customSalesMenu);
-
-*/
