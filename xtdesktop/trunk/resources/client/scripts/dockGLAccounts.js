353f85bc3d39933fdae458d81c663e588ca52b8d
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-05-05 02:18:04 +0000

    issue #10974:  Get GL account selection working.

---
+++ xtdesktop/trunk/resources/client/scripts/dockGLAccounts.js
@@ -0,0 +1,164 @@
+/*
+ * This file is part of the xTuple ERP: PostBooks Edition, a free and
+ * open source Enterprise Resource Planning software suite,
+ * Copyright (c) 1999-2010 by OpenMFG LLC, d/b/a xTuple.
+ * It is licensed to you under the Common Public Attribution License
+ * version 1.0, the full text of which (including xTuple-specific Exhibits)
+ * is available at www.xtuple.com/CPAL.  By using this software, you agree
+ * to be bound by its terms.
+ */
+
+var _b1GLAccounts;
+var _b2GLAccounts;
+var _dockGLAccounts;
+var _glAccounts;
+
+/*!
+  Initializes Monitored Accounts dock widget and places it in the main window.
+*/
+function initDockGLAccounts()
+{
+  if (!privileges.check("ViewGLAccountsDock"))
+    return;
+
+  // Set up objects
+  _dockGLAccounts = mainwindow.findChild("_dockGLAccounts");
+  _glAccounts = mainwindow.findChild("_glAccounts");
+
+  _b1GLAccounts = _dockGLAccounts.findChild("_button1");
+  _b2GLAccounts = _dockGLAccounts.findChild("_button2");
+  _labelGLAccounts = _dockGLAccounts.findChild("_label");
+
+  // Set icons
+  var iReload = new QIcon;
+  iReload.addDbImage("reload_16");
+  _b1GLAccounts.icon = iReload;
+  _b1GLAccounts.text = "";
+  _b1GLAccounts.toolTip = qsTr("Reload");
+
+  var iGear = new QIcon();
+  iGear.addDbImage("gear_16");
+  _b2GLAccounts.icon = iGear;
+  _b2GLAccounts.text = "";
+  _b2GLAccounts.toolTip = qsTr("Preferences...");
+
+  // Set columns on list
+  _glAccounts.addColumn(qsTr("Number"), -1, Qt.AlignLeft, true, "accnt_id");
+  _glAccounts.addColumn(qsTr("Description"), -1, Qt.AlignLeft, true, "accnt_descrip");
+  _glAccounts.addColumn(qsTr("Type"), -1, Qt.AlignLeft, true, "accnt_type");
+  _glAccounts.addColumn(qsTr("Balance"), -1, Qt.AlignRight, true, "balance");
+
+  // Connect Signals and Slots
+  _b1GLAccounts.clicked.connect(fillListGLAccounts);
+  _b2GLAccounts.clicked.connect(preferencesGLAccounts);
+
+  _dtTimer.timeout.connect(fillListGLAccounts);
+  mainwindow.bankAdjustmentsUpdated.connect(fillListGLAccounts);
+  mainwindow.checksUpdated.connect(fillListGLAccounts);
+  mainwindow.creditMemosUpdated.connect(fillListGLAccounts);
+  mainwindow.cashReceiptsUpdated.connect(fillListGLAccounts);
+  mainwindow.glSeriesUpdated.connect(fillListGLAccounts);
+  mainwindow.invoicesUpdated.connect(fillListGLAccounts);
+  mainwindow.paymentsUpdated.connect(fillListGLAccounts);
+  mainwindow.standardPeriodsUpdated.connect(fillListGLAccounts);
+  mainwindow.vouchersUpdated.connect(fillListGLAccounts);
+  mainwindow.workOrderMaterialsUpdated.connect(fillListGLAccounts);
+  mainwindow.workOrderOperationsUpdated.connect(fillListGLAccounts);
+  mainwindow.workOrdersUpdated.connect(fillListGLAccounts);
+
+  _glAccounts.itemSelected.connect(openWindowGLAccounts);
+  _glAccounts["populateMenu(QMenu*,XTreeWidgetItem*,int)"]
+    .connect(populateMenuGLAccounts);
+
+  _dockGLAccounts.visibilityChanged.connect(fillListGLAccounts);
+}
+
+/*!
+  Fills the list with sales history data based on parameters determined by
+  sales history preferences.
+
+  \sa preferencesGLAccounts()
+*/
+function fillListGLAccounts()
+{
+  _dockGLAccounts = mainwindow.findChild("_dockGLAccounts");
+  _glAccounts = mainwindow.findChild("_glAccounts");
+
+  if (!_dockGLAccounts.visible) 
+    return;
+
+  var params = new Object;
+  params.asset = qsTr("Asset");
+  params.liability = qsTr("Liability");
+  params.revenue = qsTr("Revenue");
+  params.expense = qsTr("Expense");
+  params.equity = qsTr("Equity");
+  params.accnt_id_list = preferences.value("MonitoredAccounts");
+
+  _glAccounts.populate(toolbox.executeDbQuery("desktop","glaccountBal", params));
+}
+
+/*! 
+  Opens the window associated with the selected item.
+*/
+function openWindowGLAccounts()
+{
+  var ui;
+  var params = getDatesGLAccounts();
+  params.run = true;
+
+  // TO DO: Work it out so totals launch a window too
+  if (_glAccounts.id() == -1)
+    return;
+  
+  // Make sure we can open the window
+  if (!privilegeCheckGLAccounts())
+    return;
+
+  // Determine which window to open
+  params.transtype = "R";
+  params.ordertype = "WO";
+  params.run = true;
+
+  // Open the window and perform any special handling required
+  toolbox.openWindow(ui);
+  toolbox.lastWindow()._warehouse.setAll();
+  var dates = toolbox.lastWindow().findChild("_dates");
+  dates.setStartDate(params.startDate);
+  dates.setEndDate(params.endDate);
+  toolbox.lastWindow().set(params);
+}
+
+/*!
+  Adds actions to \a pMenu, typically from a right click on sales history.
+*/
+function populateMenuGLAccounts(pMenu)
+{
+  var menuItem;
+  var enable = privilegeCheckGLAccounts();
+
+  menuItem = toolbox.menuAddAction(pMenu, _open, enable);
+  menuItem.triggered.connect(openWindowGLAccounts);
+}
+
+/*!
+  Returns whether user has privileges to view Sales History detail.
+*/
+function privilegeCheckGLAccounts()
+{
+  return privileges.check("ViewGLTransactions");
+}
+
+/*! 
+  Launches the preferences window where the user can set sales data output preferences.
+*/
+function preferencesGLAccounts()
+{
+  params = new Object;
+
+  var newdlg = toolbox.openWindow("preferencesSelections", mainwindow,
+                                  Qt.ApplicationModal, Qt.Dialog);
+  toolbox.lastWindow().set(params);
+  if (newdlg.exec())
+    fillListGLAccounts();
+}
