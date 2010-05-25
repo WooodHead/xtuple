304df128303ecfa0341801f217edcb56d8f9db5c
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-05-25 20:31:44 +0000

    issue #10062:  Include tax on credit memos generated from P/O returns.

--- xtupleserver/trunk/dbscripts/functions/postporeturncreditmemo.sql
+++ xtupleserver/trunk/dbscripts/functions/postporeturncreditmemo.sql
@@ -26,6 +26,9 @@ DECLARE
   _tmpTotal NUMERIC;
   _test INTEGER;
   _exchDate DATE;
+  _tax RECORD;
+  _taxAmount NUMERIC := 0;
+  _taxAmount_base NUMERIC;
 
 BEGIN
 --Set things up
@@ -36,8 +39,8 @@ BEGIN
   _glseriesTotal := 0;
 
 --Get poreject data
-  SELECT pohead_vend_id, pohead_number, pohead_curr_id, pohead_orderdate,
-         poitem_id, poitem_itemsite_id,poitem_expcat_id,
+  SELECT pohead_vend_id, pohead_number, pohead_curr_id, pohead_orderdate, pohead_taxzone_id,
+         poitem_id, poitem_itemsite_id,poitem_expcat_id, poitem_taxtype_id,
         itemsite_costcat_id, poreject_qty, poreject_date,
         ('Return of Item ' || COALESCE(item_number,poitem_vend_item_number)
            || ', qty. ' || formatqty(poreject_qty)) AS notes,
@@ -83,6 +86,47 @@ BEGIN
     END IF;
   END IF;
 
+--  AP Open Item record
+    INSERT INTO apopen
+    ( apopen_id, apopen_username, apopen_journalnumber,
+      apopen_vend_id, apopen_docnumber, apopen_doctype, apopen_ponumber,
+      apopen_docdate, apopen_duedate, apopen_distdate, apopen_terms_id,
+      apopen_amount, apopen_paid, apopen_open, apopen_notes, apopen_accnt_id, apopen_curr_id,
+      apopen_closedate )
+    VALUES
+    ( _apopenid, CURRENT_USER, _journalNumber,
+      _p.pohead_vend_id, _docNumber, 'C', _p.pohead_number,
+      CURRENT_DATE, CURRENT_DATE, CURRENT_DATE, -1,
+      round(_itemAmount, 2), 0, (round(_itemAmount, 2) <> 0), _p.notes, -1, _p.pohead_curr_id,
+      CASE WHEN (round(_itemAmount, 2) = 0) THEN _p.poreject_date END );
+
+-- Taxes
+    FOR _tax IN
+      SELECT taxdetail_tax_id, sum(taxdetail_tax) AS taxdetail_tax,
+        currToBase(_p.pohead_curr_id, round(sum(taxdetail_tax),2), current_date) AS taxbasevalue
+      FROM calculateTaxDetail(_p.pohead_taxzone_id, _p.poitem_taxtype_id,
+	  		       current_date, _p.pohead_curr_id, 
+			       _itemAmount) 
+      GROUP BY taxdetail_tax_id
+    LOOP
+      INSERT INTO apopentax (taxhist_basis,taxhist_percent,taxhist_amount,taxhist_docdate, taxhist_tax_id, taxhist_tax, 
+                             taxhist_taxtype_id, taxhist_parent_id, taxhist_journalnumber ) 
+      VALUES (0, 0, 0, current_date, _tax.taxdetail_tax_id, _tax.taxdetail_tax, getadjustmenttaxtypeid(), 
+              _apopenid, _journalNumber);
+
+      _taxAmount := _taxAmount + _tax.taxdetail_tax;
+ 
+    END LOOP;
+
+    _taxAmount_base := addTaxToGLSeries(_sequence,
+		       'A/P', 'CM', _docNumber,
+		       _p.pohead_curr_id, current_date, current_date,
+                      'apopentax', _apopenid,
+                      _p.notes);
+
+    UPDATE apopen SET apopen_amount = round(_itemAmount + _taxAmount,2)
+    WHERE (apopen_id = _apopenid);
+
 --  Distribute from the clearing account
     PERFORM insertIntoGLSeries( _sequence, 'A/P', 'CM', _docNumber,
                 _a.lb_accnt_id,
@@ -111,9 +155,8 @@ BEGIN
      WHERE (poitem_id=_p.poitem_id);
 
 --  Post to A/P
-
   SELECT insertIntoGLSeries( _sequence, 'A/P', 'CM', _docNumber,
-                             accnt_id, round(_itemAmount_base, 2) *-1,
+                             accnt_id, round(_itemAmount_base + _taxAmount_base, 2) *-1,
                              current_date, _p.notes ) INTO _test
   FROM accnt
   WHERE (findAPAccount(_p.pohead_vend_id)=accnt_id);
@@ -135,19 +178,6 @@ BEGIN
 
 --  Post it all
   PERFORM postGLSeries(_sequence, _journalNumber);
-
-  INSERT INTO apopen
-  ( apopen_id, apopen_username, apopen_journalnumber,
-    apopen_vend_id, apopen_docnumber, apopen_doctype, apopen_ponumber,
-    apopen_docdate, apopen_duedate, apopen_distdate, apopen_terms_id,
-    apopen_amount, apopen_paid, apopen_open, apopen_notes, apopen_accnt_id, apopen_curr_id,
-    apopen_closedate )
-  VALUES
-  ( _apopenid, CURRENT_USER, _journalNumber,
-    _p.pohead_vend_id, _docNumber, 'C', _p.pohead_number,
-    CURRENT_DATE, CURRENT_DATE, CURRENT_DATE, -1,
-    round(_itemAmount, 2), 0, (round(_itemAmount, 2) <> 0), _p.notes, -1, _p.pohead_curr_id,
-    CASE WHEN (round(_itemAmount, 2) = 0) THEN _p.poreject_date END );
 
   RETURN _journalNumber;
 
