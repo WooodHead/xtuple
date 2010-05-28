2bf47aa43ef3bd0e2f9bbaa81c8e79995adf4054
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-05-28 14:19:16 +0000

    Issue #9456:check for overlapping cmhead and aropen numbers

--- xtupleserver/trunk/dbscripts/functions/releasearmemonumber.sql
+++ xtupleserver/trunk/dbscripts/functions/releasearmemonumber.sql
@@ -1,5 +1,5 @@
 
-CREATE OR REPLACE FUNCTION releaseARMemoNumber(INTEGER) RETURNS BOOLEAN AS '
+CREATE OR REPLACE FUNCTION releaseARMemoNumber(INTEGER) RETURNS BOOLEAN AS $$
 DECLARE
   pNumber ALIAS FOR $1;
   _test INTEGER;
@@ -9,8 +9,17 @@ BEGIN
 --  Check to see if a A/R Open Item exists with the passed number
   SELECT aropen_id INTO _test
   FROM aropen
-  WHERE ( (aropen_doctype IN (''D'', ''C'', ''R''))
+  WHERE ( (aropen_doctype IN ('D', 'C', 'R'))
    AND (aropen_docnumber=CAST(pNumber AS TEXT)) );
+
+  IF (FOUND) THEN
+    RETURN FALSE;
+  END IF;
+
+--  Check to see if a C/M exists with the passed C/M Number
+  SELECT cmhead_id INTO _test
+  FROM cmhead
+  WHERE (cmhead_number=CAST(pNumber AS TEXT));
 
   IF (FOUND) THEN
     RETURN FALSE;
@@ -19,7 +28,7 @@ BEGIN
 --  Check to see if ARMemoNumber orderseq has been incremented past the passed number
   SELECT orderseq_number INTO _test
   FROM orderseq
-  WHERE (orderseq_name=''ARMemoNumber'');
+  WHERE (orderseq_name='ARMemoNumber');
 
   IF ((_test - 1) <> pNumber) THEN
     RETURN FALSE;
@@ -28,10 +37,10 @@ BEGIN
 --  Decrement the orderseq, releasing the passed number
   UPDATE orderseq
   SET orderseq_number = (orderseq_number - 1)
-  WHERE (orderseq_name=''ARMemoNumber'');
+  WHERE (orderseq_name='ARMemoNumber');
 
   RETURN TRUE;
 
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
 
