bb77b755b9a6a9e2eee08f1f3cf5edfaadcb4e1c
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-06-20 00:56:50 +0000

    issue #11622:  Database support for editing posted Journal Entries.

--- xtupleserver/trunk/updatescripts/from_351/insertPriv.sql
+++ xtupleserver/trunk/updatescripts/from_351/insertPriv.sql
@@ -1,1 +1,3 @@
 INSERT INTO priv (priv_module, priv_name, priv_descrip) VALUES ('Accounting','DeletePostedJournals','Can delete posted Standard and Simple Journal Entries');
+INSERT INTO priv (priv_module, priv_name, priv_descrip) VALUES ('Accounting','EditPostedJournals','Can delete posted Standard and Simple Journal Entries');
+
