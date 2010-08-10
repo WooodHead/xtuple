4af3b20b568c4a9ffef03f0fd82157580c0012f8
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-08-10 16:01:19 +0000

    issue #11571:  Update POS package menus to work in new Setup screen.

--- xtpos/trunk/resources/client/scripts/initMenu.js
+++ xtpos/trunk/resources/client/scripts/initMenu.js
@@ -30,24 +30,6 @@ var regHistAction	= toolbox.menuInsertAction(reportMenu, earnedCommAction,
 var detailedRegHistAction = toolbox.menuInsertAction(reportMenu, earnedCommAction,
                         "Detailed Register History", privileges.value("ViewRetailSales"));
 
-toolbox.menuInsertSeparator(reportMenu, earnedCommAction);
-
-// Master Information menu references
-var masterInfoMenu 	= mainwindow.findChild("menu.sales.masterinfo");
-var termsAction 	= mainwindow.findChild("so.terms");
-
-// Retail Sites action
-var name;
-if (metrics.value("MultiWhs") == "t")
-  name = "Retail Sites...";
-else
-  name = "Retail Site...";
-var rtlSiteAction 	= toolbox.menuInsertAction(masterInfoMenu, termsAction, name,
-			privileges.value("MaintainRetailSites"));
-
-// Separator
-toolbox.menuInsertSeparator(masterInfoMenu, termsAction);
-
 // Connect new menus to existing custom command actions
 var cshRegTrig	= mainwindow.findChild("custom.cashRegisters");
 cshRegAction.triggered.connect(cshRegTrig, "trigger");
@@ -61,23 +43,12 @@ regHistAction.triggered.connect(regHistTrig, "trigger");
 var detailedRegHistTrig	= mainwindow.findChild("custom.dspDetailedRegisterHistory");
 detailedRegHistAction.triggered.connect(detailedRegHistTrig, "trigger");
 
-var rtlSiteTrig 	= mainwindow.findChild("custom.retailSite");
-var rtlSitesTrig 	= mainwindow.findChild("custom.retailSites");
-
-if (metrics.value("MultiWhs") == "t")
-  rtlSiteAction.triggered.connect(rtlSitesTrig, "trigger");
-else
-  rtlSiteAction.triggered.connect(rtlSiteTrig, "trigger");
-
-
 // Remove custom command actions
 var customSalesMenu	= mainwindow.findChild("menu.sales.custom");
 toolbox.menuRemove(customSalesMenu,cshRegTrig);
 toolbox.menuRemove(customSalesMenu,rtlSaleSearchTrig);
 toolbox.menuRemove(customSalesMenu,regHistTrig);
 toolbox.menuRemove(customSalesMenu,detailedRegHistTrig);
-toolbox.menuRemove(customSalesMenu,rtlSitesTrig);
-toolbox.menuRemove(customSalesMenu,rtlSiteTrig);
 
 // Remove custom menu if no custom commands left
 if (!toolbox.menuActionCount(customSalesMenu))
