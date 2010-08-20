4ce80c315ffc36c8144e47fefb76f35e5c831c8b
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-08-20 18:16:35 +0000

    bug #11912 - speed up performance by ~30x for an unposted invoice

--- xtupleserver/trunk/dbscripts/functions/invoicetotal.sql
+++ xtupleserver/trunk/dbscripts/functions/invoicetotal.sql
@@ -1,48 +1,39 @@
 CREATE OR REPLACE FUNCTION invoiceTotal(INTEGER) RETURNS NUMERIC AS $$
 DECLARE
   pInvoiceId ALIAS FOR $1;
-  _result NUMERIC;
+  _linesum   NUMERIC;
+  _linetax   NUMERIC;
+  _result    NUMERIC;
   _allocated NUMERIC;
+  _posted    BOOLEAN;
 
 BEGIN
 
-  SELECT noNeg(invchead_freight + invchead_misc_amount + COALESCE(
-         ( SELECT SUM(tax) AS tax 
-           FROM (
-                SELECT ROUND(SUM(taxdetail_tax),2) AS tax 
-                FROM tax 
-                 JOIN calculateTaxDetailSummary('I', pInvoiceId, 'T') ON (taxdetail_tax_id=tax_id)
-	        GROUP BY tax_id) AS data
-           ),0) +
-         COALESCE(SUM(ROUND(((invcitem_billed * invcitem_qty_invuomratio) *
-                             (invcitem_price / COALESCE(invcitem_price_invuomratio,1))),2)), 0)
-                          ) into _result
-  FROM invchead
-     LEFT OUTER JOIN invcitem ON (invcitem_invchead_id=invchead_id)
-     LEFT OUTER JOIN item ON (invcitem_item_id=item_id)
-  WHERE (invchead_id=pInvoiceId)
-  GROUP BY invchead_freight, invchead_misc_amount;
+  SELECT SUM(ROUND(COALESCE((invcitem_billed * invcitem_qty_invuomratio) *
+                             (invcitem_price / COALESCE(invcitem_price_invuomratio,1)), 0),2))
+                          INTO _linesum
+  FROM invcitem
+  WHERE (invcitem_invchead_id=pInvoiceId);
 
-  SELECT SUM(total_allocated) INTO _allocated
-  FROM
-    (SELECT COALESCE(SUM(CASE WHEN((aropen_amount - aropen_paid) >=
-                       currToCurr(aropenco_curr_id, aropen_curr_id,
-                          aropenco_amount, aropen_docdate))
-           THEN currToCurr(aropenco_curr_id, invchead_curr_id,
-                   aropenco_amount, aropen_docdate)
-           ELSE currToCurr(aropen_curr_id, invchead_curr_id,
-                   aropen_amount - aropen_paid, aropen_docdate)
-           END),0) AS total_allocated
-     FROM aropenco, aropen, cohead, invchead
-    WHERE ( (aropenco_aropen_id=aropen_id)
-      AND   (aropenco_cohead_id=cohead_id)
-      AND   ((aropen_amount - aropen_paid) > 0)
-      AND   (cohead_number=invchead_ordernumber)
-      AND   (NOT invchead_posted)
-      AND   (invchead_id=pInvoiceId) )
-    UNION 
+  -- TODO: why sum on the result of select round(sum(), 2)?
+  SELECT SUM(tax) INTO _linetax
+    FROM (SELECT ROUND(SUM(COALESCE(taxdetail_tax, 0)),2) AS tax 
+            FROM tax 
+            JOIN calculateTaxDetailSummary('I', pInvoiceId, 'T') ON (taxdetail_tax_id=tax_id)
+	  GROUP BY tax_id) AS data;
+
+  SELECT noNeg(invchead_freight + invchead_misc_amount + COALESCE(_linetax, 0) + COALESCE(_linesum, 0)),
+         invchead_posted INTO _result, _posted
+  FROM invchead
+  WHERE (invchead_id=pInvoiceId);
+
+  IF NOT FOUND THEN
+    RETURN 0;
+  END IF;
+
+  IF (_posted) THEN
     SELECT COALESCE(SUM(currToCurr(arapply_curr_id, aropen_curr_id,
-                                   arapply_applied, aropen_docdate)),0) AS total_allocated
+                                   arapply_applied, aropen_docdate)),0) INTO _allocated
      FROM arapply, aropen, invchead
     WHERE ( (invchead_posted)
       AND   (invchead_id=pInvoiceId)
@@ -50,15 +41,26 @@ BEGIN
       AND   (aropen_doctype='I')
       AND   (arapply_target_aropen_id=aropen_id)
       AND   (arapply_reftype='S')
-      AND   (invchead_posted) ) 
-    ) AS totalalloc;
-  
-
-  IF (NOT FOUND) THEN
-    return 0;
+      AND   (invchead_posted) ) ;
   ELSE
-    RETURN _result - _allocated;
+    SELECT COALESCE(SUM(CASE WHEN((aropen_amount - aropen_paid) >=
+                       currToCurr(aropenco_curr_id, aropen_curr_id,
+                          aropenco_amount, aropen_docdate))
+           THEN currToCurr(aropenco_curr_id, invchead_curr_id,
+                   aropenco_amount, aropen_docdate)
+           ELSE currToCurr(aropen_curr_id, invchead_curr_id,
+                   aropen_amount - aropen_paid, aropen_docdate)
+           END),0) INTO _allocated
+     FROM aropenco, aropen, cohead, invchead
+    WHERE ( (aropenco_aropen_id=aropen_id)
+      AND   (aropenco_cohead_id=cohead_id)
+      AND   ((aropen_amount - aropen_paid) > 0)
+      AND   (cohead_number=invchead_ordernumber)
+      AND   (NOT invchead_posted)
+      AND   (invchead_id=pInvoiceId) );
   END IF;
+  
+  RETURN _result - COALESCE(_allocated, 0);
 
 END;
 $$ LANGUAGE 'plpgsql';
