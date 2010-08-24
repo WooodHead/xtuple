2b8ec91ad7140ddbcf26334a9edd168bf470288c
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-08-24 13:30:51 +0000

    issue #11869:  Fix some bugs noted by ZenQA.

--- xtupleserver/trunk/dbscripts/functions/formatglaccount.sql
+++ xtupleserver/trunk/dbscripts/functions/formatglaccount.sql
@@ -1,5 +1,5 @@
 
-CREATE OR REPLACE FUNCTION formatGLAccount(INTEGER) RETURNS TEXT IMMUTABLE AS '
+CREATE OR REPLACE FUNCTION formatGLAccount(INTEGER) RETURNS TEXT IMMUTABLE AS $$
 DECLARE
   pAccntid ALIAS FOR $1;
   _number TEXT;
@@ -7,41 +7,41 @@ DECLARE
 
 BEGIN
 
-  _number := '''';
+  _number := '';
 
-  SELECT COALESCE(accnt_company, '''') AS accnt_company,
-         COALESCE(accnt_profit, '''') AS accnt_profit,
+  SELECT COALESCE(accnt_company, '') AS accnt_company,
+         COALESCE(accnt_profit, '') AS accnt_profit,
          accnt_number,
-         COALESCE(accnt_sub, '''') AS accnt_sub INTO _accnt
+         COALESCE(accnt_sub, '') AS accnt_sub INTO _accnt
   FROM accnt
   WHERE (accnt_id=pAccntid);
 
   IF (NOT FOUND) THEN
-    RETURN ''Error'';
+    RETURN 'Error';
   END IF;
 
   IF ( ( SELECT metric_value::INTEGER
          FROM metric
-         WHERE (metric_name=''GLCompanySize'') ) > 0 ) THEN
-    _number := _accnt.accnt_company || ''-'';
+         WHERE (metric_name='GLCompanySize') ) > 0 ) THEN
+    _number := _accnt.accnt_company || '-';
   END IF;
 
   IF ( ( SELECT metric_value::INTEGER
          FROM metric
-         WHERE (metric_name=''GLProfitSize'') ) > 0 ) THEN
-    _number := _number || _accnt.accnt_profit || ''-'';
+         WHERE (metric_name='GLProfitSize') ) > 0 ) THEN
+    _number := _number || _accnt.accnt_profit || '-';
   END IF;
 
   _number := _number || _accnt.accnt_number;
 
   IF ( ( SELECT metric_value::INTEGER
          FROM metric
-         WHERE (metric_name=''GLSubaccountSize'') ) > 0 ) THEN
-    _number := _number || ''-'' || _accnt.accnt_sub;
+         WHERE (metric_name='GLSubaccountSize') ) > 0 ) THEN
+    _number := _number || '-' || _accnt.accnt_sub;
   END IF;
 
   RETURN _number;
 
 END;
-' LANGUAGE 'plpgsql';
+$$	 LANGUAGE 'plpgsql';
 
