d0d48f1e356edd12b36970dab3d7306116aff435
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-08-30 14:43:13 +0000

    issue #11869:  Changes to support project accounting.

--- xtupleserver/trunk/dbscripts/functions/invexpense.sql
+++ xtupleserver/trunk/dbscripts/functions/invexpense.sql
@@ -5,13 +5,20 @@ END;
 $$ LANGUAGE 'plpgsql';
 
 CREATE OR REPLACE FUNCTION invExpense(INTEGER, NUMERIC, INTEGER, TEXT, TEXT, TIMESTAMP WITH TIME ZONE) RETURNS INTEGER AS $$
+BEGIN
+ RETURN invExpense($1, $2, $3, $4, $5, CURRENT_TIMESTAMP, NULL);
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION invExpense(INTEGER, NUMERIC, INTEGER, TEXT, TEXT, TIMESTAMP WITH TIME ZONE, INTEGER) RETURNS INTEGER AS $$
 DECLARE
   pItemsiteid ALIAS FOR $1;
   pQty ALIAS FOR $2;
   pExpcatid ALIAS FOR $3;
   pDocumentNumber ALIAS FOR $4;
   pComments ALIAS FOR $5;
-  pGlDistTS     ALIAS FOR $6;
+  pGlDistTS ALIAS FOR $6;
+  pPrjid ALIAS FOR $7;
   _invhistid INTEGER;
   _itemlocSeries INTEGER;
 
@@ -31,7 +38,7 @@ BEGIN
                        CASE WHEN (pQty < 0) THEN ('Reverse Material Expense for item ' || item_number || E'\n' ||  pComments)
                             ELSE  ('Material Expense for item ' || item_number || E'\n' ||  pComments)
                        END,
-                       expcat_exp_accnt_id, costcat_asset_accnt_id,
+                       getPrjAccntId(pPrjid, expcat_exp_accnt_id), costcat_asset_accnt_id,
                        _itemlocSeries, pGlDistTS) INTO _invhistid
   FROM itemsite, item, costcat, expcat
   WHERE ( (itemsite_item_id=item_id)
