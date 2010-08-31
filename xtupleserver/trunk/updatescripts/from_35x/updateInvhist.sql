ce49071d6797728c575e11faae9a88e64bd2edd3
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-08-31 16:59:39 +0000

    Issue #11793:asofqoh

---
+++ xtupleserver/trunk/updatescripts/from_35x/updateInvhist.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT ON (invhist_itemsite_id) invhist_itemsite_id, recalculateinvhist(invhist_id)
+FROM invhist
+WHERE (invhist_transdate<invhist_created)
+ORDER BY invhist_itemsite_id, invhist_transdate;
+
