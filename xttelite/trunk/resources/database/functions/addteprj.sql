93450ea18ea4a5b680f3473f8713cd9675771410
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-09-01 13:22:42 +0000

    Issue #11934:correctly save and populate te.teprj

--- xttelite/trunk/resources/database/functions/addteprj.sql
+++ xttelite/trunk/resources/database/functions/addteprj.sql
@@ -1,31 +1,18 @@
--- Function: te.addteprj(integer, integer, numeric)
-
--- DROP FUNCTION te.addteprj(integer, integer, numeric);
-
-CREATE OR REPLACE FUNCTION te.addteprj(integer, integer, numeric)
-  RETURNS integer AS
-$BODY$
+CREATE OR REPLACE FUNCTION te.addteprj(integer, integer, numeric) RETURNS integer AS $$
 DECLARE
-_prjid int4;
 pPrjID ALIAS FOR $1;
 pCustID ALIAS FOR $2;
 pRate ALIAS FOR $3;
 
 BEGIN
-
+--raise notice 'pPrjID=%,pCustID=%,pRate=%', pPrjID, pCustID, pRate;
 --if new, do an insert to teprj, else, update
-	select teprj_id into _prjid from te.teprj where teprj_prj_id = pPrjID;
+  IF (EXISTS(select teprj_id from te.teprj where teprj_prj_id = pPrjID)) THEN
+    update te.teprj set teprj_rate = pRate, teprj_cust_id = pCustID where teprj_prj_id = pPrjID;
+  ELSE
+    insert into te.teprj(teprj_prj_id,teprj_cust_id,teprj_rate) VALUES (pPrjID,pCustID,pRate);
+  END IF;
 
-
-	IF (_prjid > 0) THEN
-		update te.teprj set teprj_rate = pRate,teprj_cust_id = pCustID where teprj_id = _prjid;
-	ELSE
-	        insert into  te.teprj(teprj_prj_id,teprj_cust_id,teprj_rate) VALUES (pPrjID,pCustID,pRate);
-	END IF;
-
-RETURN 1;
+  RETURN 0;
 END;
-$BODY$
-  LANGUAGE 'plpgsql' VOLATILE
-  COST 100;
-ALTER FUNCTION te.addteprj(integer, integer, numeric) OWNER TO "admin";
+$$ LANGUAGE 'plpgsql';
