9a304bbd69aed4eb81743a677317c2cfa15c4317
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-09-03 20:57:32 +0000

    Issue #11941:rework connections and save/populate functions

--- xttelite/trunk/resources/client/scripts/employee.js
+++ xttelite/trunk/resources/client/scripts/employee.js
@@ -8,25 +8,21 @@ var billingPage = toolbox.loadUi("tebillingemp", mywindow);
 //insert the new tab
 toolbox.tabInsertTab(tablist,4,billingPage, "Billing Information");
 
-var _save = mywindow.findChild("_save");
-_save.clicked.connect(billingSave);
-             
-
+// employee.ssave() emits a "saved" signal after save
+mywindow.saved.connect(billingSave);
+         
+// employee.populate() emits a "populated" signal after populating
+mywindow.populated.connect(getEmployeeRate);
+         
 var _number 	= mywindow.findChild("_number");
 var _rateemp	= mywindow.findChild("_rateemp"); 
+_rateemp.setValidator(toolbox.moneyVal());
 var _cust		= mywindow.findChild("_cust");
 var _name 		= mywindow.findChild("_name");
 var _code		= mywindow.findChild("_code");
 mywindow.findChild("_item").visible = false;
 mywindow.findChild("_itemLit").visible = false;
 mywindow.findChild("_cust").visible = false;
-
-//_code.textChanged.connect(getEmployeeRate);
-//_number.textChanged.connect(getCustomerRate);
-//_number['numberChanged(QString)'].connect(getCustomerRate);
-
-_code['textChanged(QString)'].connect(getEmployeeRate);
-
 
 function set(params)
 {
@@ -38,50 +34,30 @@ function set(params)
 
 function billingSave()
 {
-
    var params = new Object();
-   params.rate = mywindow.findChild("_rateemp").text;
+   params.rate = _rateemp.toDouble();
    params.code = _code.text;
-   if (params.rate == '')
-   {
-     params.rate = 0;
-   }
    var q = toolbox.executeQuery("SELECT emp_id "
-        + "FROM emp "
-        + ' WHERE emp_code = <? value("code") ?>;',params);
+                              + "FROM emp "
+                              + ' WHERE emp_code = <? value("code") ?>;',params);
 
    if (q.first())
    {
      params.empid = q.value("emp_id");
+     var q = toolbox.executeDbQuery("te","addteemprate",params);
    }
-
-   var q = toolbox.executeDbQuery("te","addteemprate",params);
-
 }
-
-
 
 function getEmployeeRate()
 {
-    var params = new Object();
-    params.code = mywindow.findChild("_code").text;    
-
-   var q = toolbox.executeQuery("SELECT emp_id "
-        + "FROM emp "
-        + ' WHERE emp_code = <? value("code") ?>;',params);
+   var params = new Object();
+   params.code = _code.text;    
+   var q = toolbox.executeQuery("SELECT teemprate_rate "
+                              + "FROM emp JOIN te.teemprate ON (teemprate_emp_id=emp_id) "
+                              + 'WHERE emp_code = <? value("code") ?>;',params);
 
     if (q.first())
     {
-
-      params.empid = q.value("emp_id");
-       var q = toolbox.executeQuery("SELECT teemprate_rate as rate "
-        + "FROM te.teemprate "
-        + ' WHERE teemprate_emp_id = <? value("empid") ?>;',params);
-
-      if (q.first())
-      {
-        mywindow.findChild("_rateemp").setText(q.value("rate"));
-      }
+      _rateemp.text = (q.value("teemprate_rate"));
     }
-
 }
