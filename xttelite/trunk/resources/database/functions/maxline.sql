b89a85227dffa5fa0c13ff65dd2f179be3ed5b3f
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-09-03 20:07:49 +0000

    Issue #11961:correctly reset _error boolean to false

--- xttelite/trunk/resources/database/functions/maxline.sql
+++ xttelite/trunk/resources/database/functions/maxline.sql
@@ -1,28 +1,14 @@
-
--- Function: te.maxline(integer)
-
--- DROP FUNCTION te.maxline(integer);
-
-CREATE OR REPLACE FUNCTION te.maxline(integer)
-  RETURNS integer AS
-$BODY$
+CREATE OR REPLACE FUNCTION te.maxline(integer) RETURNS integer AS $$
 DECLARE
   pHeadID ALIAS FOR $1;
-  _returnVal INTEGER;
+  _returnVal INTEGER := 0;
 BEGIN
 
-  SELECT max(teitem_linenumber) INTO _returnVal
+  SELECT COALESCE(max(teitem_linenumber), 0) INTO _returnVal
   FROM te.teitem
   WHERE (teitem_tehead_id=pHeadID);
 
-  IF (_returnVal IS NULL) THEN
-	--RAISE EXCEPTION 'Sheet % not found.', pHeadID;
-	_returnVal = 1;
-  END IF;
-
   RETURN _returnVal;
 END;
-$BODY$
-  LANGUAGE 'plpgsql' VOLATILE
-  COST 100;
-ALTER FUNCTION te.maxline(integer) OWNER TO "admin";
+$$
+LANGUAGE 'plpgsql';
