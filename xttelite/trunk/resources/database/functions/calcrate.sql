ec9360a37c1eb8107af21434eb931e162e5aa766
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-09-22 16:26:03 +0000

    issue #12022: Get time sheet posting to work on T&E.

--- xttelite/trunk/resources/database/functions/calcrate.sql
+++ xttelite/trunk/resources/database/functions/calcrate.sql
@@ -1,26 +1,26 @@
 CREATE OR REPLACE FUNCTION te.calcrate(numeric, char(2)) RETURNS numeric AS $$
 DECLARE
 pAmount ALIAS FOR $1;
-pType ALIAS FOR $2;
+pPeriod ALIAS FOR $2;
 _state integer;
 _count integer;
 
 BEGIN
   -- Convert amount to hourly rate
-  IF (pType = 'H') THEN  -- hourly
+  IF (pPeriod = 'H') THEN  -- hourly
     RETURN round(pAmount,2);
-  ELSIF (pType = 'D') THEN -- daily
+  ELSIF (pPeriod = 'D') THEN -- daily
     RETURN round(pAmount / 8, 2);
-  ELSIF (pType = 'W') THEN  -- weekly
+  ELSIF (pPeriod = 'W') THEN  -- weekly
     RETURN round(pAmount / 40, 2);
-  ELSIF (pType = 'BW') THEN  -- bi-weekly
+  ELSIF (pPeriod = 'BW') THEN  -- bi-weekly
     RETURN round(pAmount / 80, 2);
-  ELSIF (pType = 'M') THEN -- monthly
+  ELSIF (pPeriod = 'M') THEN -- monthly
     RETURN round(pAmount / 160, 2);
-  ELSIF (pType = 'Y') THEN -- annually 
+  ELSIF (pPeriod = 'Y') THEN -- annually 
     RETURN round(pAmount / 2080, 2);
   ELSE
-    RAISE EXCEPTION 'Unknown period type passed: %', pType;
+    RAISE EXCEPTION 'Unknown period type passed: %', pPeriod;
   END IF;
 
 END;
