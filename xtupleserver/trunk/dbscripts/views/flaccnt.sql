600dd17679a0a91cd516a7a3ff7fea3fa4126168
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-09-26 20:30:48 +0000

    issue #12020: Add in missing filter criteria that didn't carry over from function to view.

--- xtupleserver/trunk/dbscripts/views/flaccnt.sql
+++ xtupleserver/trunk/dbscripts/views/flaccnt.sql
@@ -15,12 +15,12 @@ CREATE OR REPLACE VIEW flaccnt AS
     JOIN flitem ON (flhead_id=flitem_flhead_id),
     accnt	
   WHERE ((flitem_accnt_id=-1)
-   AND ((accnt_type=flitem_type)
-   OR (accnt_company=flitem_company)
-   OR (accnt_profit=flitem_profit)
-   OR (accnt_number=flitem_number)
-   OR (accnt_sub=flitem_sub)
-   OR (accnt_subaccnttype_code=flitem_subaccnttype_code)))
+    AND ((flitem_type='') OR (accnt_type=flitem_type))
+    AND ((flitem_company='All') OR (accnt_company=flitem_company))
+    AND ((flitem_profit='All') OR (accnt_profit=flitem_profit))
+    AND ((flitem_number='All') OR (accnt_number=flitem_number))
+    AND ((flitem_sub='All') OR (accnt_sub=flitem_sub))
+    AND ((flitem_subaccnttype_code='All') OR (accnt_subaccnttype_code=flitem_subaccnttype_code)))
   ORDER BY accnt_company, accnt_profit,accnt_number,accnt_sub
 );
 
