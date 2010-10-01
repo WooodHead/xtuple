59a9cdb0e05e91686f473a5e9dd3b26c3020a3b6
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-10-01 17:47:25 +0000

    Create packages for xtpos 3.6.0 beta 2

--- xtpos/trunk/updatescripts/from_351/deleteCmd.sql
+++ xtpos/trunk/updatescripts/from_351/deleteCmd.sql
@@ -1,3 +1,5 @@
+UPDATE pkghead SET pkghead_indev = true WHERE pkghead_name = 'xtpos';
+
 DELETE FROM xtpos.pkgcmdarg
 WHERE cmdarg_cmd_id IN (
   SELECT cmd_id
@@ -6,3 +8,5 @@ WHERE cmdarg_cmd_id IN (
 
 DELETE FROM xtpos.pkgcmd
 WHERE (cmd_title IN ('Retail Site','Retail Sites'));
+
+UPDATE pkghead SET pkghead_indev = false WHERE pkghead_name = 'xtpos';
