0304042e32f7e87644ba339b18058cffc0052754
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-10-08 14:29:35 +0000

    issue #12206: Pull out standard edition specific function drops.

--- xtupleserver/trunk/updatescripts/from_360beta2/dropFunctions.sql
+++ xtupleserver/trunk/updatescripts/from_360beta2/dropFunctions.sql
@@ -1,6 +1,4 @@
 SELECT dropIfExists('function','locksohead(integer)');
 SELECT dropIfExists('function','releasesohead(integer)');
-SELECT dropIfExists('function','locktohead(integer)');
-SELECT dropIfExists('function','releasetohead(integer)');
 SELECT dropIfExists('function','lockhead(text, integer)');
 SELECT dropIfExists('function','releaseheadlock(text, integer)');
