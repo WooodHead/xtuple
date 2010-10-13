a5382fab7a4ba90d5df470796721841a2829616b
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-10-13 18:07:49 +0000

    issue #11793: Pull out rest of db side functions relating to an approach that is unworkable.

--- xtupleserver/trunk/updatescripts/from_360beta2/dropFunctions.sql
+++ xtupleserver/trunk/updatescripts/from_360beta2/dropFunctions.sql
@@ -2,3 +2,6 @@ SELECT dropIfExists('function','locksohead(integer)');
 SELECT dropIfExists('function','releasesohead(integer)');
 SELECT dropIfExists('function','lockhead(text, integer)');
 SELECT dropIfExists('function','releaseheadlock(text, integer)');
+SELECT dropIfExists('FUNCTION','calcAsOfQoh(integer, integer, date)');
+SELECT dropIfExists('FUNCTION','calcAsOfQoh(integer, date)');
+SELECT dropIfExists('FUNCTION','recalculateinvhist(integer) ');
