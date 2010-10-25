78f0d6a3492952aa0b22f98825feeec595629c7f
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-10-25 23:27:08 +0000

    issue #12378: Remove constraints that cause problems with updates.

--- xtupleserver/trunk/dbscripts/triggers/prjtask.sql
+++ xtupleserver/trunk/dbscripts/triggers/prjtask.sql
@@ -12,26 +12,6 @@ BEGIN
     RAISE EXCEPTION 'You must ender a valid name.';	
   END IF;
 
-  SELECT usrpref_username INTO _test
-  FROM usrpref
-  WHERE ((usrpref_username=NEW.prjtask_username)
-  AND (usrpref_name='active')
-  AND (usrpref_value='t'));
-
-  IF NOT (FOUND) THEN
-    RAISE EXCEPTION 'Assigned to is not an active user.';
-  END IF;
-
-  SELECT usrpref_username INTO _test
-  FROM usrpref
-  WHERE ((usrpref_username=NEW.prjtask_owner_username)
-  AND (usrpref_name='active')
-  AND (usrpref_value='t'));
-
-  IF NOT (FOUND) THEN
-    RAISE EXCEPTION 'Owner is not an active user.';
-  END IF;
-
   RETURN NEW;
 END;
 $$ LANGUAGE 'plpgsql';
