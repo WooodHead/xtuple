c5290f4d0a92a20bca048ece4f6c6b5d1a356018
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-11-06 13:34:05 +0000

    issue #12382:  Add change log entry when someone changes hours.

--- xtupleserver/trunk/dbscripts/triggers/prjtask.sql
+++ xtupleserver/trunk/dbscripts/triggers/prjtask.sql
@@ -1,6 +1,9 @@
 CREATE OR REPLACE FUNCTION _prjtaskTrigger () RETURNS TRIGGER AS $$
 DECLARE
   _test text;
+  _oldActHrs NUMERIC := 0;
+  _oldActExp NUMERIC := 0;
+  _cmnttypeid INTEGER;
 BEGIN
 
   --  Checks
@@ -12,6 +15,26 @@ BEGIN
     RAISE EXCEPTION 'You must ender a valid name.';	
   END IF;
 
+    SELECT cmnttype_id INTO _cmnttypeid
+    FROM cmnttype
+    WHERE (cmnttype_name='ChangeLog');
+    IF (FOUND) THEN
+      IF (TG_OP = 'UPDATE') THEN
+        _oldActHrs := OLD.prjtask_hours_actual;
+        _oldActExp := OLD.prjtask_exp_actual;
+      END IF;
+
+      IF (NEW.prjtask_hours_actual != _oldActHrs) THEN
+        PERFORM postComment(_cmnttypeid, 'TA', NEW.prjtask_id, 
+          'Actual Hours changed from ' || formatQty(_oldActHrs) || ' to ' || formatQty(NEW.prjtask_hours_actual));
+      END IF;
+
+      IF (NEW.prjtask_exp_actual != _oldActExp) THEN
+        PERFORM postComment(_cmnttypeid, 'TA', NEW.prjtask_id, 
+          'Actual Expense changed from ' || formatQty(_oldActExp) || ' to ' || formatQty(NEW.prjtask_exp_actual));
+      END IF;
+    END IF;
+  
   RETURN NEW;
 END;
 $$ LANGUAGE 'plpgsql';
