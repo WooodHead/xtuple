d0f9b8bb18fe88d57c5d75d99c262ce0998202fb
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-12-09 14:14:41 +0000

    Create 360 package for xtdesktop.  Remove obsolete updatescripts folder.  Update translation files.

--- xtdesktop/trunk/updatescripts/create_351alpha/setMetrics.sql
+++
@@ -1,2 +0,0 @@
-select setMetric('desktop/welcome','http://www.xtuple.org/client/welcome');
-select setMetric('desktop/timer','900000');
--- xtdesktop/trunk/updatescripts/create_351beta/setMetrics.sql
+++
@@ -1,2 +0,0 @@
-select setMetric('desktop/welcome','http://welcome.xtuple.org/');
-select setMetric('desktop/timer','900000');
