82f343b00b79c131ac2b7070eeae449bf9eaac08
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2010-12-30 19:06:04 +0000

    issue #13080:  Finish consolidating bookings reports.  Add drill down to regular bookings report.

--- xtupleserver/trunk/updatescripts/from_36x/deleteReports.sql
+++ xtupleserver/trunk/updatescripts/from_36x/deleteReports.sql
@@ -1,3 +1,12 @@
 DELETE FROM report 
-WHERE report_name IN ('BookingsByCustomer','BookingsByCustomerGroup','BookingsByItem','BookingsByProductCategory','BookingsBySalesRep','BookingsByShipTo')
+WHERE report_name IN (
+'BookingsByCustomer',
+'BookingsByCustomerGroup',
+'BookingsByItem',
+'BookingsByProductCategory',
+'BookingsBySalesRep',
+'BookingsByShipTo',
+'TimePhasedBookingsByCustomer',
+'TimePhasedBookingsByProductCategory',
+'TimePhasedBookingsByItem')
 AND report_grade=0;
