971cec2276047f48b7320e09ac67ad81e496f367
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-01-27 16:45:31 +0000

    issue #12351: Performance tweaks for currency functions.

--- xtupleserver/trunk/dbscripts/functions/basecurrid.sql
+++ xtupleserver/trunk/dbscripts/functions/basecurrid.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE FUNCTION baseCurrID ()
-    RETURNS INTEGER AS '
+    RETURNS INTEGER IMMUTABLE AS $$
     DECLARE
 	returnVal INTEGER;
     BEGIN
@@ -7,29 +7,8 @@ CREATE OR REPLACE FUNCTION baseCurrID ()
 	    FROM curr_symbol
 	    WHERE curr_base = TRUE;
     	IF NOT FOUND THEN
-	    RAISE EXCEPTION ''No base currency found'';
+	    RAISE EXCEPTION 'No base currency found';
 	END IF;
 	RETURN returnVal;
     END;
-' LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION currConcat(VARCHAR(3), VARCHAR(9))
-	RETURNS VARCHAR(15) AS '
-    DECLARE
-	curr_abbr   ALIAS FOR $1;
-	curr_symbol ALIAS FOR $2;
-	returnVal   VARCHAR(15) := '''';
-    BEGIN
-	IF length(trim(curr_abbr)) > 0 AND length(trim(curr_symbol)) > 0 THEN
-	    returnVal := trim(curr_abbr) || '' - '' || trim(curr_symbol);
-
-	ELSIF length(trim(curr_abbr)) > 0 THEN
-	    returnVal := curr_abbr;
-
-	ELSIF length(trim(curr_symbol)) > 0 THEN
-	    returnVal := curr_symbol;
-	END IF;
-
-	RETURN returnVal;
-    END;
-' LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
