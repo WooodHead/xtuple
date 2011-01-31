c01560fd4ca7ad0ea6436767e9d72ed1b6f80369
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-01-31 15:47:46 +0000

    Remove tag in the wrong place

--- xtdesktop/R3_7_0ALPHA/resources/database/misc/setMetrics.sql
+++
@@ -1,5 +0,0 @@
-select setMetric('desktop/welcome',
-                 'http://welcome.xtuple.org/index.html?' ||
-                 'ServerVersion=' || fetchmetrictext('ServerVersion') || '&' ||
-                 'Application=' || fetchmetrictext('Application') );
-select setMetric('desktop/timer','900000');
