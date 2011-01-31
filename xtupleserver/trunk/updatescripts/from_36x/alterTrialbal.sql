164574cc815cb51900f96792e8aa9ef407ba3f63
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-01-31 16:32:34 +0000

    Removed file that should not have been part of release.

--- xtupleserver/trunk/updatescripts/from_36x/alterTrialbal.sql
+++
@@ -1,2 +0,0 @@
-alter table trialbal add column trialbal_adj_ending numeric(20,02) not null default 0;
-COMMENT ON COLUMN trialbal.trialbal_adj_ending IS 'Contains the adjusted value of an imported trial balance based on end of period exchange rate.';
