efb9163bb76196015e9ccd4e23e4b7f4820a24b6
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-02-02 18:35:38 +0000

    issue #8889:  Don't allow years to be closed if periods are not closed.  Don't allow opening and closing out of order.

--- xtupleserver/trunk/dbscripts/functions/openaccountingyearperiod.sql
+++ xtupleserver/trunk/dbscripts/functions/openaccountingyearperiod.sql
@@ -1,7 +1,7 @@
 
-CREATE OR REPLACE FUNCTION openAccountingYearPeriod(INTEGER) RETURNS INTEGER AS '
+CREATE OR REPLACE FUNCTION openAccountingYearPeriod(INTEGER) RETURNS INTEGER AS $$
 DECLARE
-  pPeriodid ALIAS FOR $1;
+  pYearPeriodId ALIAS FOR $1;
   _r RECORD;
 
 BEGIN
@@ -9,17 +9,28 @@ BEGIN
 --  Check to make use that the yearperiod is closed
   IF ( ( SELECT (NOT yearperiod_closed)
          FROM yearperiod
-         WHERE (yearperiod_id=pPeriodid) ) ) THEN
+         WHERE (yearperiod_id=pYearPeriodId) ) ) THEN
     RETURN -1;
+  END IF;
+
+  IF ( ( SELECT (count(yearperiod_id) > 0)
+           FROM yearperiod
+          WHERE ((yearperiod_end> (
+            SELECT yearperiod_end 
+            FROM yearperiod 
+            WHERE (yearperiod_id=pYearPeriodId))
+          )
+           AND (yearperiod_closed)) ) ) THEN
+    RETURN -2;
   END IF;
 
 --  Reset the yearperiod_closed flag
   UPDATE yearperiod
   SET yearperiod_closed=FALSE
-  WHERE (yearperiod_id=pPeriodid);
+  WHERE (yearperiod_id=pYearPeriodId);
 
   RETURN pPeriodid;
 
 END;
-' LANGUAGE 'plpgsql';
+$$ LANGUAGE 'plpgsql';
 
