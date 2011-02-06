7626b066f785a0948d9559d69146e29811ba9841
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-02-06 15:53:10 +0000

    issue #13704: Consolidate customer export functionality into customer list.

--- xtupleserver/trunk/updatescripts/from_370alpha/deleteMetasql2.sql
+++ xtupleserver/trunk/updatescripts/from_370alpha/deleteMetasql2.sql
@@ -1,1 +1,2 @@
 delete from metasql where metasql_group='workOrderSoStatus' and metasql_name='detail' and metasql_grade=0
+delete from metasql where metasql_group='customerInformationExport' and metasql_name='detail' and metasql_grade=0
