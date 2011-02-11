719b66afa7bb1150aac1c9339fe8d2b360f80219
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-02-11 16:55:31 +0000

    Issue #13788:change to loop

--- xtupleserver/trunk/dbscripts/functions/postinvoices.sql
+++ xtupleserver/trunk/dbscripts/functions/postinvoices.sql
@@ -11,6 +11,7 @@ CREATE OR REPLACE FUNCTION postInvoices(BOOLEAN, BOOLEAN) RETURNS INTEGER AS $$
 DECLARE
   pPostUnprinted ALIAS FOR $1;
   pInclZeros     ALIAS FOR $2;
+  _invcheadid    INTEGER;
   _journalNumber INTEGER;
   _itemlocSeries INTEGER;
   _counter INTEGER;
@@ -23,24 +24,41 @@ BEGIN
   _itemlocSeries := 0;
 
   IF (pInclZeros) THEN
-    SELECT postInvoice(invchead_id, _journalNumber, _itemlocSeries) INTO _itemlocSeries
-    FROM invchead
-    WHERE ( (NOT invchead_posted)
-     AND (checkInvoiceSitePrivs(invchead_id))
-     AND (pPostUnprinted OR invchead_printed) );
+
+    FOR _invcheadid IN
+      SELECT invchead_id
+      FROM invchead
+      WHERE ( (NOT invchead_posted)
+       AND (checkInvoiceSitePrivs(invchead_id))
+       AND (pPostUnprinted OR invchead_printed) ) LOOP
+
+      SELECT postInvoice(_invcheadid, _journalNumber, _itemlocSeries) INTO _itemlocSeries;
+      IF (_itemlocSeries < 0) THEN
+        RETURN _itemlocSeries;
+      END IF;
+    END LOOP;
 
   ELSE
-    SELECT postInvoice(invchead_id, _journalNumber, _itemlocSeries) INTO _itemlocSeries
-       FROM invchead LEFT OUTER JOIN invcitem ON (invchead_id=invcitem_invchead_id)
-                     LEFT OUTER JOIN item ON (invcitem_item_id=item_id)  
+
+    FOR _invcheadid IN
+      SELECT invchead_id
+      FROM invchead LEFT OUTER JOIN invcitem ON (invchead_id=invcitem_invchead_id)
+                    LEFT OUTER JOIN item ON (invcitem_item_id=item_id)  
       WHERE((NOT invchead_posted)
         AND (checkInvoiceSitePrivs(invchead_id))
         AND (pPostUnprinted OR invchead_printed))
       GROUP BY invchead_id, invchead_freight, invchead_misc_amount
-     HAVING (COALESCE(SUM(round((invcitem_billed * invcitem_qty_invuomratio) * (invcitem_price /  
+      HAVING (COALESCE(SUM(round((invcitem_billed * invcitem_qty_invuomratio) * (invcitem_price /  
               CASE WHEN (item_id IS NULL) THEN 1 
               ELSE invcitem_price_invuomratio END), 2)),0)
-             + invchead_freight + invchead_misc_amount) > 0;
+             + invchead_freight + invchead_misc_amount) > 0 LOOP
+
+      SELECT postInvoice(_invcheadid, _journalNumber, _itemlocSeries) INTO _itemlocSeries;
+      IF (_itemlocSeries < 0) THEN
+        RETURN _itemlocSeries;
+      END IF;
+    END LOOP;
+
   END IF;
 
   RETURN _itemlocSeries;
