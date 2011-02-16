5e5364685572732c75069bcb49b2482ba77e8b4a
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-02-16 02:32:14 +0000

    issue #13838:  Add order option to characteristics.

--- xtupleserver/trunk/updatescripts/from_370alpha/createCharopt.sql
+++ xtupleserver/trunk/updatescripts/from_370alpha/createCharopt.sql
@@ -1,7 +1,8 @@
 create table charopt	 (
   charopt_id serial,
   charopt_char_id integer references char (char_id) on delete cascade,
-  charopt_value text not null
+  charopt_value text not null,
+  charopt_order integer not null default (0)
 );
 
 grant all on charopt to xtrole;
@@ -11,5 +12,6 @@ COMMENT ON TABLE charopt IS 'Stores list options for characteristics';
 COMMENT ON COLUMN charopt.charopt_id IS 'Primary key';
 COMMENT ON COLUMN charopt.charopt_char_id IS 'Reference to char table';
 COMMENT ON COLUMN charopt.charopt_value IS 'Option value';
+COMMENT ON COLUMN charopt.charopt_order IS 'Option sort order';
 
 
