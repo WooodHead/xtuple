0b0dbc3382a5ca3db3e5054d040a0f6b416fa153
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-02-24 16:44:03 +0000

    update copyright notices

--- xttelite/trunk/resources/client/scripts/configureCRM.js
+++ xttelite/trunk/resources/client/scripts/configureCRM.js
@@ -1,6 +1,6 @@
 /*
   This file is part of the xtprjaccnt Package for xTuple ERP,
-  and is Copyright (c) 1999-2010 by OpenMFG LLC, d/b/a xTuple.  It
+  and is Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple.  It
   is licensed to you under the xTuple End-User License Agreement ("the
   EULA"), the full text of which is available at www.xtuple.com/EULA.
   While the EULA gives you access to source code and encourages your
