7dc2ee00c8b6e72521d4533c4482d7056b6e0d55
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-03-10 17:56:42 +0000

    issue #13798:  Add status to sales order header for improved performance.

--- xtupleserver/trunk/dbscripts/functions/getsostatus.sql
+++ xtupleserver/trunk/dbscripts/functions/getsostatus.sql
@@ -1,22 +1,15 @@
 CREATE OR REPLACE FUNCTION getSoStatus(INTEGER) RETURNS CHAR(1) AS $$
 DECLARE
   pCoheadid ALIAS FOR $1;
-  _r RECORD;
+  _result char(1);
 
 BEGIN
 
-  SELECT coitem_status,
-  CASE 
-    WHEN (coitem_status = 'O') THEN 1
-    WHEN (coitem_status = 'C') then 2
-    ELSE  3
-  END AS seq INTO _r
-  FROM coitem
-  WHERE (coitem_cohead_id=pCoheadid)
-  ORDER BY seq
-  LIMIT 1;
+  SELECT cohead_status INTO _result
+  FROM cohead
+  WHERE (cohead_id=pCoheadid);
 
-  RETURN COALESCE(_r.coitem_status,'O');
+  RETURN _result;
 
 END;
 $$ LANGUAGE 'plpgsql' STABLE;
