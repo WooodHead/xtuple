72efd0633d314f27daad8b7716cf943ea011e96c
omfgppc <omfgppc@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-04-26 19:41:06 +0000

    Issue #14376 Create index on todoitem_username

--- xtupleserver/trunk/updatescripts/from_37x/createIndexes.sql
+++ xtupleserver/trunk/updatescripts/from_37x/createIndexes.sql
@@ -1,2 +1,3 @@
 create index pohead_pohead_status_idx ON pohead (pohead_status);
 create index cohead_cohead_status_idx ON cohead (cohead_status);
+create index todoitem_todoitem_username_idx ON todoitem (todoitem_username);
