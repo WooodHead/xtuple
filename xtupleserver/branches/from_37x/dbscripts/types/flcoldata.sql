13341e45fc9a4ddb780d2cc6856befed2ef96e17
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-05-20 14:45:30 +0000

    Issue #14765. Fixed problem with command that was duplicated.

--- xtupleserver/branches/from_37x/dbscripts/types/flcoldata.sql
+++ xtupleserver/branches/from_37x/dbscripts/types/flcoldata.sql
@@ -5,10 +5,3 @@ CREATE TYPE flcoldata AS (
   flcoldata_start   	DATE,
   flcoldata_end        DATE
 );
-select dropifexists('FUNCTION', 'getflcoldata(int,int)');
-select dropifexists('TYPE', 'flcoldata');
-CREATE TYPE flcoldata AS (
-  flcoldata_column  	INTEGER,
-  flcoldata_start   	DATE,
-  flcoldata_end        DATE
-);
