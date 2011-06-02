93496fb86d58cd99478b7c3332cdb4af4e485677
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-06-02 15:00:28 +0000

    Issue #14921:correct parent first and then use returnWoMaterial for backflush

--- xtupleserver/trunk/dbscripts/functions/correctproduction.sql
+++ xtupleserver/trunk/dbscripts/functions/correctproduction.sql
@@ -39,7 +39,7 @@ DECLARE
   _parentWIPAccntid INTEGER;
   _parentQty        NUMERIC;
   _qty              NUMERIC;
-  _invqty           NUMERIC;
+  _wipPost          NUMERIC;
   _sense            TEXT;
   _status           TEXT;
   _type             TEXT;
@@ -47,6 +47,8 @@ DECLARE
 
 BEGIN
 
+  -- Qty is positive for Assembly W/O
+  -- Qty is negative for Disassembly W/O
   IF (pQty = 0) THEN
     RETURN pItemlocseries;
   ELSIF (pQty > 0) THEN
@@ -57,10 +59,9 @@ BEGIN
 
   SELECT item_type, roundQty(item_fractional, pQty), wo_status
     INTO _type, _parentQty, _status
-    FROM wo, itemsite, item
-   WHERE ((wo_itemsite_id=itemsite_id)
-      AND (itemsite_item_id=item_id)
-      AND (wo_id=pWoid));
+    FROM wo JOIN itemsite ON (itemsite_id=wo_itemsite_id)
+            JOIN item ON (item_id=itemsite_item_id)
+   WHERE (wo_id=pWoid);
   
   IF (_status != 'I') THEN
     RETURN -1;
@@ -76,13 +77,37 @@ BEGIN
     _itemlocSeries := pItemlocSeries;
   END IF;
 
-  --  Cache the parent WIP account (No SELECT ... INTO x, ... INTO x;!)
-  SELECT costcat_wip_accnt_id INTO _parentWIPAccntid
-  FROM wo, itemsite, costcat
+  --  Calculate the WIP to correct 
+  SELECT CASE WHEN (wo_cosmethod = 'D') THEN wo_postedvalue
+              ELSE  round(((wo_postedvalue - wo_wipvalue) / wo_qtyrcv * _parentQty), 2)
+         END INTO _wipPost
+  FROM wo
+  WHERE (wo_id=pWoid);
+
+  --  Post the inventory transaction
+  SELECT postInvTrans( itemsite_id, 'RM', (_parentQty * -1.0),
+                       'W/O', 'WO', formatwonumber(pWoid), '',
+                       ('Correct Receive Inventory ' || item_number || ' ' || _sense || ' Manufacturing'),
+                       costcat_asset_accnt_id, getPrjAccntId(wo_prj_id, costcat_wip_accnt_id), _itemlocSeries, pGlDistTS,
+                       (_wipPost * -1.0), -- only used when cost is average
+                       pInvhistId) INTO _invhistid
+  FROM wo JOIN itemsite ON (itemsite_id=wo_itemsite_id)
+          JOIN item ON (item_id=itemsite_item_id)
+          JOIN costcat ON (costcat_id=itemsite_costcat_id)
+  WHERE (wo_id=pWoid);
+
+  --  Decrease this W/O's qty. received and increase its WIP value
+  UPDATE wo
+  SET wo_qtyrcv = (wo_qtyrcv - _parentQty),
+      wo_wipvalue = (wo_wipvalue + (CASE WHEN(itemsite_costmethod IN ('A','J'))
+                                              THEN _wipPost
+                                         WHEN(itemsite_costmethod='S')
+                                              THEN stdcost(itemsite_item_id) * _parentQty
+                                         ELSE 0.0 END))
+  FROM itemsite
   WHERE ( (wo_itemsite_id=itemsite_id)
-   AND (itemsite_costcat_id=costcat_id)
    AND (wo_id=pWoid) );
- 
+
   IF (pBackflush) THEN
     FOR _r IN SELECT item_id, item_fractional,
                       itemsite_id, itemsite_warehous_id,
@@ -91,59 +116,24 @@ BEGIN
                       wo_qtyrcv, wo_prj_id,
                       womatl_id, womatl_qtyfxd, womatl_qtyper,
                       womatl_scrap, womatl_issuemethod, womatl_uom_id
-               FROM womatl, wo, itemsite, item
-               WHERE ( (womatl_wo_id=wo_id)
-                AND (womatl_itemsite_id=itemsite_id)
-                AND (itemsite_item_id=item_id)
-                AND (womatl_issuemethod = 'L')
-                AND (wo_id=pWoid) ) LOOP
+               FROM wo JOIN womatl ON (womatl_wo_id=wo_id AND womatl_issuemethod='L')
+                       JOIN itemsite ON (itemsite_id=womatl_itemsite_id)
+                       JOIN item ON (item_id=itemsite_item_id)
+               WHERE (wo_id=pWoid) LOOP
 
       --  Cache the qty to be issued
       -- If going back to beginning, unissue fixed qty as well
-      IF (_r.wo_qtyrcv-pQty > 0) THEN
+      IF (_r.wo_qtyrcv - _parentQty > 0) THEN
         _qtyfxd := 0;
       ELSE
         _qtyfxd := _r.womatl_qtyfxd;
       END IF;
       
       _qty = roundQty(_r.item_fractional, (_qtyfxd + _parentQty * _r.womatl_qtyper) * (1 + _r.womatl_scrap));
-      _invqty = itemuomtouom(_r.item_id, _r.womatl_uom_id, NULL, (_qtyfxd + _parentQty * _r.womatl_qtyper) * (1 + _r.womatl_scrap));
 
-      IF (_itemlocSeries = 0) THEN
-        SELECT NEXTVAL('itemloc_series_seq') INTO _itemlocSeries;
+      IF (_qty > 0) THEN
+        SELECT returnWoMaterial(_r.womatl_id, _qty, _itemlocSeries, pGlDistTS) INTO _itemlocSeries;
       END IF;
-
-      IF (_invqty > 0) THEN
-        SELECT postInvTrans( itemsite_id, 'IM', (_invqty * -1),
-                             'W/O', 'WO', formatwonumber(pWoid), '',
-                             ('Correct Receive Inventory ' || item_number || ' ' || _sense || ' Manufacturing'),
-                             getPrjAccntId(_r.wo_prj_id, _parentWIPAccntid), costcat_asset_accnt_id, _itemlocSeries, pGlDistTS ) INTO _invhistid
-        FROM itemsite, item, costcat
-        WHERE ( (itemsite_item_id=item_id)
-         AND (itemsite_costcat_id=costcat_id)
-         AND (itemsite_id=_r.itemsite_id) );
-      END IF;
-
-      --  Decrease the parent W/O's WIP value by the value of the returned components
-      UPDATE wo
-      SET wo_wipvalue = (wo_wipvalue -
-                         (CASE WHEN(_r.itemsite_costmethod IN ('A','J'))
-                                      THEN avgcost(_r.itemsite_id)
-                               WHEN(_r.itemsite_costmethod='S')
-                                      THEN stdcost(_r.item_id)
-                               ELSE 0.0 END * _invqty)),
-          wo_postedvalue = (wo_postedvalue -
-                            (CASE WHEN(_r.itemsite_costmethod IN ('A','J'))
-                                         THEN avgcost(_r.itemsite_id)
-                                  WHEN(_r.itemsite_costmethod='S')
-                                         THEN stdcost(_r.item_id)
-                                  ELSE 0.0 END * _invqty))
-      WHERE (wo_id=pWoid);
-
-      UPDATE womatl
-      SET womatl_qtyiss = (womatl_qtyiss - _qty),
-          womatl_lastreturn = CURRENT_DATE
-      WHERE (womatl_id=_r.womatl_id);
 
     END LOOP;
 
@@ -181,34 +171,6 @@ BEGIN
 	--End ROB
 
   END IF;
-
-  --  Post the inventory transaction
-  SELECT postInvTrans( itemsite_id, 'RM', (_parentQty * -1),
-                       'W/O', 'WO', formatwonumber(pWoid), '',
-                       ('Correct Receive Inventory ' || item_number || ' ' || _sense || ' Manufacturing'),
-                       costcat_asset_accnt_id, getPrjAccntId(wo_prj_id, costcat_wip_accnt_id), _itemlocSeries, pGlDistTS,
-                       CASE WHEN (wo_qtyrcv > 0) THEN
-                       NONEG(((wo_postedvalue - wo_wipvalue) / wo_qtyrcv) * _parentQty) -- only used when cost is average
-                            ELSE 0 END
-                       , pInvhistId) INTO _invhistid
-  FROM wo, itemsite, item, costcat
-  WHERE ( (wo_itemsite_id=itemsite_id)
-   AND (itemsite_item_id=item_id)
-   AND (itemsite_costcat_id=costcat_id)
-   AND (wo_id=pWoid) );
-
-  --  Decrease this W/O's qty. received and increase its WIP value
-  UPDATE wo
-  SET wo_qtyrcv = (wo_qtyrcv - _parentQty),
-      wo_wipvalue = (wo_wipvalue + (CASE WHEN(itemsite_costmethod IN ('A','J'))
-                                              THEN ((wo_postedvalue - wo_wipvalue) / wo_qtyrcv)
-                                         WHEN(itemsite_costmethod='S')
-                                              THEN stdcost(itemsite_item_id)
-                                         ELSE 0.0 END * _parentQty))
-  FROM itemsite, item
-  WHERE ( (wo_itemsite_id=itemsite_id)
-   AND (itemsite_item_id=item_id)
-   AND (wo_id=pWoid) );
 
   RETURN _itemlocSeries;
 
