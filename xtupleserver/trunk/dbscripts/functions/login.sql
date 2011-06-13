6f4a0c26987e3e523070a8b8b1c5ef1b8571a4a0
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-06-13 17:55:06 +0000

    Issue #14999. When the login function is called by the client check the version and if it's 9 or higher set the bytea_output setting to escape to make sure bytea output is in the correct format for encryption and other in general.

--- xtupleserver/trunk/dbscripts/functions/login.sql
+++ xtupleserver/trunk/dbscripts/functions/login.sql
@@ -9,6 +9,11 @@ BEGIN
      FROM pg_stat_activity
     WHERE(procpid = pg_backend_pid());
 
+  -- This is new to version 9.0 and higher and will error on older versions
+  IF (select CAST(split_part(split_part(version(), ' ', 2),'.',1) AS integer) >= 9) THEN
+    SET bytea_output TO escape;
+  END IF;
+
   SELECT usr_id, userCanLogin(usr_username) AS usr_active INTO _p
   FROM usr
   WHERE (usr_username=CURRENT_USER);
