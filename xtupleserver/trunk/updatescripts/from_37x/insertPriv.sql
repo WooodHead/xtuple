07ce5ccb8525605079fef9c928875ddbb9b2949a
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-06-28 18:54:32 +0000

    Issue #15076. Fixed typo in priv script.

--- xtupleserver/trunk/updatescripts/from_37x/insertPriv.sql
+++ xtupleserver/trunk/updatescripts/from_37x/insertPriv.sql
@@ -1,2 +1,2 @@
-INSERT INTO priv (priv_module, priv_name, priv_descrip) VALUES ('System','MaintaintTranslations','User is allowed to maintain/download translations to their local computer.');
+INSERT INTO priv (priv_module, priv_name, priv_descrip) VALUES ('System','MaintainTranslations','User is allowed to maintain/download translations to their local computer.');
 
