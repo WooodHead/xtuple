78b2a1e2449841507f62fee57fe5d61f8dc7417c
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-07-12 18:52:54 +0000

    Issue #15076. New priv for dictionary download.

--- xtupleserver/trunk/updatescripts/from_37x/insertPriv.sql
+++ xtupleserver/trunk/updatescripts/from_37x/insertPriv.sql
@@ -1,2 +1,3 @@
 INSERT INTO priv (priv_module, priv_name, priv_descrip) VALUES ('System','MaintainTranslations','User is allowed to maintain/download translations to their local computer.');
+INSERT INTO priv (priv_module, priv_name, priv_descrip) VALUES ('System','MaintainDictionaries','User is allowed to maintain/download spell check dictionaries to their local computer.');
 
