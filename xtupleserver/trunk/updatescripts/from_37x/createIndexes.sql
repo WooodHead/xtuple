cbd76a8cd6adc929c83ae4704243ccb00c659beb
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-07-13 19:09:04 +0000

    Issue #15230:prevent accnt duplicates

--- xtupleserver/trunk/updatescripts/from_37x/createIndexes.sql
+++ xtupleserver/trunk/updatescripts/from_37x/createIndexes.sql
@@ -1,3 +1,4 @@
 create index pohead_pohead_status_idx ON pohead (pohead_status);
 create index cohead_cohead_status_idx ON cohead (cohead_status);
 create index todoitem_todoitem_username_idx ON todoitem (todoitem_username);
+create unique index accnt_unique_idx ON accnt (accnt_number, accnt_profit, accnt_sub, accnt_company);
