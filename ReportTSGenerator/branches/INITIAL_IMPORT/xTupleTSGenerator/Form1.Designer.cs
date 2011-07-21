90138cc5ec9fc9bcfe11ec48b6e71a68444f3d6a
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-07-21 15:39:54 +0000

    Releasing new tool by Groupe Concerti for generating report .ts files, under CPAL license.

---
+++ ReportTSGenerator/branches/INITIAL_IMPORT/xTupleTSGenerator/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace xTupleTSGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtTSFilename = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDBName = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(132, 230);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(194, 23);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate TS file";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtTSFilename
+            // 
+            this.txtTSFilename.Location = new System.Drawing.Point(132, 204);
+            this.txtTSFilename.Name = "txtTSFilename";
+            this.txtTSFilename.Size = new System.Drawing.Size(339, 20);
+            this.txtTSFilename.TabIndex = 1;
+            this.txtTSFilename.Text = "c:\\tmp\\reports.ts";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.txtUser);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtDBName);
+            this.groupBox1.Controls.Add(this.txtPort);
+            this.groupBox1.Controls.Add(this.txtServer);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(499, 173);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PostgreSQL connection parameters";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(56, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Password:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(56, 113);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Username:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(120, 136);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(127, 20);
+            this.txtPassword.TabIndex = 7;
+            this.txtPassword.Text = "Dsic0nseil";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(120, 110);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(127, 20);
+            this.txtUser.TabIndex = 6;
+            this.txtUser.Text = "postgres";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Database:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Server:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtDBName
+            // 
+            this.txtDBName.Location = new System.Drawing.Point(120, 84);
+            this.txtDBName.Name = "txtDBName";
+            this.txtDBName.Size = new System.Drawing.Size(127, 20);
+            this.txtDBName.TabIndex = 2;
+            this.txtDBName.Text = "demo";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(120, 58);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(71, 20);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "5432";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(120, 32);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(127, 20);
+            this.txtServer.TabIndex = 0;
+            this.txtServer.Text = "localhost";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Generated TS file:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 264);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtTSFilename);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
+            this.Text = "TS file generation tool";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtTSFilename;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtDBName;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUser;
+    }
+}
+
---
+++ ReportTSGenerator/branches/INITIAL_IMPORT/xTupleTSGenerator/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Npgsql;
+using System.Xml.Linq;
+
+namespace xTupleTSGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection(String.Format("Server={0};Port={1};User Id={2};Password={3};Database={4};",txtServer.Text,txtPort.Text,txtUser.Text,txtPassword.Text,txtDBName.Text));
+            conn.Open();
+
+            NpgsqlCommand command = new NpgsqlCommand("select * from report", conn);
+           
+            try
+            {
+                NpgsqlDataReader dr = command.ExecuteReader();
+
+                XDocument xmlTS = new XDocument();
+             
+                // Données de base
+                xmlTS.Add(new XDocumentType("TS",null,null,null));
+
+                XElement ts = new XElement("TS",new XAttribute("version","2.0"));
+                xmlTS.Add(ts);
+
+
+                while (dr.Read())
+                {
+                    XDocument xmlReport = XDocument.Parse(dr["report_source"].ToString());
+                         
+
+                    XElement context = new XElement("context", new XElement("name",xmlReport.Element("report").Element("name").Value));
+                    ts.Add(context);
+
+                    
+                    foreach (XElement label in from i in xmlReport.Element("report").Descendants("label") select i)
+                    {
+                        XElement message = new XElement("message",
+                                    new XElement("source", label.Element("string").Value),
+                                    new XElement("translation",
+                                        new XAttribute("type", "unfinished"), "")
+                                );
+
+                      //  if (chkWidth.Checked) message.AddFirst(new XElement("width", label.Element("rect").Element("width").Value));
+
+                        context.Add(message);
+
+                    }
+                    
+                }
+                
+                xmlTS.Save(txtTSFilename.Text);
+                MessageBox.Show("Conversion terminée");
+            }
+
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+
+    }
+}
