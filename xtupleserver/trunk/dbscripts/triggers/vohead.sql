df840a4cd878733da484029ec81ee8d718da438b
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-08-08 20:47:47 +0000

    Issue #7997:add recurrence to misc. vouchers

--- xtupleserver/trunk/dbscripts/triggers/vohead.sql
+++ xtupleserver/trunk/dbscripts/triggers/vohead.sql
@@ -1,10 +1,35 @@
 CREATE OR REPLACE FUNCTION _voheadBeforeTrigger() RETURNS "trigger" AS $$
 DECLARE
+  _recurid     INTEGER;
+  _newparentid INTEGER;
 
 BEGIN
   IF (TG_OP = 'DELETE') THEN
     DELETE FROM voheadtax
     WHERE (taxhist_parent_id=OLD.vohead_id);
+
+    SELECT recur_id INTO _recurid
+      FROM recur
+     WHERE ((recur_parent_id=OLD.vohead_id)
+        AND (recur_parent_type='V'));
+    IF (_recurid IS NOT NULL) THEN
+      SELECT vohead_id INTO _newparentid
+        FROM vohead
+       WHERE ((vohead_recurring_vohead_id=OLD.vohead_id)
+          AND (vohead_id!=OLD.vohead_id))
+       ORDER BY vohead_docdate
+       LIMIT 1;
+
+      IF (_newparentid IS NULL) THEN
+        DELETE FROM recur WHERE recur_id=_recurid;
+      ELSE
+        UPDATE recur SET recur_parent_id=_newparentid
+         WHERE recur_id=_recurid;
+        UPDATE vohead SET vohead_recurring_vohead_id=_newparentid
+         WHERE vohead_recurring_vohead_id=OLD.vohead_id
+           AND vohead_id!=OLD.vohead_id;
+      END IF;
+    END IF;
 
     RETURN OLD;
   END IF;
