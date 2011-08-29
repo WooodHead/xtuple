27f96c31b6e3b04a0959d795e94a5a37b216ae8c
cryan <cryan@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-08-29 16:24:12 +0000

    Issue #15520. Updated functions to actually support setting/getting the effective user using temp tables.

--- xtupleserver/trunk/dbscripts/functions/seteffectivextuser.sql
+++ xtupleserver/trunk/dbscripts/functions/seteffectivextuser.sql
@@ -2,7 +2,21 @@ CREATE OR REPLACE FUNCTION setEffectiveXtUser(TEXT) RETURNS BOOL AS $$
 DECLARE
   pUsername ALIAS FOR $1;
 BEGIN
-  -- this currently does nothing
-  RETURN false;
+  PERFORM initEffectiveXtUser();
+
+  PERFORM *
+     FROM effective_user
+    WHERE effective_key = 'username';
+
+  IF FOUND THEN
+    UPDATE effective_user
+       SET effective_value = pUsername
+     WHERE effective_key = 'username';
+  ELSE
+    INSERT INTO effective_user (effective_key, effective_value)
+         VALUES('username', pUsername);
+  END IF;
+
+  RETURN true;
 END;
 $$ LANGUAGE 'plpgsql';
