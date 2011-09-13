6b66af881167244f13aa1f265085a625f2a17834
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-09-13 15:12:32 +0000

    disable the optional pre380 package, since part of that package will interfere with proper functioning of some crm account features in 3.8.0 xtuple erp

---
+++ xtupleserver/trunk/updatescripts/from_380alpha/disablePre380package.sql
@@ -0,0 +1,7 @@
+/* the pre380 package works around bugs in the crmaccounts-details metasql
+   statement by defining one of a higher grade. however, the version in
+   the package can't know about some of the new columns in the crmacct
+   table. therefore we need to disable or delete that package for 3.8.0's
+   crmaccounts and crmaccountMergePickAccounts windows to work correctly.
+*/
+SELECT disablePackage('pre380');
