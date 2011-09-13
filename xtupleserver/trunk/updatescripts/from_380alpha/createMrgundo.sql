8a8569fbf1a342fcfb8f1bd8f7e59dfb6a52e630
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-09-13 15:13:08 +0000

    issue #15320 - fix typo

--- xtupleserver/trunk/updatescripts/from_380alpha/createMrgundo.sql
+++ xtupleserver/trunk/updatescripts/from_380alpha/createMrgundo.sql
@@ -24,7 +24,7 @@ GRANT ALL ON TABLE mrgundo TO xtrole;
 COMMENT ON TABLE mrgundo IS 'This table keeps track of the original values of changes made while merging two records. It is a generalization of mrghist and trgthist, which are specific to merging contacts. The schema, table, and pkey_id columns uniquely identify the record that was changed while the _base_ columns identify the merge target. The _base_ columns are required to allow finding all of the records that pertain to a particular merge (e.g. find changes to the comment table that pertain to a crmacct merge).';
 COMMENT ON COLUMN mrgundo.mrgundo_base_schema IS 'The schema in which the merge target resides.';
 COMMENT ON COLUMN mrgundo.mrgundo_base_table IS 'The table in which the merge target resides.';
-COMMENT ON COLUMN mrgundo.mrgundo_base_id 'The internal id of the merge target record.';
+COMMENT ON COLUMN mrgundo.mrgundo_base_id IS 'The internal id of the merge target record.';
 COMMENT ON COLUMN mrgundo.mrgundo_schema IS 'The name of the schema in which the modified table resides.';
 COMMENT ON COLUMN mrgundo.mrgundo_table IS 'The name of the table that was modified during a merge.';
 COMMENT ON COLUMN mrgundo.mrgundo_pkey_col IS 'The name of the primary key column in the modified table. This could be derived during the undo processing but it is simpler just to store it during the merge.';
