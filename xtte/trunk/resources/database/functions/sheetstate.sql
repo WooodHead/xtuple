ae2277058e4b4a23a41bfc25581c45eb49a91929
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-11-04 19:25:28 +0000

    add property to normalize end-of-line, add copyright notices to db functions and metasql statements

--- xtte/trunk/resources/database/functions/sheetstate.sql
+++ xtte/trunk/resources/database/functions/sheetstate.sql
@@ -1,4 +1,6 @@
 CREATE OR REPLACE FUNCTION te.sheetstate(INTEGER, CHAR(1)) RETURNS INTEGER AS $$
+-- Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple. 
+-- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
   pTeheadId ALIAS FOR $1;
   pType     ALIAS FOR $2;
--- xtte/trunk/resources/database/triggers/tehead.sql
+++ xtte/trunk/resources/database/triggers/tehead.sql
@@ -1,4 +1,6 @@
 CREATE OR REPLACE FUNCTION te.triggertehead() RETURNS "trigger" AS $$
+-- Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple. 
+-- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
 _r RECORD;
 _sense INTEGER := 0;
