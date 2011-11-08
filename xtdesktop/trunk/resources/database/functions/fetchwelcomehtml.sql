e1b6c7be42091a0ecabd65abe61f40186b1e20be
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-11-08 13:26:31 +0000

    add property to normalize end-of-line, add copyright notices to functions and metasql

--- xtdesktop/trunk/resources/database/functions/fetchwelcomehtml.sql
+++ xtdesktop/trunk/resources/database/functions/fetchwelcomehtml.sql
@@ -1,5 +1,7 @@
 
 CREATE OR REPLACE FUNCTION fetchWelcomeHtml() RETURNS TEXT IMMUTABLE AS $$
+-- Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple.
+-- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
   _html TEXT;
 
