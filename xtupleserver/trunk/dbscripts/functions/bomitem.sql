700a2ea0611a8d31cc06e58338918d9f30e48cea
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-11-17 19:25:30 +0000

    issue #15973: Set some functions as stable and refactor code a bit to speed queries up.

--- xtupleserver/trunk/dbscripts/functions/bomitem.sql
+++ xtupleserver/trunk/dbscripts/functions/bomitem.sql
@@ -1,42 +1,11 @@
-CREATE OR REPLACE FUNCTION bomitem(INTEGER) RETURNS SETOF bomitem AS '
+CREATE OR REPLACE FUNCTION bomitem(INTEGER) RETURNS SETOF bomitem AS $$
 -- Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
-DECLARE
-  pItemid ALIAS FOR $1;
-  _row bomitem%ROWTYPE;
-  _revid INTEGER;
+  SELECT * FROM bomitem WHERE ((bomitem_parent_item_id=$1) AND (bomitem_rev_id=getActiveRevId('BOM',$1)));
+$$ LANGUAGE 'sql';
 
-BEGIN
-
-  SELECT getActiveRevId(''BOM'',pItemid) INTO _revid;
-  
-  FOR _row IN SELECT *
-            FROM bomitem(pItemid,_revid)
-  LOOP
-    RETURN NEXT _row;
-  END LOOP;
-
-  RETURN;
-END;
-' LANGUAGE 'plpgsql';
-
-CREATE OR REPLACE FUNCTION bomitem(INTEGER,INTEGER) RETURNS SETOF bomitem AS '
+CREATE OR REPLACE FUNCTION bomitem(INTEGER,INTEGER) RETURNS SETOF bomitem AS $$
 -- Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
-DECLARE
-  pItemid ALIAS FOR $1;
-  pRevid ALIAS FOR $2;
-  _row bomitem%ROWTYPE;
-
-BEGIN
-  FOR _row IN SELECT *
-            FROM bomitem
-            WHERE ((bomitem_parent_item_id=pItemid)
-            AND (bomitem_rev_id=pRevid))
-  LOOP
-    RETURN NEXT _row;
-  END LOOP;
-
-  RETURN;
-END;
-' LANGUAGE 'plpgsql';
+  SELECT * FROM bomitem WHERE ((bomitem_parent_item_id=$1) AND (bomitem_rev_id=$2));
+$$ LANGUAGE 'sql';
--- xtupleserver/trunk/dbscripts/functions/getactiverevid.sql
+++ xtupleserver/trunk/dbscripts/functions/getactiverevid.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION getActiveRevId(TEXT,INTEGER) RETURNS INTEGER AS $$
+CREATE OR REPLACE FUNCTION getActiveRevId(TEXT,INTEGER) RETURNS INTEGER STABLE AS $$
 -- Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
