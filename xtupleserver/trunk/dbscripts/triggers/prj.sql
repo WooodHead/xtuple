880afb724b98300674a51fd06dcf4b65371e3234
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2011-12-29 16:31:17 +0000

    Issue #16613:post date changes to comment log

--- xtupleserver/trunk/dbscripts/triggers/prj.sql
+++ xtupleserver/trunk/dbscripts/triggers/prj.sql
@@ -41,3 +41,52 @@ CREATE TRIGGER prjbeforedeletetrigger
   ON prj
   FOR EACH ROW
   EXECUTE PROCEDURE _prjBeforeDeleteTrigger();
+
+CREATE OR REPLACE FUNCTION _prjAfterTrigger() RETURNS TRIGGER AS $$
+-- Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple. 
+-- See www.xtuple.com/CPAL for the full text of the software license.
+DECLARE
+  _cmnttypeid INTEGER;
+BEGIN
+
+--  Cache the cmnttype_id for ChangeLog
+  SELECT cmnttype_id INTO _cmnttypeid
+  FROM cmnttype
+  WHERE (cmnttype_name='ChangeLog');
+  IF (NOT FOUND) THEN
+    RAISE EXCEPTION 'Comment type ChangeLog not found';
+  END IF;
+
+  IF (TG_OP = 'INSERT') THEN
+    PERFORM postComment(_cmnttypeid, 'J', NEW.prj_id, 'Created');
+
+  ELSIF (TG_OP = 'UPDATE') THEN
+    IF (OLD.prj_start_date <> NEW.prj_start_date) THEN
+      PERFORM postComment( _cmnttypeid, 'J', NEW.prj_id,
+                           ('Start Date Changed from ' || formatDate(OLD.prj_start_date) || ' to ' || formatDate(NEW.prj_start_date)) );
+    END IF;
+    IF (OLD.prj_due_date <> NEW.prj_due_date) THEN
+      PERFORM postComment( _cmnttypeid, 'J', NEW.prj_id,
+                           ('Due Date Changed from ' || formatDate(OLD.prj_due_date) || ' to ' || formatDate(NEW.prj_due_date)) );
+    END IF;
+    IF (OLD.prj_assigned_date <> NEW.prj_assigned_date) THEN
+      PERFORM postComment( _cmnttypeid, 'J', NEW.prj_id,
+                           ('Assigned Date Changed from ' || formatDate(OLD.prj_assigned_date) || ' to ' || formatDate(NEW.prj_assigned_date)) );
+    END IF;
+    IF (OLD.prj_completed_date <> NEW.prj_completed_date) THEN
+      PERFORM postComment( _cmnttypeid, 'J', NEW.prj_id,
+                           ('Completed Date Changed from ' || formatDate(OLD.prj_completed_date) || ' to ' || formatDate(NEW.prj_completed_date)) );
+    END IF;
+
+  END IF;
+
+  RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+SELECT dropIfExists('TRIGGER', 'prjaftertrigger');
+CREATE TRIGGER prjaftertrigger
+  AFTER INSERT OR UPDATE
+  ON prj
+  FOR EACH ROW
+  EXECUTE PROCEDURE _prjAfterTrigger();
