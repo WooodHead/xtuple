8fdf72fb0b433e32dfed8cb5f74fe99fb621313c
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2012-01-18 22:02:36 +0000

    merge packaging changes and reference databases from branch to trunk after testing 3.8.0rc2-to-3.8.0(final) and 3.7.x-to-3.8.0(final) packages

--- xtupleserver/trunk/updatescripts/from_380rc2/resetDocaddSeq.sql
+++ xtupleserver/trunk/updatescripts/from_380rc2/resetDocaddSeq.sql
@@ -1,3 +1,17 @@
-SELECT SETVAL('docass_docass_id_seq',
-              GREATEST(NEXTVAL('docass_docass_id_seq'),
-                       NEXTVAL('imageass_imageass_id_seq'));
+CREATE OR REPLACE FUNCTION upgradeResetDocAssIdSeq() RETURNS INTEGER AS $$
+BEGIN
+  BEGIN
+    SELECT SETVAL('docass_docass_id_seq',
+                  GREATEST(NEXTVAL('docass_docass_id_seq'),
+                           NEXTVAL('imageass_imageass_id_seq')));
+  EXCEPTION WHEN undefined_table THEN
+    RETURN 0;
+  END;
+
+  RETURN 1;
+END;
+$$ LANGUAGE 'plpgsql';
+
+SELECT upgradeResetDocAssIdSeq();
+
+SELECT dropIfExists('FUNCTION', 'upgradeResetDocAssIdSeq()');
