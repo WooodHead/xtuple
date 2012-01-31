dc9cda7bae6127cd3f0b93b320a0d8ec779256cc
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2012-01-31 15:50:53 +0000

    update copyright notices

--- xtdesktop/trunk/resources/database/functions/fetchwelcomehtml.sql
+++ xtdesktop/trunk/resources/database/functions/fetchwelcomehtml.sql
@@ -1,6 +1,6 @@
 
 CREATE OR REPLACE FUNCTION xtdesktop.fetchWelcomeHtml() RETURNS TEXT IMMUTABLE AS $$
--- Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple.
+-- Copyright (c) 1999-2012 by OpenMFG LLC, d/b/a xTuple.
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
   _html TEXT;
