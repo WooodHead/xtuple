82a4ae3f43967f87b6f53d8300cd7e5eb5d7bb8e
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2012-01-31 15:05:58 +0000

    make sure the function gets into the right schema

--- xtdesktop/trunk/resources/database/functions/fetchwelcomehtml.sql
+++ xtdesktop/trunk/resources/database/functions/fetchwelcomehtml.sql
@@ -1,5 +1,5 @@
 
-CREATE OR REPLACE FUNCTION fetchWelcomeHtml() RETURNS TEXT IMMUTABLE AS $$
+CREATE OR REPLACE FUNCTION xtdesktop.fetchWelcomeHtml() RETURNS TEXT IMMUTABLE AS $$
 -- Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple.
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
