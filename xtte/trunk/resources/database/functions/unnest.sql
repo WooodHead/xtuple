dbdcf1f724c4ec3b441696e006789ff4eaf0386b
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2012-01-31 16:08:38 +0000

    update copyright notices

--- xtte/trunk/resources/database/functions/unnest.sql
+++ xtte/trunk/resources/database/functions/unnest.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION te.unnest(anyarray)
   RETURNS SETOF anyelement AS
 $BODY$
--- Copyright (c) 1999-2011 by OpenMFG LLC, d/b/a xTuple.
+-- Copyright (c) 1999-2012 by OpenMFG LLC, d/b/a xTuple.
 -- See www.xtuple.com/CPAL for the full text of the software license.
 SELECT $1[i] FROM
     generate_series(array_lower($1,1),
