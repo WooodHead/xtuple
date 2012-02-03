d72464269432381854fef717c3a3dd222e952f7b
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2012-02-03 18:42:43 +0000

    Issue #16616:create and close WO which allows use of normal postProduction

--- xtupleserver/trunk/dbscripts/functions/postproduction.sql
+++ xtupleserver/trunk/dbscripts/functions/postproduction.sql
@@ -86,8 +86,13 @@ BEGIN
 		AND  (womatl_itemsite_id=itemsite_id)
 		AND  (itemsite_item_id=item_id)) LOOP
       -- Don't issue more than should have already been consumed at this point
-      IF (noNeg(_r.expected - _r.consumed) > 0) THEN
-        SELECT issueWoMaterial(_r.womatl_id, noNeg(_r.expected - _r.consumed), _itemlocSeries) INTO _itemlocSeries;
+      IF (pQty > 0) THEN
+        IF (noNeg(_r.expected - _r.consumed) > 0) THEN
+          SELECT issueWoMaterial(_r.womatl_id, noNeg(_r.expected - _r.consumed), _itemlocSeries) INTO _itemlocSeries;
+        END IF;
+      ELSE
+        -- Used by postMiscProduction of disassembly
+        SELECT returnWoMaterial(_r.womatl_id, (_r.expected * -1.0), _itemlocSeries, CURRENT_TIMESTAMP) INTO _itemlocSeries;
       END IF;
 
       UPDATE womatl
