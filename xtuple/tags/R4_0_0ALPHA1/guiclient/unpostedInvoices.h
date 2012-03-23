4bcab0bd6e0abf739d5880f0523fff7bf77119b9
gilmoskowitz <gilmoskowitz@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2012-03-23 17:24:06 +0000

    retagging trunk for retry of xtuple erp 4.0.0alpha

---
+++ xtuple/tags/R4_0_0ALPHA1/guiclient/unpostedInvoices.h
@@ -0,0 +1,37 @@
+/*
+ * This file is part of the xTuple ERP: PostBooks Edition, a free and
+ * open source Enterprise Resource Planning software suite,
+ * Copyright (c) 1999-2012 by OpenMFG LLC, d/b/a xTuple.
+ * It is licensed to you under the Common Public Attribution License
+ * version 1.0, the full text of which (including xTuple-specific Exhibits)
+ * is available at www.xtuple.com/CPAL.  By using this software, you agree
+ * to be bound by its terms.
+ */
+
+#ifndef UNPOSTEDINVOICES_H
+#define UNPOSTEDINVOICES_H
+
+#include "guiclient.h"
+#include "display.h"
+
+class unpostedInvoices : public display
+{
+    Q_OBJECT
+
+public:
+    unpostedInvoices(QWidget* parent = 0, const char* name = 0, Qt::WFlags fl = Qt::Window);
+    virtual bool checkSitePrivs(int invcid);
+
+public slots:
+    virtual void sNew();
+    virtual void sEdit();
+    virtual void sView();
+    virtual void sDelete();
+    virtual void sPrint();
+    virtual void sPost();
+    virtual void sPopulateMenu(QMenu *, QTreeWidgetItem *, int);
+    virtual void sFillList();
+
+};
+
+#endif // UNPOSTEDINVOICES_H
