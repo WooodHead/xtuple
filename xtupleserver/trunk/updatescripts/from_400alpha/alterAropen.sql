27386fbd29381ce4b46c9df67112de35596ded59
jrogelstad <jrogelstad@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2012-04-02 19:39:38 +0000

    add defaults to aropen for data consistency

---
+++ xtupleserver/trunk/updatescripts/from_400alpha/alterAropen.sql
@@ -0,0 +1,4 @@
+alter table aropen alter column aropen_discount set default false;
+alter table aropen alter column aropen_cobmisc_id set default -1;
+alter table aropen alter column aropen_salescat_id set default -1;
+
