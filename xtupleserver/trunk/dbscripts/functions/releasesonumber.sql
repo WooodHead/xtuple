17400dfb13f64b40ab5f7353e08775453500e915
gpazo <gpazo@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2012-06-11 13:27:13 +0000

    issue #17704 modified db function releasesonumber to handle the '' and NULL input cases

--- xtupleserver/trunk/dbscripts/functions/releasesonumber.sql
+++ xtupleserver/trunk/dbscripts/functions/releasesonumber.sql
@@ -7,5 +7,14 @@ $$ LANGUAGE 'sql';
 CREATE OR REPLACE FUNCTION releaseSoNumber(TEXT) RETURNS INTEGER AS $$
 -- Copyright (c) 1999-2012 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
-  SELECT releaseNumber('SoNumber', $1::INTEGER);
-$$ LANGUAGE 'sql';
+BEGIN
+	IF (COALESCE($1, '') = '' OR $1 ~ '[^0-9]')
+	THEN
+	--do nothing;
+	RETURN 1;
+	ELSE
+	RETURN releaseNumber('SoNumber', $1::INTEGER);
+	
+	END IF;
+END;
+$$ LANGUAGE 'plpgsql';
