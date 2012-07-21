397b598921f8a38cd892d10a99e848efc5ef7074
omfgppc <omfgppc@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2012-07-21 01:09:25 +0000

    aligned notes Left on tesheet

--- xtte/trunk/resources/client/scripts/timeExpenseSheet.js
+++ xtte/trunk/resources/client/scripts/timeExpenseSheet.js
@@ -50,7 +50,7 @@ _lines.addColumn(qsTr("Item"),          XTreeWidget.itemColumn,   Qt.AlignLeft, 
 _lines.addColumn(qsTr("Description"),   -1,                       Qt.AlignLeft,    true,   "item_descrip1");
 _lines.addColumn(qsTr("Hours"),         XTreeWidget.qtyColumn,    Qt.AlignRight,   true,   "hours");
 _lines.addColumn(qsTr("Expense"),       XTreeWidget.qtyColumn,    Qt.AlignRight,   true,   "expense");
-_lines.addColumn(qsTr("Notes"),         XTreeWidget.qtyColumn,    Qt.AlignRight,   false,  "f_notes");
+_lines.addColumn(qsTr("Notes"),         XTreeWidget.qtyColumn,    Qt.AlignLeft,    false,  "f_notes");
 
 if (privileges.check("CanViewRates"))
 {
