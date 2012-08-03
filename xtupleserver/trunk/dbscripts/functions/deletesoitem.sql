35c9f0e3e5e9b7dfda9b78ab48dcc8782f5187bc
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2012-08-03 18:42:49 +0000

    Issue #18143:remove redundant deletion of supply orders and create separate delete triggers

--- xtupleserver/trunk/dbscripts/functions/deletesoitem.sql
+++ xtupleserver/trunk/dbscripts/functions/deletesoitem.sql
@@ -91,24 +91,22 @@ BEGIN
   WHERE (coitem_id=pSoitemid);
 
   IF (_jobItem AND _r.coitem_order_type='W') THEN
--- Delete associated Work Order
+-- Delete associated Job Work Order
     SELECT deleteWo(_r.coitem_order_id, TRUE, TRUE) INTO _result;
     IF (_result < 0) THEN
       RETURN -104;
     END IF;
-  ELSE
--- Break association
-    IF (_r.coitem_order_type='R') THEN
-      UPDATE pr SET pr_prj_id=-1
-      WHERE (pr_id=_r.coitem_order_id);
-    ELSE
-      IF (_r.coitem_order_type='W') THEN
-        PERFORM changeWoProject(_r.coitem_order_id, -1, TRUE);
-      END IF;
+  ELSIF (_r.coitem_order_type='W') THEN
+-- Delete associated Job Work Order
+    SELECT deleteWo(_r.coitem_order_id, TRUE) INTO _result;
+    IF (_result < 0) THEN
+      -- Cannot delete so break association
+      PERFORM changeWoProject(_r.coitem_order_id, -1, TRUE);
     END IF;
-  END IF;
-
-  IF (_r.coitem_order_type='P') THEN
+  ELSIF (_r.coitem_order_type='R') THEN
+-- Delete associated Purchase Request
+    PERFORM deletePr(_r.coitem_order_id);
+  ELSIF (_r.coitem_order_type='P') THEN
 -- Delete associated Purchase Order Item
     SELECT deletepoitem(_r.coitem_order_id) INTO _result;
     IF ((_result < 0) AND (_result <> -20)) THEN
