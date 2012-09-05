1d50ffe8074087221155f8cb20435ee14ff200f8
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2012-09-05 20:08:31 +0000

    Issue #18313:merge ipsprodcat into ipsiteminfo

--- xtupleserver/trunk/dbscripts/triggers/item.sql
+++ xtupleserver/trunk/dbscripts/triggers/item.sql
@@ -224,6 +224,7 @@ BEGIN
           PERFORM postComment( _cmnttypeid, 'I', NEW.item_id,
                                ( 'Maximum Desired Cost Changed from "' || formatCost(OLD.item_maxcost) ||
                                  '" to "' || formatCost(NEW.item_maxcost) || '"' ) );
+        END IF;
 
         IF (OLD.item_listcost <> NEW.item_listcost) THEN
           PERFORM postComment( _cmnttypeid, 'I', NEW.item_id,
