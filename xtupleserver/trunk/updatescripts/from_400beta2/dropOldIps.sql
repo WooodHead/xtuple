849a43afa8e882f4003368f888fd66b07b58481e
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2012-09-05 19:26:59 +0000

    Issue #18313:merge ipsprodcat into ipsiteminfo

--- xtupleserver/trunk/updatescripts/from_400beta2/dropOldIps.sql
+++ xtupleserver/trunk/updatescripts/from_400beta2/dropOldIps.sql
@@ -1,4 +1,2 @@
 SELECT dropIfExists('VIEW', 'ipsitem', 'public', true);
 SELECT dropIfExists('VIEW', 'itemprices', 'public', true);
-SELECT dropIfExists('FUNCTION', 'updateprice', 'public', true);
-
