59623c4e6193022d5d37b4793ad11d23413dbc45
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2012-09-13 19:06:43 +0000

    Issue #18387:item source enhancements

---
+++ xtupleserver/trunk/updatescripts/from_400beta2/alterItemsrc.sql
@@ -0,0 +1,18 @@
+ALTER TABLE itemsrc ADD COLUMN itemsrc_effective DATE;
+ALTER TABLE itemsrc ADD COLUMN itemsrc_expires DATE;
+--ALTER TABLE itemsrc ADD COLUMN itemsrc_contrct_id INTEGER REFERENCES contrct(contrct_id);
+
+UPDATE itemsrc SET itemsrc_effective=startOfTime(), itemsrc_expires=endOfTime();
+
+ALTER TABLE itemsrc ALTER COLUMN itemsrc_effective SET NOT NULL;
+ALTER TABLE itemsrc ALTER COLUMN itemsrc_effective SET DEFAULT startOfTime();
+ALTER TABLE itemsrc ALTER COLUMN itemsrc_expires SET NOT NULL;
+ALTER TABLE itemsrc ALTER COLUMN itemsrc_expires SET DEFAULT endOfTime();
+
+ALTER TABLE itemsrc DROP CONSTRAINT itemsrc_itemsrc_vend_id_key;
+--CREATE UNIQUE INDEX itemsrc_itemsrc_vend_id_key ON itemsrc USING btree
+--  (itemsrc_vend_id, itemsrc_item_id, itemsrc_contrct_id, itemsrc_effective, itemsrc_expires,
+--   itemsrc_vend_item_number, itemsrc_manuf_name, itemsrc_manuf_item_number);
+CREATE UNIQUE INDEX itemsrc_itemsrc_vend_id_key ON itemsrc USING btree
+  (itemsrc_vend_id, itemsrc_item_id, itemsrc_effective, itemsrc_expires,
+   itemsrc_vend_item_number, itemsrc_manuf_name, itemsrc_manuf_item_number);
