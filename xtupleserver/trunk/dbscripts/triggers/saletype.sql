0b9633cace8a6a1dcf49b0aafec4039961083ef4
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2012-12-13 20:33:17 +0000

    Issue #18974:fix problems with deleting a saletype

--- xtupleserver/trunk/dbscripts/triggers/saletype.sql
+++ xtupleserver/trunk/dbscripts/triggers/saletype.sql
@@ -8,7 +8,7 @@ BEGIN
 --  Check to see if any sales orders are assigned to the passed saletype
   SELECT cohead_number INTO _check
   FROM cohead
-  WHERE (cohead_saletype_id=pSaletypeid)
+  WHERE (cohead_saletype_id=OLD.saletype_id)
   LIMIT 1;
   IF (FOUND) THEN
     RAISE EXCEPTION 'Assigned to Sales Order % and possibly more. [xtuple: deletesaletype, -1]', _check;
@@ -17,7 +17,7 @@ BEGIN
 --  Check to see if any quotes are assigned to the passed saletype
   SELECT quhead_number INTO _check
   FROM quhead
-  WHERE (quhead_saletype_id=pSaletypeid)
+  WHERE (quhead_saletype_id=OLD.saletype_id)
   LIMIT 1;
   IF (FOUND) THEN
     RAISE EXCEPTION 'Assigned to Quote % and possibly more. [xtuple: deletesaletype, -2]', _check;
@@ -26,7 +26,7 @@ BEGIN
 --  Check to see if any invoice are assigned to the passed saletype
   SELECT invchead_invcnumber INTO _check
   FROM invchead
-  WHERE (invchead_saletype_id=pSaletypeid)
+  WHERE (invchead_saletype_id=OLD.saletype_id)
   LIMIT 1;
   IF (FOUND) THEN
     RAISE EXCEPTION 'Assigned to Invoice % and possibly more. [xtuple: deletesaletype, -3]', _check;
@@ -35,10 +35,21 @@ BEGIN
 --  Check to see if any credit memos are assigned to the passed saletype
   SELECT cmhead_number INTO _check
   FROM cmhead
-  WHERE (cmhead_saletype_id=pSaletypeid)
+  WHERE (cmhead_saletype_id=OLD.saletype_id)
   LIMIT 1;
   IF (FOUND) THEN
     RAISE EXCEPTION 'Assigned to Credit Memo % and possibly more. [xtuple: deletesaletype, -4]', _check;
+  END IF;
+
+--  Check to see if any return ruthorizations are assigned to the passed saletype
+  IF (fetchMetricBool('EnableReturnAuth')) THEN
+    SELECT rahead_number INTO _check
+    FROM rahead
+    WHERE (rahead_saletype_id=OLD.saletype_id)
+    LIMIT 1;
+    IF (FOUND) THEN
+      RAISE EXCEPTION 'Assigned to Return Authorization % and possibly more returns. [xtuple: deleteSaleType, -5]', _check;
+    END IF;
   END IF;
 
   RETURN OLD;
