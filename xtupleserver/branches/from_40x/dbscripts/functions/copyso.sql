7415f3b1d177c31a7737983d67c5f2fc2e691d97
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2013-02-11 16:08:35 +0000

    Issue #19259:backport to 4.0.2

--- xtupleserver/branches/from_40x/dbscripts/functions/copyso.sql
+++ xtupleserver/branches/from_40x/dbscripts/functions/copyso.sql
@@ -1,10 +1,11 @@
-CREATE OR REPLACE FUNCTION copySo(INTEGER, DATE) RETURNS INTEGER AS $$
+CREATE OR REPLACE FUNCTION copySo(pSoheadid INTEGER,
+                                  pSchedDate DATE) RETURNS INTEGER AS $$
 -- Copyright (c) 1999-2012 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
-  pSoheadid ALIAS FOR $1;
-  pSchedDate ALIAS FOR $2;
   _soheadid INTEGER;
+  _soitemid INTEGER;
+  _soitem RECORD;
 
 BEGIN
 
@@ -173,94 +174,99 @@ BEGIN
   FROM cohead
   WHERE (cohead_id=pSoheadid);
 
-  INSERT INTO coitem
-  ( coitem_cohead_id,
-    coitem_linenumber,
-    coitem_itemsite_id,
-    coitem_status,
-    coitem_scheddate,
-    coitem_promdate,
-    coitem_qtyord,
-    coitem_unitcost,
-    coitem_price,
-    coitem_custprice,
-    coitem_qtyshipped,
-    coitem_order_id,
-    coitem_memo,
-    coitem_imported,
-    coitem_qtyreturned,
-    coitem_closedate,
-    coitem_custpn,
-    coitem_order_type,
-    coitem_close_username,
---    coitem_lastupdated,
-    coitem_substitute_item_id,
-    coitem_created,
-    coitem_creator,
-    coitem_prcost,
-    coitem_qty_uom_id,
-    coitem_qty_invuomratio,
-    coitem_price_uom_id,
-    coitem_price_invuomratio,
-    coitem_warranty,
-    coitem_cos_accnt_id,
-    coitem_qtyreserved,
-    coitem_subnumber,
-    coitem_firm,
-    coitem_taxtype_id )
-  SELECT
-    _soheadid,
-    coitem_linenumber,
-    coitem_itemsite_id,
-    'O',
-    COALESCE(pSchedDate, coitem_scheddate),
-    coitem_promdate,
-    coitem_qtyord,
-    stdCost(itemsite_item_id),
-    coitem_price,
-    coitem_custprice,
-    0.0,
-    -1,
-    coitem_memo,
-    FALSE,
-    0.0,
-    NULL,
-    coitem_custpn,
-    coitem_order_type,
-    NULL,
---    NULL,
-    coitem_substitute_item_id,
-    NULL,
-    getEffectiveXtUser(),
-    coitem_prcost,
-    coitem_qty_uom_id,
-    coitem_qty_invuomratio,
-    coitem_price_uom_id,
-    coitem_price_invuomratio,
-    coitem_warranty,
-    coitem_cos_accnt_id,
-    0.0,
-    coitem_subnumber,
-    coitem_firm,
-    coitem_taxtype_id
-  FROM coitem, itemsite
-  WHERE ( (coitem_itemsite_id=itemsite_id)
-   AND (coitem_status <> 'X')
-   AND (coitem_cohead_id=pSoheadid)
-   AND (coitem_subnumber = 0) );
+  FOR _soitem IN
+    SELECT *
+    FROM coitem JOIN itemsite ON (itemsite_id=coitem_itemsite_id)
+    WHERE ( (coitem_cohead_id=pSoheadid)
+      AND   (coitem_status <> 'X')
+      AND   (coitem_subnumber = 0) ) LOOP
 
-  INSERT INTO charass
-        (charass_target_type, charass_target_id,
-         charass_char_id, charass_value)
-  SELECT charass_target_type, b.coitem_id,
-         charass_char_id, charass_value
-    FROM coitem a, charass, coitem b
-   WHERE ((charass_target_type='SI')
-     AND  (charass_target_id=a.coitem_id)
-     AND  (a.coitem_cohead_id=pSoheadid)
-     AND  (b.coitem_cohead_id=_soheadid)
-     AND  (a.coitem_linenumber=b.coitem_linenumber)
-     AND  (a.coitem_subnumber=b.coitem_subnumber));
+    SELECT NEXTVAL('coitem_coitem_id_seq') INTO _soitemid;
+
+    -- insert characteristics first so they can be copied to associated supply order
+    INSERT INTO charass
+          (charass_target_type, charass_target_id,
+           charass_char_id, charass_value)
+    SELECT charass_target_type, _soitemid,
+           charass_char_id, charass_value
+      FROM charass
+     WHERE ((charass_target_type='SI')
+       AND  (charass_target_id=_soitem.coitem_id));
+
+    INSERT INTO coitem
+    ( coitem_id,
+      coitem_cohead_id,
+      coitem_linenumber,
+      coitem_itemsite_id,
+      coitem_status,
+      coitem_scheddate,
+      coitem_promdate,
+      coitem_qtyord,
+      coitem_unitcost,
+      coitem_price,
+      coitem_custprice,
+      coitem_qtyshipped,
+      coitem_order_id,
+      coitem_memo,
+      coitem_imported,
+      coitem_qtyreturned,
+      coitem_closedate,
+      coitem_custpn,
+      coitem_order_type,
+      coitem_close_username,
+--      coitem_lastupdated,
+      coitem_substitute_item_id,
+      coitem_created,
+      coitem_creator,
+      coitem_prcost,
+      coitem_qty_uom_id,
+      coitem_qty_invuomratio,
+      coitem_price_uom_id,
+      coitem_price_invuomratio,
+      coitem_warranty,
+      coitem_cos_accnt_id,
+      coitem_qtyreserved,
+      coitem_subnumber,
+      coitem_firm,
+      coitem_taxtype_id )
+    VALUES
+    ( _soitemid,
+      _soheadid,
+      _soitem.coitem_linenumber,
+      _soitem.coitem_itemsite_id,
+      'O',
+      COALESCE(pSchedDate, _soitem.coitem_scheddate),
+      _soitem.coitem_promdate,
+      _soitem.coitem_qtyord,
+      stdCost(_soitem.itemsite_item_id),
+      _soitem.coitem_price,
+      _soitem.coitem_custprice,
+      0.0,
+      -1,
+      _soitem.coitem_memo,
+      FALSE,
+      0.0,
+      NULL,
+      _soitem.coitem_custpn,
+      _soitem.coitem_order_type,
+      NULL,
+--      NULL,
+      _soitem.coitem_substitute_item_id,
+      NULL,
+      getEffectiveXtUser(),
+      _soitem.coitem_prcost,
+      _soitem.coitem_qty_uom_id,
+      _soitem.coitem_qty_invuomratio,
+      _soitem.coitem_price_uom_id,
+      _soitem.coitem_price_invuomratio,
+      _soitem.coitem_warranty,
+      _soitem.coitem_cos_accnt_id,
+      0.0,
+      _soitem.coitem_subnumber,
+      _soitem.coitem_firm,
+      _soitem.coitem_taxtype_id );
+
+  END LOOP;
 
   RETURN _soheadid;
 
