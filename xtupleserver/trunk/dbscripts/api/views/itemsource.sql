45eedf36618185d8875348762bba828a311595b2
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2013-02-27 17:10:43 +0000

    Issue #19651:add new itemsrc and itemsrcp columns

--- xtupleserver/trunk/dbscripts/api/views/itemsource.sql
+++ xtupleserver/trunk/dbscripts/api/views/itemsource.sql
@@ -18,10 +18,15 @@ CREATE VIEW api.itemsource AS
        itemsrc.itemsrc_vend_item_descrip AS vendor_description,
        itemsrc.itemsrc_manuf_name AS manufacturer_name,
        itemsrc.itemsrc_manuf_item_number AS manufacturer_item_number,
-       itemsrc.itemsrc_manuf_item_descrip AS manufacturer_description
+       itemsrc.itemsrc_manuf_item_descrip AS manufacturer_description,
+       itemsrc.itemsrc_upccode AS bar_code,
+       contrct.contrct_number AS contract_number,
+       itemsrc.itemsrc_effective AS effective_date,
+       itemsrc.itemsrc_expires AS expires_date
    FROM itemsrc
    LEFT JOIN item ON itemsrc.itemsrc_item_id = item.item_id
    LEFT JOIN vendinfo ON itemsrc.itemsrc_vend_id = vendinfo.vend_id
+   LEFT JOIN contrct ON itemsrc.itemsrc_contrct_id = contrct.contrct_id
   ORDER BY item.item_number::VARCHAR(100), vendinfo.vend_number::VARCHAR(100);
         
 GRANT ALL ON TABLE api.itemsource TO xtrole;
@@ -48,7 +53,11 @@ CREATE OR REPLACE RULE "_INSERT" AS
     itemsrc_default,
     itemsrc_manuf_name,
     itemsrc_manuf_item_number,
-    itemsrc_manuf_item_descrip) 
+    itemsrc_manuf_item_descrip,
+    itemsrc_upccode,
+    itemsrc_contrct_id,
+    itemsrc_effective,
+    itemsrc_expires) 
   VALUES (
     getitemid(NEW.item_number), 
     getvendid(NEW.vendor), 
@@ -65,7 +74,12 @@ CREATE OR REPLACE RULE "_INSERT" AS
     COALESCE(NEW.itemsrc_default, true),
     COALESCE(NEW.manufacturer_name,''),
     COALESCE(NEW.manufacturer_item_number,''),
-    NEW.manufacturer_description);
+    NEW.manufacturer_description,
+    NEW.bar_code,
+    getcontrctid(NEW.contract_number),
+    COALESCE(getcontrcteffective(NEW.contract_number), NEW.effective_date),
+    COALESCE(getcontrctexpires(NEW.contract_number), NEW.expires_date))
+;
 
 CREATE OR REPLACE RULE "_UPDATE" AS 
     ON UPDATE TO api.itemsource DO INSTEAD
@@ -84,7 +98,11 @@ CREATE OR REPLACE RULE "_UPDATE" AS
     itemsrc_default = NEW.itemsrc_default,
     itemsrc_manuf_name = NEW.manufacturer_name,
     itemsrc_manuf_item_number = NEW.manufacturer_item_number,
-    itemsrc_manuf_item_descrip = NEW.manufacturer_description
+    itemsrc_manuf_item_descrip = NEW.manufacturer_description,
+    itemsrc_upccode=NEW.bar_code,
+    itemsrc_contrct_id=getcontrctid(NEW.contract_number),
+    itemsrc_effective=COALESCE(getcontrcteffective(NEW.contract_number), NEW.effective_date),
+    itemsrc_expires=COALESCE(getcontrctexpires(NEW.contract_number), NEW.expires_date)
   WHERE ((itemsrc.itemsrc_item_id = getitemid(old.item_number)) 
   AND (itemsrc.itemsrc_vend_id = getvendid(old.vendor))
   AND (itemsrc.itemsrc_vend_item_number=old.vendor_item_number)
