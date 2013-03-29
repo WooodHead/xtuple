976a1eb6a83be5d5dd0d8bc0fbda0a4d55eb8981
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2013-03-29 20:53:19 +0000

    Issue #19961:performance tweaks

--- xtupleserver/trunk/dbscripts/functions/roundlocale.sql
+++ xtupleserver/trunk/dbscripts/functions/roundlocale.sql
@@ -1,7 +1,7 @@
 
 CREATE OR REPLACE FUNCTION roundLocale(pFractional BOOLEAN,
                                        pQty NUMERIC,
-                                       pLocale TEXT) RETURNS NUMERIC AS $$
+                                       pLocale TEXT) RETURNS NUMERIC STABLE AS $$
 -- Copyright (c) 1999-2012 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
@@ -9,19 +9,19 @@ DECLARE
   _scale INTEGER;
 
 BEGIN
-  SELECT * INTO _r
-  FROM locale, usr
-  WHERE ((usr_locale_id=locale_id) AND (usr_username=getEffectiveXtUser()));
-
-  _scale := CASE pLocale WHEN 'qtyper' THEN _r.locale_qtyper_scale
-                         WHEN 'cost' THEN _r.locale_cost_scale
-                         ELSE _r.locale_qty_scale
-            END;
-
   IF (pFractional) THEN
+    SELECT * INTO _r
+    FROM locale, usr
+    WHERE ((usr_locale_id=locale_id) AND (usr_username=getEffectiveXtUser()));
+
+    _scale := CASE pLocale WHEN 'qtyper' THEN _r.locale_qtyper_scale
+                           WHEN 'cost' THEN _r.locale_cost_scale
+                           ELSE _r.locale_qty_scale
+              END;
+
     RETURN ROUND(pQty, _scale);
   ELSE
-    IF (TRUNC(pQty) < ROUND(pQty, _scale)) THEN
+    IF (TRUNC(pQty) < pQty) THEN
       RETURN (TRUNC(pQty) + 1);
     ELSE
       RETURN TRUNC(pQty);
