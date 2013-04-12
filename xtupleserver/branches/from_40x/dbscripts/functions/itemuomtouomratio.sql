6ffb87284d8c220f47b6582549f78d36e83c2538
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2013-04-12 15:10:47 +0000

    Issue #19760:backport to 4.0.3

--- xtupleserver/branches/from_40x/dbscripts/functions/itemuomtouomratio.sql
+++ xtupleserver/branches/from_40x/dbscripts/functions/itemuomtouomratio.sql
@@ -1,15 +1,15 @@
-CREATE OR REPLACE FUNCTION itemuomtouomratio(INTEGER, INTEGER, INTEGER) RETURNS NUMERIC STABLE AS $$
+CREATE OR REPLACE FUNCTION itemuomtouomratio(pItemid INTEGER,
+                                             pUomidFrom INTEGER,
+                                             pUomidTo INTEGER) RETURNS NUMERIC STABLE AS $$
 -- Copyright (c) 1999-2012 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
-  pItemid ALIAS FOR $1;
-  pUomidFrom ALIAS FOR $2;
-  pUomidTo ALIAS FOR $3;
-
   _uomidFrom INTEGER;
   _uomidTo   INTEGER;
-  _valueFrom NUMERIC;
-  _valueTo   NUMERIC;
+  _uomidInv  INTEGER;
+  _valueFrom NUMERIC := 0.0;
+  _valueTo   NUMERIC := 0.0;
+  _value     NUMERIC := 0.0;
   _item      RECORD;
   _conv      RECORD;
 BEGIN
@@ -24,15 +24,13 @@ BEGIN
 
   _uomidFrom := COALESCE(pUomidFrom, _item.item_inv_uom_id);
   _uomidTo   := COALESCE(pUomidTo,   _item.item_inv_uom_id);
+  _uomidInv  := _item.item_inv_uom_id;
 
   IF(_uomidFrom = _uomidTo) THEN
     RETURN 1.0;
   END IF;
 
-  IF(_uomidFrom != _item.item_inv_uom_id AND _uomidTo != _item.item_inv_uom_id) THEN
-    RAISE EXCEPTION 'Converting from/to a UOM where one is not the inventory UOM is currently not supported';
-  END IF;
-
+  -- Try a direct conversion
   SELECT itemuomconv_from_uom_id, itemuomconv_from_value,
          itemuomconv_to_uom_id, itemuomconv_to_value
     INTO _conv
@@ -40,19 +38,57 @@ BEGIN
    WHERE(((itemuomconv_from_uom_id=_uomidFrom AND itemuomconv_to_uom_id=_uomidTo)
        OR (itemuomconv_from_uom_id=_uomidTo AND itemuomconv_to_uom_id=_uomidFrom))
      AND (itemuomconv_item_id=pItemid));
-  IF(NOT FOUND) THEN
-    RAISE EXCEPTION 'A conversion for item_id % from uom_id % to uom_id % was not found.', pItemid, _uomidFrom, _uomidTo;
-  END IF;
-
-  IF(_conv.itemuomconv_from_uom_id=_uomidFrom) THEN
-    _valueFrom := _conv.itemuomconv_from_value;
-    _valueTo := _conv.itemuomconv_to_value;
+  IF(FOUND) THEN
+    IF(_conv.itemuomconv_from_uom_id=_uomidFrom) THEN
+      _valueFrom := _conv.itemuomconv_from_value;
+      _valueTo := _conv.itemuomconv_to_value;
+    ELSE
+      _valueFrom := _conv.itemuomconv_to_value;
+      _valueTo := _conv.itemuomconv_from_value;
+    END IF;
+    _value := (_valueTo / _valueFrom);
   ELSE
-    _valueFrom := _conv.itemuomconv_to_value;
-    _valueTo := _conv.itemuomconv_from_value;
+    -- Try to convert the from uom to the inventory uom
+    SELECT itemuomconv_from_uom_id, itemuomconv_from_value,
+           itemuomconv_to_uom_id, itemuomconv_to_value
+      INTO _conv
+      FROM itemuomconv
+     WHERE(((itemuomconv_from_uom_id=_uomidFrom AND itemuomconv_to_uom_id=_uomidInv)
+         OR (itemuomconv_from_uom_id=_uomidInv AND itemuomconv_to_uom_id=_uomidFrom))
+       AND (itemuomconv_item_id=pItemid));
+    IF(NOT FOUND) THEN
+      RAISE EXCEPTION 'A conversion for item_id % from uom_id % to inv_uom_id % was not found.', pItemid, _uomidFrom, _uomidInv;
+    END IF;
+    IF(_conv.itemuomconv_from_uom_id=_uomidInv) THEN
+      _valueFrom := _conv.itemuomconv_from_value;
+      _valueTo := _conv.itemuomconv_to_value;
+    ELSE
+      _valueFrom := _conv.itemuomconv_to_value;
+      _valueTo := _conv.itemuomconv_from_value;
+    END IF;
+    _value := (_valueTo / _valueFrom);
+    -- Try to convert the to uom to the inventory uom
+    SELECT itemuomconv_from_uom_id, itemuomconv_from_value,
+           itemuomconv_to_uom_id, itemuomconv_to_value
+      INTO _conv
+      FROM itemuomconv
+     WHERE(((itemuomconv_from_uom_id=_uomidInv AND itemuomconv_to_uom_id=_uomidTo)
+         OR (itemuomconv_from_uom_id=_uomidTo AND itemuomconv_to_uom_id=_uomidInv))
+       AND (itemuomconv_item_id=pItemid));
+    IF(NOT FOUND) THEN
+      RAISE EXCEPTION 'A conversion for item_id % from uom_id % to inv_uom_id % was not found.', pItemid, _uomidTo, _uomidInv;
+    END IF;
+    IF(_conv.itemuomconv_from_uom_id=_uomidInv) THEN
+      _valueFrom := _conv.itemuomconv_from_value;
+      _valueTo := _conv.itemuomconv_to_value;
+    ELSE
+      _valueFrom := _conv.itemuomconv_to_value;
+      _valueTo := _conv.itemuomconv_from_value;
+    END IF;
+    _value := _value * (_valueTo / _valueFrom);
   END IF;
 
-  RETURN (_valueTo/_valueFrom);
+  RETURN _value;
 END;
 $$ LANGUAGE 'plpgsql';
 
