a14343f37ec68a5987983183e92e1feb4194183f
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2013-04-26 01:50:43 +0000

    Issue #19971:improve performance

--- xtupleserver/trunk/dbscripts/functions/roundcost.sql
+++ xtupleserver/trunk/dbscripts/functions/roundcost.sql
@@ -1,23 +1,20 @@
 
-CREATE OR REPLACE FUNCTION roundCost(NUMERIC) RETURNS NUMERIC AS $$
+CREATE OR REPLACE FUNCTION roundCost(pCost NUMERIC) RETURNS NUMERIC IMMUTABLE AS $$
 -- Copyright (c) 1999-2012 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
-  _pCost ALIAS FOR $1;
-
   _scale INTEGER;
 
 BEGIN
-  IF (_pCost IS NULL) THEN
+  IF (pCost IS NULL) THEN
     RETURN NULL;
   END IF;
 
   SELECT locale_cost_scale INTO _scale
-  FROM locale, usr
-  WHERE ((usr_locale_id=locale_id)
-     AND (usr_username=getEffectiveXtUser()));
+  FROM locale
+  WHERE (locale_id=getUsrLocaleId());
 
-  RETURN ROUND(_pCost, _scale);
+  RETURN ROUND(pCost, _scale);
 
 END;
 $$ LANGUAGE 'plpgsql';
