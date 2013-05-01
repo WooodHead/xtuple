214ceaac917527688ae63b24332b5fa028fba737
sunsetsolutions <sunsetsolutions@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2013-05-01 21:23:06 +0000

    Issue #20108:include itemsite_nnqoh in avg cost calc

--- xtupleserver/trunk/dbscripts/functions/avgcost.sql
+++ xtupleserver/trunk/dbscripts/functions/avgcost.sql
@@ -1,19 +1,19 @@
-CREATE OR REPLACE FUNCTION avgCost(INTEGER) RETURNS NUMERIC AS $$
+CREATE OR REPLACE FUNCTION avgCost(pItemsiteid INTEGER) RETURNS NUMERIC AS $$
 -- Copyright (c) 1999-2012 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
 DECLARE
-  pItemsiteid ALIAS FOR $1;
   _value NUMERIC;
   _qoh NUMERIC;
+  _qohnn NUMERIC;
 BEGIN
-  SELECT itemsite_value, itemsite_qtyonhand
-    INTO _value, _qoh
+  SELECT itemsite_value, itemsite_qtyonhand, itemsite_nnqoh
+    INTO _value, _qoh, _qohnn
     FROM itemsite
    WHERE(itemsite_id=pItemsiteid);
-  IF (_qoh = 0) THEN
-    RETURN 0;
+  IF (_qoh = 0.0 AND _qohnn = 0.0) THEN
+    RETURN 0.0;
   END IF;
-  RETURN _value / _qoh;
+  RETURN _value / (_qoh + _qohnn);
 END;
 $$ LANGUAGE 'plpgsql';
 
