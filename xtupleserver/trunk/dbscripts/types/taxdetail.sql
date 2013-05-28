0a4e56a47843dad0fd273408bc0d18d4ea785239
mikeodonnell <mikeodonnell@ea1a15ef-c238-0410-93cb-fa28e9f07733>
2013-05-28 20:35:27 +0000

    Issue #19337 - changed precision of taxrate percentage

--- xtupleserver/trunk/dbscripts/types/taxdetail.sql
+++ xtupleserver/trunk/dbscripts/types/taxdetail.sql
@@ -11,7 +11,7 @@ CREATE TYPE taxdetail AS
   taxdetail_tax_code text,
   taxdetail_tax_descrip text,
   taxdetail_tax_basis_tax_id integer,
-  taxdetail_taxrate_percent numeric(8,4),
+  taxdetail_taxrate_percent numeric(10,6),
   taxdetail_taxrate_amount numeric(16,2),
   taxdetail_level integer,
   taxdetail_taxclass_id integer,
